09133fb7b0e833e5b994d094fea4c4c758e8cdb0
thecockatiel <thecockatiel@proton.me>
2024-10-27 22:30:51 +0330

    chore: port mailbox message

---
+++ bisq/core/network/p2p/mailbox/mailbox_message.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from bisq.core.network.p2p.direct_message import DirectMessage
+from bisq.core.network.p2p.storage.payload.expirable_payload import ExpirablePayload
+from bisq.core.network.p2p.uid_message import UidMessage
+
+if TYPE_CHECKING:
+    from bisq.core.network.p2p.node_address import NodeAddress
+
+class MailboxMessage(DirectMessage, UidMessage, ExpirablePayload, ABC):
+    
+    @abstractmethod
+    def get_sender_node_address(self) -> NodeAddress:
+        pass
