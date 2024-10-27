33afbdb152e25535148368b925e7510d6d70a385
thecockatiel <thecockatiel@proton.me>
2024-10-27 22:04:30 +0330

    chore: port some basic stuff

---
+++ bisq/core/common/proto.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from google.protobuf.message import Message
+
+class Proto(ABC):
+    """
+    Base interface for Envelope and Payload.
+    """
+    @abstractmethod
+    def to_proto_message(self) -> Message:
+        pass
+
+    def serialize(self) -> bytes:
+        return self.to_proto_message().SerializeToString()
+
+    # If the class implements ExcludedFieldsProto this method will be overwritten so that
+    # fields annotated with ExcludeForHash will be excluded.
+    def serialize_for_hash(self) -> bytes:
+        return self.serialize()
