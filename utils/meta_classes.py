3e6f961179fc54be1f6de849c7f8e539f929dea5
thecockatiel <thecockatiel@proton.me>
2024-11-01 17:46:06 +0330

    fix: change Singleton to a decorator

--- utils/meta_classes.py
+++ utils/meta_classes.py
@@ -1,6 +1,14 @@
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+
+def Singleton(cls):
+    """
+    A class decorator that implements the Singleton pattern.
+    Can be used with any class regardless of inheritance.
+    """
+    instances = {}
+    
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+        
+    return get_instance
