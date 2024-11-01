c6a33b4b0de56f2f5227435912597db5edadb831
thecockatiel <thecockatiel@proton.me>
2024-11-01 22:39:47 +0330

    chore: add FileTransferPart

---
+++ bisq/core/network/p2p/file_transfer_part.py
@@ -0,0 +1,56 @@
+from abc import ABC
+from dataclasses import dataclass, field
+
+from bisq.core.common.protocol.network.network_envelope import NetworkEnvelope
+from bisq.core.network.p2p.extended_data_size_permission import ExtendedDataSizePermission
+from bisq.core.network.p2p.node_address import NodeAddress
+from bisq.core.network.p2p.senders_node_address_message import SendersNodeAddressMessage
+import proto.pb_pb2 as protobuf
+
+@dataclass(frozen=True)
+class FileTransferPart(NetworkEnvelope, ExtendedDataSizePermission, SendersNodeAddressMessage):
+    sender_node_address: NodeAddress
+    trade_id: str
+    trader_id: int
+    uid: str
+    seq_num_or_file_length: int
+    message_data: bytes = field(default=b'') # if message_data is empty it is the first message, requesting file upload permission
+
+    @property
+    def is_initial_request(self) -> bool:
+        return len(self.message_data) == 0
+
+    @staticmethod
+    def from_proto(proto: protobuf.FileTransferPart, message_version: int):
+        sender_node_address = NodeAddress.from_proto(proto.sender_node_address)
+        trade_id = proto.trade_id
+        trader_id = proto.trader_id
+        uid = proto.uid
+        seq_num_or_file_length = proto.seq_num_or_file_length
+        message_data = proto.message_data
+        return FileTransferPart(message_version=message_version,
+                                sender_node_address=sender_node_address,
+                                trade_id=trade_id, 
+                                trader_id=trader_id,
+                                uid=uid,
+                                seq_num_or_file_length=seq_num_or_file_length,
+                                message_data=message_data)
+
+    def to_proto_network_envelope(self):
+        envelope = self.get_network_envelope_builder()
+        protobuf.FileTransferPart(
+            senderNodeAddress=self.sender_node_address.to_proto_message(),
+            tradeId=self.trade_id,
+            traderId=self.trader_id,
+            uid=self.uid,
+            seqNumOrFileLength=self.seq_num_or_file_length,
+            messageData=self.message_data
+        )
+        envelope.file_transfer_part.CopyFrom()
+        return envelope
+
+    def get_sender_node_address(self) -> NodeAddress:
+        return self.sender_node_address
+
+    def __str__(self):
+        return f"FileTransferPart{{\n     senderNodeAddress='{self.sender_node_address.get_host_name_for_display()}',\n     uid='{self.uid}',\n     tradeId='{self.trade_id}',\n     traderId='{self.trader_id}',\n     seqNumOrFileLength={self.seq_num_or_file_length}\n}} {super().__str__()}"
