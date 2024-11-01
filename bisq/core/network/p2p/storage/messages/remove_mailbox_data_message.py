dc20930f24d1fc584314beb85337164ce8913e49
thecockatiel <thecockatiel@proton.me>
2024-11-01 14:42:54 +0330

    chore: add remove mailbox data message

--- bisq/core/network/p2p/storage/messages/remove_mailbox_data_message.py
+++ bisq/core/network/p2p/storage/messages/remove_mailbox_data_message.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 
 from bisq.core.network.p2p.storage.broadcast_message import BroadcastMessage
-from bisq.core.network.p2p.storage.payload.protected_storage_entry import ProtectedStorageEntry
+from bisq.core.network.p2p.storage.payload.protected_mailbox_storage_entry import ProtectedMailboxStorageEntry
 import bisq.core.common.version as Version
 import proto.pb_pb2 as protobuf
 
@@ -11,21 +11,21 @@ if TYPE_CHECKING:
     from bisq.core.common.protocol.network.network_proto_resolver import NetworkProtoResolver    
 
 @dataclass(frozen=True)
-class RemoveDataMessage(BroadcastMessage):
-    protected_storage_entry: ProtectedStorageEntry
-    
-    def __init__(self, protected_storage_entry: ProtectedStorageEntry, message_version: int = Version.get_p2p_message_version()):
+class RemoveMailboxDataMessage(BroadcastMessage):
+    protected_storage_entry: ProtectedMailboxStorageEntry
+
+    def __init__(self, protected_storage_entry: ProtectedMailboxStorageEntry, message_version: int = Version.get_p2p_message_version()):
         super().__init__(message_version)
         self.protected_storage_entry = protected_storage_entry
 
     # PROTO BUFFER
     def to_proto_network_envelope(self):
         envelope = self.get_network_envelope_builder()
-        envelope.remove_data_message = protobuf.RemoveDataMessage(
+        envelope.remove_mailbox_data_message = protobuf.RemoveMailboxDataMessage(
             protected_storage_entry = self.protected_storage_entry.to_proto_message()
         )
         return envelope
 
     @staticmethod
-    def from_proto(proto: 'protobuf.RemoveDataMessage', resolver: 'NetworkProtoResolver', message_version: int):
-        return RemoveDataMessage(ProtectedStorageEntry.from_proto(proto.protected_storage_entry, resolver), message_version)
+    def from_proto(proto: 'protobuf.RemoveMailboxDataMessage', resolver: 'NetworkProtoResolver', message_version: int):
+        return RemoveMailboxDataMessage(ProtectedMailboxStorageEntry.from_proto(proto.protected_storage_entry, resolver), message_version)
