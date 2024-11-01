b23f2b62b063eab72f097004965e4e3c3b352f4a
thecockatiel <thecockatiel@proton.me>
2024-11-01 18:38:20 +0330

    chore: add minimal config for DefaultSeedNodeRepository

--- bisq/core/common/config/config.py
+++ bisq/core/common/config/config.py
@@ -1,1 +1,12 @@
-config = None 
+# TODO: THIS CLASS IS INCOMPLETE. made to work with DefaultSeedNodeRepository
+
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Config:
+    def __init__(self):
+        self.seed_nodes = []
+        self.filter_provided_seed_nodes = []
+        self.banned_seed_nodes = []
+
+config = None
