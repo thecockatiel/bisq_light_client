d91f020d0bae1097b8ea07f2ea678b4a6b703b49
thecockatiel <thecockatiel@proton.me>
2024-11-01 14:01:09 +0330

    fix: add data message

--- bisq/core/network/p2p/storage/messages/add_data_message.py
+++ bisq/core/network/p2p/storage/messages/add_data_message.py
@@ -12,18 +12,16 @@ import bisq.core.common.version as Version
 class AddDataMessage(BroadcastMessage):
     protected_storage_entry: ProtectedStorageEntry
 
-    def __init__(self, protected_storage_entry: ProtectedStorageEntry, message_version: int = None):
-        if message_version is None:
-            message_version = Version.get_p2p_message_version()
+    def __init__(self, protected_storage_entry: ProtectedStorageEntry, message_version: int = Version.get_p2p_message_version()):
         super().__init__(message_version)
         self.protected_storage_entry = protected_storage_entry
 
     # PROTO BUFFER
 
-    @classmethod
-    def from_proto(cls, proto: protobuf.AddDataMessage, resolver: NetworkProtoResolver, message_version: int) -> 'AddDataMessage':
+    @staticmethod
+    def from_proto(proto: protobuf.AddDataMessage, resolver: NetworkProtoResolver, message_version: int) -> 'AddDataMessage':
         protected_storage_entry = cast(ProtectedStorageEntry, resolver.from_proto_storage_entry_wrapper(proto.entry))
-        return cls(protected_storage_entry, message_version)
+        return AddDataMessage(protected_storage_entry, message_version)
 
     def to_proto_network_envelope(self) -> protobuf.NetworkEnvelope:
         entry = protobuf.StorageEntryWrapper()
