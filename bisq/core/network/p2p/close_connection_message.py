51d0b1f9ac1eeb5c781f451c15d7afe8cedb978c
thecockatiel <thecockatiel@proton.me>
2024-11-01 14:00:35 +0330

    fix: close connection message

--- bisq/core/network/p2p/close_connection_message.py
+++ bisq/core/network/p2p/close_connection_message.py
@@ -2,16 +2,15 @@ from dataclasses import dataclass
 
 from bisq.core.common.protocol.network.network_envelope import NetworkEnvelope
 import proto.pb_pb2 as protobuf
+import bisq.core.common.version as Version
 
 @dataclass(frozen=True)
 class CloseConnectionMessage(NetworkEnvelope):
     reason: str
 
-    def __init__(self, reason: str, message_version: int = None):
-        if message_version is None:
-            message_version = Version.get_p2p_message_version()
+    def __init__(self, reason: str, message_version: int = Version.get_p2p_message_version()): 
         super().__init__(message_version)
-        object.__setattr__(self, 'reason', reason)
+        self.reason = reason
 
     # PROTO BUFFER
 
@@ -20,6 +19,6 @@ class CloseConnectionMessage(NetworkEnvelope):
         envelope.close_connection_message = protobuf.CloseConnectionMessage(reason=self.reason)
         return envelope
 
-    @classmethod
-    def from_proto(cls, proto: protobuf.CloseConnectionMessage, message_version: int) -> 'CloseConnectionMessage':
-        return cls(proto.reason, message_version)
+    @staticmethod
+    def from_proto(proto: protobuf.CloseConnectionMessage, message_version: int) -> 'CloseConnectionMessage':
+        return CloseConnectionMessage(proto.reason, message_version)
