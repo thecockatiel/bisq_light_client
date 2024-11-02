4203d6549c84624dd34cc18382c03fff790a43e1
thecockatiel <thecockatiel@proton.me>
2024-11-02 12:32:57 +0330

    chore: update sig and add tests

--- bisq/core/common/crypto/sig.py
+++ bisq/core/common/crypto/sig.py
@@ -1,6 +1,7 @@
 import base64
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.backends import default_backend
 from cryptography.exceptions import InvalidSignature
 
 from bisq.core.common.crypto.crypto_exception import CryptoException
@@ -16,7 +17,7 @@ class Sig:
     @staticmethod
     def generate_key_pair():
         try:
-            private_key = dsa.generate_private_key(key_size=1024)
+            private_key = dsa.generate_private_key(key_size=1024, backend=default_backend())
             public_key = private_key.public_key()
             return (private_key, public_key)
         except ValueError as e:
@@ -76,7 +77,16 @@ class Sig:
             raise KeyConversionException(e) from e
 
     @staticmethod
-    def get_public_key_bytes(sig_public_key: dsa.DSAPublicKey):
+    def get_private_key_from_bytes(sig_private_key_bytes: bytes):
+        try:
+            private_key = serialization.load_der_private_key(sig_private_key_bytes, password=None)
+            return private_key
+        except Exception as e:
+            logger.error(f"Error creating sigPublicKey from bytes. sigPublicKeyBytes as hex=REDACTED, error={e}", exc_info=True)
+            raise KeyConversionException(e) from e
+
+    @staticmethod
+    def get_public_key_bytes(sig_public_key: dsa.DSAPublicKey) -> bytes:
         try:
             return sig_public_key.public_bytes(
                 encoding=serialization.Encoding.DER,
@@ -87,6 +97,18 @@ class Sig:
             raise KeyConversionException(e) from e
 
     @staticmethod
+    def get_private_key_bytes(sig_private_key: dsa.DSAPrivateKey) -> bytes:
+        try:
+            return sig_private_key.private_bytes(
+                encoding=serialization.Encoding.DER,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            )
+        except Exception as e:
+            logger.error("Error encoding private key to bytes.", exc_info=True)
+            raise KeyConversionException(e) from e
+
+    @staticmethod
     def get_public_key_bytes_as_hex_string(sig_public_key: dsa.DSAPublicKey, allow_none: bool = False):
         if not sig_public_key:
             if not allow_none:
@@ -94,3 +116,12 @@ class Sig:
             return 'null'
         
         return Sig.get_public_key_bytes(sig_public_key).hex()
+
+    @staticmethod
+    def get_private_key_bytes_as_hex_string(sig_private_key: dsa.DSAPrivateKey, allow_none: bool = False):
+        if not sig_private_key:
+            if not allow_none:
+                raise KeyConversionException("Private key is None.")
+            return 'null'
+        
+        return Sig.get_private_key_bytes(sig_private_key).hex()
---
+++ tests/sig_test.py
@@ -0,0 +1,28 @@
+import base64
+import unittest
+
+from bisq.core.common.crypto.sig import Sig
+
+class TestEncryption(unittest.TestCase):
+    def setUp(self):
+        # These hex examples come from running the java codes from Encryption class on bisq/core/common/crypto/encryption.java
+        self.public_hex = "308201b73082012c06072a8648ce3804013082011f02818100fd7f53811d75122952df4a9c2eece4e7f611b7523cef4400c31e3f80b6512669455d402251fb593d8d58fabfc5f5ba30f6cb9b556cd7813b801d346ff26660b76b9950a5a49f9fe8047b1022c24fbba9d7feb7c61bf83b57e7c6a8a6150f04fb83f6d3c51ec3023554135a169132f675f3ae2b61d72aeff22203199dd14801c70215009760508f15230bccb292b982a2eb840bf0581cf502818100f7e1a085d69b3ddecbbcab5c36b857b97994afbbfa3aea82f9574c0b3d0782675159578ebad4594fe67107108180b449167123e84c281613b7cf09328cc8a6e13c167a8b547c8d28e0a3ae1e2bb3a675916ea37f0bfa213562f1fb627a01243bcca4f1bea8519089a883dfe15ae59f06928b665e807b552564014c3bfecf492a03818400028180271aa0e3a61563971ba3e76ade5f7c3275f71ba662f6a6cddd53016d983989179b75dd13c5cb8a7ac8950dc2496470e3b43848f2a5b8dd7989097c71296bf0934af7a7a2f1d4428db5bd14a8e27c59792a965a1b0c430d1f06243e78e60b227f856215dcfa3c273c605a7afd53cc20f02ec861b5397639be12a43f0b3a7cb2ff"
+        self.private_hex = "3082014b0201003082012c06072a8648ce3804013082011f02818100fd7f53811d75122952df4a9c2eece4e7f611b7523cef4400c31e3f80b6512669455d402251fb593d8d58fabfc5f5ba30f6cb9b556cd7813b801d346ff26660b76b9950a5a49f9fe8047b1022c24fbba9d7feb7c61bf83b57e7c6a8a6150f04fb83f6d3c51ec3023554135a169132f675f3ae2b61d72aeff22203199dd14801c70215009760508f15230bccb292b982a2eb840bf0581cf502818100f7e1a085d69b3ddecbbcab5c36b857b97994afbbfa3aea82f9574c0b3d0782675159578ebad4594fe67107108180b449167123e84c281613b7cf09328cc8a6e13c167a8b547c8d28e0a3ae1e2bb3a675916ea37f0bfa213562f1fb627a01243bcca4f1bea8519089a883dfe15ae59f06928b665e807b552564014c3bfecf492a041602143aa9d8a87d70434f07a85d3bb367363f91758fc6"
+        self.text_message = "TestIsGood"
+        self.text_message_signature_hex = "302c02142fd9d8d8894453ff3d279798e1f9340e5e05331c021451734d432c7c2b1bef21f5c52dbedec97c4c67e7"
+
+    def test_encrypt_decrypt_payload(self):
+        privkey = Sig.get_private_key_from_bytes(bytes.fromhex(self.private_hex))
+        pubkey = Sig.get_public_key_from_bytes(bytes.fromhex(self.public_hex))
+        signature = Sig.sign(privkey, self.text_message.encode())
+        self.assertTrue(Sig.verify(pubkey, self.text_message.encode(), signature))
+    
+    def test_encrypt_decrypt_payload_with_provided_key(self):
+        privkey = Sig.get_private_key_from_bytes(bytes.fromhex(self.private_hex))
+        pubkey = Sig.get_public_key_from_bytes(bytes.fromhex(self.public_hex))
+        signature = Sig.sign_message(privkey, self.text_message)
+        self.assertTrue(Sig.verify_message(pubkey, self.text_message, signature))
+        self.assertTrue(Sig.verify_message(pubkey, self.text_message, base64.b64encode(bytes.fromhex(self.text_message_signature_hex)).decode('utf-8')))
+
+if __name__ == '__main__':
+    unittest.main()
