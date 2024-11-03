10cc6c5f7e20f55f48394771146bef1c224bdba5
thecockatiel <thecockatiel@proton.me>
2024-11-03 11:36:29 +0330

    chore: improve sealed_and_signed

--- bisq/core/common/crypto/sealed_and_signed.py
+++ bisq/core/common/crypto/sealed_and_signed.py
@@ -5,22 +5,26 @@ from bisq.core.common.protocol.network.network_payload import NetworkPayload
 from proto import pb_pb2 as protobuf
 from .sig import Sig, dsa  # Replace with actual module imports
 
-
-@dataclass(frozen=True)
 class SealedAndSigned(NetworkPayload):
     encrypted_secret_key: bytes
     encrypted_payload_with_hmac: bytes
     signature: bytes
-    sig_public_key: Optional['dsa.DSAPublicKey'] = field(default=None)
-    sig_public_key_bytes: Optional[bytes] = field(default=None)
+    sig_public_key: Optional['dsa.DSAPublicKey'] = None
+    sig_public_key_bytes: Optional[bytes] = None
 
-    def __post_init__(self):
-        if self.sig_public_key is not None:
-            object.__setattr__(self, 'sig_public_key_bytes', Sig.get_public_key_bytes(self.sig_public_key))
-        elif self.sig_public_key_bytes is not None:
-            object.__setattr__(self, 'sig_public_key', Sig.get_public_key_from_bytes(self.sig_public_key_bytes))
+    def __init__(self, encrypted_secret_key: bytes, encrypted_payload_with_hmac: bytes, signature: bytes, sig_public_key: Optional['dsa.DSAPublicKey'] = None, sig_public_key_bytes: Optional[bytes] = None):
+        super().__init__()
+        if sig_public_key is not None:
+            self.sig_public_key_bytes = Sig.get_public_key_bytes(sig_public_key)
+            self.sig_public_key = sig_public_key
+        elif sig_public_key_bytes is not None:
+            self.sig_public_key = Sig.get_public_key_from_bytes(sig_public_key_bytes)
+            self.sig_public_key_bytes = sig_public_key_bytes
         else:
             raise ValueError("Either sig_public_key or sig_public_key_bytes must be provided.")
+        self.encrypted_secret_key = encrypted_secret_key
+        self.encrypted_payload_with_hmac = encrypted_payload_with_hmac
+        self.signature = signature
 
     @staticmethod
     def from_proto(proto: protobuf.SealedAndSigned) -> 'SealedAndSigned':
@@ -38,3 +42,9 @@ class SealedAndSigned(NetworkPayload):
             signature=self.signature,
             sig_public_key_bytes=self.sig_public_key_bytes
         )
+
+    def __eq__(self, other):
+        return isinstance(other, SealedAndSigned) and self.encrypted_secret_key == other.encrypted_secret_key and self.encrypted_payload_with_hmac == other.encrypted_payload_with_hmac and self.signature == other.signature and self.sig_public_key_bytes == other.sig_public_key_bytes
+    
+    def __hash__(self) -> int:
+        return hash((self.encrypted_secret_key, self.encrypted_payload_with_hmac, self.signature, self.sig_public_key_bytes))
