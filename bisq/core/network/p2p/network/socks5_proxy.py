c4514d464605fac91ed45137930ea62687e9f9a6
thecockatiel <thecockatiel@proton.me>
2024-11-05 11:01:29 +0330

    chore: rename fields to be simpler

--- bisq/core/network/p2p/network/socks5_proxy.py
+++ bisq/core/network/p2p/network/socks5_proxy.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class Socks5Proxy:
-    proxy_host: str
-    proxy_port: int
+    host: str
+    port: int
     username: str = field(default=None)
     password: str = field(default=None)
 
     @property
     def url(self) -> str:
         if self.username is not None and self.password is not None:
-            return f"socks5://{self.username}:{self.password}@{self.proxy_host}:{self.proxy_port}"
-        return f"socks5://{self.proxy_host}:{self.proxy_port}"
+            return f"socks5://{self.username}:{self.password}@{self.host}:{self.port}"
+        return f"socks5://{self.host}:{self.port}"
