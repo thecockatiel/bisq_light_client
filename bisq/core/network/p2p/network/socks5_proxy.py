bd324c5d61ea18e40192eb466d6888778e6624d7
thecockatiel <thecockatiel@proton.me>
2024-11-05 12:16:54 +0330

    chore: add some funcs to socks5_proxy

--- bisq/core/network/p2p/network/socks5_proxy.py
+++ bisq/core/network/p2p/network/socks5_proxy.py
@@ -13,3 +13,12 @@ class Socks5Proxy:
         if self.username is not None and self.password is not None:
             return f"socks5://{self.username}:{self.password}@{self.host}:{self.port}"
         return f"socks5://{self.host}:{self.port}"
+
+    def __str__(self):
+        return self.url
+    
+    def __hash__(self) -> int:
+        return hash(self.url)
+    
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Socks5Proxy) and self.url == other.url
