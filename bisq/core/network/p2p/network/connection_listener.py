e8644000a3bd00e4b80bce3e1dc5d5d4e508a4b7
thecockatiel <thecockatiel@proton.me>
2024-11-05 19:58:23 +0330

    chore: add types to connection listener

--- bisq/core/network/p2p/network/connection_listener.py
+++ bisq/core/network/p2p/network/connection_listener.py
@@ -1,11 +1,17 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from bisq.core.network.p2p.network.connection import Connection
+    from bisq.core.network.p2p.network.close_connection_reason import CloseConnectionReason
 
 class ConnectionListener(ABC):
 
     @abstractmethod
-    def on_connection(self, connection):
+    def on_connection(self, connection: 'Connection'):
         pass
 
     @abstractmethod
-    def on_disconnect(self, close_connection_reason, connection):
+    def on_disconnect(self, close_connection_reason: 'CloseConnectionReason', connection: 'Connection'):
         pass
