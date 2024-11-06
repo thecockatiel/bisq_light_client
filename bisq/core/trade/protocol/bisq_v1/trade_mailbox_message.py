52fe27a30493509ad79a3dc6c4738eb05cc9736a
thecockatiel <thecockatiel@proton.me>
2024-11-06 09:18:11 +0330

    chore: add TradeMailboxMessage

---
+++ bisq/core/trade/protocol/bisq_v1/trade_mailbox_message.py
@@ -0,0 +1,21 @@
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import timedelta
+
+from bisq.core.trade.protocol.trade_message import TradeMessage
+
+@dataclass(frozen=True, kw_only=True)
+class TradeMailboxMessage(TradeMessage, ABC):
+    # 15 days in milliseconds
+    TTL: int = field(init=False, hash=False, compare=False, repr=False, default=int(timedelta(days=15).total_seconds() * 1000)) 
+
+    def __eq__(self, other):
+        if not isinstance(other, TradeMailboxMessage):
+            return False
+        return super().__eq__(other)
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def __str__(self):
+        return f"TradeMailboxMessage(trade_id='{self.trade_id}', uid='{self.uid}', message_version={self.message_version})"
