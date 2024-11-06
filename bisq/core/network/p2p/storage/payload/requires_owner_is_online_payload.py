8bb8a0c08f728e3d0eec1c66f98b3de3a3e1d478
thecockatiel <thecockatiel@proton.me>
2024-11-06 11:33:45 +0330

    chore: add RequiresOwnerIsOnlinePayload

---
+++ bisq/core/network/p2p/storage/payload/requires_owner_is_online_payload.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from bisq.core.common.protocol.network.network_payload import NetworkPayload
+
+if TYPE_CHECKING:
+    from bisq.core.network.p2p.node_address import NodeAddress
+
+
+class RequiresOwnerIsOnlinePayload(NetworkPayload, ABC):
+    @abstractmethod
+    def get_owner_node_address(self) -> NodeAddress:
+        pass
