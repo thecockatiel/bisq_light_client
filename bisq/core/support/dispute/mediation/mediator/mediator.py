a0b1bd5a76aaabd7a61057be6284bda70b45a8bf
thecockatiel <thecockatiel@proton.me>
2024-11-07 15:20:37 +0330

    chore: add mediator

--- bisq/core/support/dispute/mediation/mediator/mediator.py
+++ bisq/core/support/dispute/mediation/mediator/mediator.py
@@ -1,20 +1,16 @@
+from dataclasses import dataclass
 from typing import List, Dict, Optional
 
 from bisq.core.common.crypto.pub_key_ring import PubKeyRing
-from bisq.core.common.crypto.sig import Sig
 from bisq.core.common.protocol.proto_util import ProtoUtil
 from bisq.core.network.p2p.node_address import NodeAddress
 from bisq.core.support.dispute.agent.dispute_agent import DisputeAgent
 import proto.pb_pb2 as protobuf
 
-
-class Arbitrator(DisputeAgent):
-
+class Mediator(DisputeAgent):
     def __init__(
         self,
         node_address: NodeAddress,
-        btc_pub_key: bytes,
-        btc_address: str,
         pub_key_ring: PubKeyRing,
         language_codes: List[str],
         registration_date: int,
@@ -36,38 +32,28 @@ class Arbitrator(DisputeAgent):
             extra_data_map,
         )
 
-        self.btc_pub_key = btc_pub_key
-        self.btc_address = btc_address
-
-    def to_proto_message(self) -> protobuf.StoragePayload:
-        arbitrator = protobuf.Arbitrator(
+    def to_proto_message(self):
+        mediator = protobuf.Mediator(
             node_address=self.node_address.to_proto_message(),
             language_codes=self.language_codes,
             registration_date=self.registration_date,
             registration_pub_key=self.registration_pub_key,
             registration_signature=self.registration_signature,
             pub_key_ring=self.pub_key_ring.to_proto_message(),
-            btc_pub_key=self.btc_pub_key,
-            btc_address=self.btc_address,
         )
-
         if self.email_address:
-            arbitrator.email_address = self.email_address
+            mediator.email_address = self.email_address
         if self.info:
-            arbitrator.info = self.info
+            mediator.info = self.info
         if self.extra_data_map:
-            arbitrator.extra_data.update(self.extra_data_map)
+            mediator.extra_data.update(self.extra_data_map)
 
-        return protobuf.StoragePayload(
-            arbitrator=arbitrator,
-        )
+        return protobuf.StoragePayload(mediator=mediator)
 
     @staticmethod
-    def from_proto(proto: protobuf.Arbitrator) -> "Arbitrator":
-        return Arbitrator(
+    def from_proto(proto: protobuf.Mediator) -> "Mediator":
+        return Mediator(
             node_address=NodeAddress.from_proto(proto.node_address),
-            btc_pub_key=proto.btc_pub_key,
-            btc_address=proto.btc_address,
             pub_key_ring=PubKeyRing.from_proto(proto.pub_key_ring),
             language_codes=list(proto.language_codes),
             registration_date=proto.registration_date,
@@ -79,22 +65,7 @@ class Arbitrator(DisputeAgent):
         )
 
     def __str__(self) -> str:
-        return (
-            f"Arbitrator("
-            f"\n     btc_pub_key={self.btc_pub_key.hex()}"
-            f",\n     btc_address='{self.btc_address}'"
-            f"\n) {super().__str__()}"
-        )
+        return f"Mediator{{{super().__str__()}}}"
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, Arbitrator) and super().__eq__(other) and (
-            self.btc_pub_key == other.btc_pub_key and
-            self.btc_address == other.btc_address
-        )
-        
-    def __hash__(self):
-        return hash((
-            super().__hash__(),
-            self.btc_pub_key,
-            self.btc_address,
-        ))
+        return isinstance(other, Mediator) and super().__eq__(other)
