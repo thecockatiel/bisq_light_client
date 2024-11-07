1418c2d326e379aab81a13052e6e9523b93bce46
thecockatiel <thecockatiel@proton.me>
2024-11-07 16:18:12 +0330

    chore: add RefundAgent

--- bisq/core/support/refund/refundagent/refund_agent.py
+++ bisq/core/support/refund/refundagent/refund_agent.py
@@ -1,12 +1,30 @@
-from typing import List, Dict, Optional
+from typing import List, Optional, Dict
 
+from bisq.core.common.capabilities import Capabilities
+from bisq.core.common.capability import Capability
 from bisq.core.common.crypto.pub_key_ring import PubKeyRing
 from bisq.core.common.protocol.proto_util import ProtoUtil
 from bisq.core.network.p2p.node_address import NodeAddress
+from bisq.core.network.p2p.storage.payload.capability_requiring_payload import (
+    CapabilityRequiringPayload,
+)
 from bisq.core.support.dispute.agent.dispute_agent import DisputeAgent
+from bisq.logging import get_logger
 import proto.pb_pb2 as protobuf
 
-class Mediator(DisputeAgent):
+logger = get_logger(__name__)
+
+class RefundAgent(DisputeAgent, CapabilityRequiringPayload):
+    node_address: NodeAddress
+    pub_key_ring: PubKeyRing
+    language_codes: List[str]
+    registration_date: int
+    registration_pub_key: bytes
+    registration_signature: str
+    email_address: Optional[str] = None
+    info: Optional[str] = None
+    extra_data_map: Optional[Dict[str, str]] = None
+
     def __init__(
         self,
         node_address: NodeAddress,
@@ -20,19 +38,19 @@ class Mediator(DisputeAgent):
         extra_data_map: Optional[Dict[str, str]] = None,
     ):
         super().__init__(
-            node_address,
-            pub_key_ring,
-            language_codes,
-            registration_date,
-            registration_pub_key,
-            registration_signature,
-            email_address,
-            info,
-            extra_data_map,
+            node_address=node_address,
+            pub_key_ring=pub_key_ring,
+            language_codes=language_codes,
+            registration_date=registration_date,
+            registration_pub_key=registration_pub_key,
+            registration_signature=registration_signature,
+            email_address=email_address,
+            info=info,
+            extra_data_map=extra_data_map,
         )
 
     def to_proto_message(self):
-        mediator = protobuf.Mediator(
+        refund_agent = protobuf.RefundAgent(
             node_address=self.node_address.to_proto_message(),
             language_codes=self.language_codes,
             registration_date=self.registration_date,
@@ -40,18 +58,19 @@ class Mediator(DisputeAgent):
             registration_signature=self.registration_signature,
             pub_key_ring=self.pub_key_ring.to_proto_message(),
         )
-        if self.email_address:
-            mediator.email_address = self.email_address
-        if self.info:
-            mediator.info = self.info
-        if self.extra_data_map:
-            mediator.extra_data.update(self.extra_data_map)
 
-        return protobuf.StoragePayload(mediator=mediator)
+        if self.email_address:
+            refund_agent.email_address = self.email_address
+        if self.info:
+            refund_agent.info = self.info
+        if self.extra_data_map:
+            refund_agent.extra_data.update(self.extra_data_map)
+
+        return protobuf.StoragePayload(refund_agent=refund_agent)
 
     @staticmethod
-    def from_proto(proto: protobuf.Mediator) -> "Mediator":
-        return Mediator(
+    def from_proto(proto: "protobuf.RefundAgent") -> "RefundAgent":
+        return RefundAgent(
             node_address=NodeAddress.from_proto(proto.node_address),
             pub_key_ring=PubKeyRing.from_proto(proto.pub_key_ring),
             language_codes=list(proto.language_codes),
@@ -63,8 +82,8 @@ class Mediator(DisputeAgent):
             extra_data_map=dict(proto.extra_data) if proto.extra_data else None,
         )
 
-    def __str__(self) -> str:
-        return f"Mediator{{{super().__str__()}}}"
+    def __str__(self):
+        return f"RefundAgent() {super().__str__()}"
 
-    def __eq__(self, other) -> bool:
-        return isinstance(other, Mediator) and super().__eq__(other)
+    def get_required_capabilities(self):
+        return Capabilities([Capability.REFUND_AGENT])
