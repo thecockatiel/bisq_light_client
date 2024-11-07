17559d14d166fd28f01ba3aa4bcc6cd5a4293039
thecockatiel <thecockatiel@proton.me>
2024-11-08 00:24:06 +0330

    chore: add PaymentAccountFilter

---
+++ bisq/core/filter/payment_account_filter.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from bisq.core.common.protocol.network.network_payload import NetworkPayload
+from bisq.logging import get_logger
+import proto.pb_pb2 as protobuf
+
+logger = get_logger(__name__)
+
+
+@dataclass(frozen=True)
+class PaymentAccountFilter(NetworkPayload):
+    payment_method_id: str
+    get_method_name: str
+    value: str
+
+    def to_proto_message(self) -> protobuf.PaymentAccountFilter:
+        return protobuf.PaymentAccountFilter(
+            payment_method_id=self.payment_method_id,
+            get_method_name=self.get_method_name,
+            value=self.value,
+        )
+
+    @staticmethod
+    def from_proto(proto: protobuf.PaymentAccountFilter):
+        return PaymentAccountFilter(
+            payment_method_id=proto.payment_method_id,
+            get_method_name=proto.get_method_name,
+            value=proto.value,
+        )
