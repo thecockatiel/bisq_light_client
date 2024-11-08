08d0da30eaf8f3a81069a57dbfa0e8ddb03cbbf6
thecockatiel <thecockatiel@proton.me>
2024-11-08 04:02:23 +0330

    chore: add DisputeMessage

---
+++ bisq/core/support/dispute/messages/dispute_message.py
@@ -0,0 +1,14 @@
+from abc import ABC
+from dataclasses import dataclass
+from datetime import timedelta
+
+from bisq.core.support.messages.support_message import SupportMessage
+from bisq.core.support.support_type import SupportType
+
+
+@dataclass(frozen=True, kw_only=True)
+class DisputeMessage(SupportMessage, ABC):
+    TTL = int(timedelta(days=15).total_seconds() * 1000)
+
+    def get_ttl(self) -> int:
+        return self.TTL
