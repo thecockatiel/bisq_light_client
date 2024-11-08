7f1d440a1cab0928534cae1879e9608e95f74d9a
thecockatiel <thecockatiel@proton.me>
2024-11-08 12:42:14 +0330

    chore: add DeviationByPercentage

---
+++ bisq/core/network/p2p/inventory/model/deviation_by_percentage.py
@@ -0,0 +1,26 @@
+
+from bisq.core.network.p2p.inventory.model.deviation_severity import DeviationSeverity
+
+
+class DeviationByPercentage:
+    
+    # In case want to see the % deviation but not trigger any warnings or alerts
+    # don't pass any values to the constructor
+    def __init__(self, 
+                 lower_alert_trigger: float = 0,
+                 upper_alert_trigger: float = float('inf'),
+                 lower_warn_trigger: float = 0, 
+                 upper_warn_trigger: float = float('inf')):
+        self.lower_alert_trigger = lower_alert_trigger
+        self.upper_alert_trigger = upper_alert_trigger
+        self.lower_warn_trigger = lower_warn_trigger
+        self.upper_warn_trigger = upper_warn_trigger
+
+    def get_deviation_severity(self, deviation: float) -> DeviationSeverity:
+        if deviation <= self.lower_alert_trigger or deviation >= self.upper_alert_trigger:
+            return DeviationSeverity.ALERT
+            
+        if deviation <= self.lower_warn_trigger or deviation >= self.upper_warn_trigger:
+            return DeviationSeverity.WARN
+            
+        return DeviationSeverity.OK
