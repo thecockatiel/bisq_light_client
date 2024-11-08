9d82fff9f3c22d77f031f1b3fa930e47732e6175
thecockatiel <thecockatiel@proton.me>
2024-11-08 19:00:14 +0330

    fix: unfreeze some data classes

--- bisq/core/support/messages/support_message.py
+++ bisq/core/support/messages/support_message.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
     from bisq.core.support.support_type import SupportType
 
  
-@dataclass(frozen=True, kw_only=True)
+@dataclass(kw_only=True)
 class SupportMessage(NetworkEnvelope, ABC):
     uid: str
     # Added with v1.1.6. Old clients will not have set that field and we fall back to entry 0 which is ARBITRATION.
