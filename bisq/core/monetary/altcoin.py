b27e803228c68fb96304dd78c049f6d2ed582b7f
thecockatiel <thecockatiel@proton.me>
2024-11-09 22:25:27 +0330

    chore: add formatting

--- bisq/core/monetary/altcoin.py
+++ bisq/core/monetary/altcoin.py
@@ -1,3 +1,4 @@
+from bisq.core.util.parsing_util import convert_chars_for_number
 from bitcoinj.base.utils.fiat import Fiat
 from bitcoinj.base.utils.monetary_format import (
     ALTCOIN_FRIENDLY_FORMAT,
@@ -9,3 +10,14 @@ class Altcoin(Fiat):
     SMALLEST_UNIT_EXPONENT = 8
     FRIENDLY_FORMAT = ALTCOIN_FRIENDLY_FORMAT
     PLAIN_FORMAT = ALTCOIN_PLAIN_FORMAT
+
+    @staticmethod
+    def parse_altcoin(currency_code:str, input: str):
+        cleaned = convert_chars_for_number(input)
+        return Altcoin.parse_fiat(currency_code, cleaned)
+    
+    def to_friendly_string(self) -> str:
+        return Altcoin.FRIENDLY_FORMAT.code(0, self._currency_code).format(self)
+
+    def to_plain_string(self) -> str:
+        return Altcoin.PLAIN_FORMAT.format(self)
