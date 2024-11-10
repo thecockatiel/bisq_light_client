431b97ac6e672e1ed06153cbbddf1c14cb36499b
thecockatiel <thecockatiel@proton.me>
2024-11-11 03:24:32 +0330

    chore: add RegexAddressValidator

---
+++ bisq/asset/regex_address_validator.py
@@ -0,0 +1,22 @@
+import re
+from bisq.asset.address_validation_result import AddressValidationResult
+from bisq.asset.address_validator import AddressValidator
+
+
+class RegexAddressValidator(AddressValidator):
+    """
+    Validates an Asset address against a given regular expression.
+    """
+    
+    def __init__(self, regex: str, error_msg_i18n_key: str = None):
+        super().__init__()
+        self.regex = re.compile(regex)
+        self.error_msg_i18n_key = error_msg_i18n_key
+
+    def validate(self, address: str):
+        if not re.match(self.regex, address):
+            if self.error_msg_i18n_key:
+                return AddressValidationResult.invalid_structure(self.error_msg_i18n_key)
+            else:
+                return AddressValidationResult.invalid_structure()
+        return AddressValidationResult.valid_address()
