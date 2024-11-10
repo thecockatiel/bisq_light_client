8219a96c6d062ab1139a26c4196c46ff0a8f5cd4
thecockatiel <thecockatiel@proton.me>
2024-11-11 03:16:34 +0330

    chore: add adeptio & test

---
+++ bisq/asset/coins/adeptio.py
@@ -0,0 +1,32 @@
+from bisq.asset.address_validation_result import AddressValidationResult
+from bisq.asset.base58_address_validator import Base58AddressValidator
+from bisq.asset.coin import Coin
+from bisq.asset.network_parameters_adapter import NetworkParametersAdapter
+import re
+
+_adeptio_address_regex = re.compile(r'^[A][a-km-zA-HJ-NP-Z1-9]{24,33}$')
+
+class AdeptioAddressValidator(Base58AddressValidator):
+    def __init__(self, network_parameters: NetworkParametersAdapter):
+        super().__init__(network_parameters)
+    
+    def validate(self, address: str):
+        if not re.match(_adeptio_address_regex, address):
+            return AddressValidationResult.invalid_structure()
+        return super().validate(address)
+    
+
+class Adeptio(Coin):
+    
+    class AdeptioParams(NetworkParametersAdapter):
+        def __init__(self):
+            super().__init__()
+            self.address_header = 23
+            self.p2sh_header = 16
+    
+    def __init__(self):
+        super().__init__(
+            name="Adeptio",
+            ticker_symbol="ADE",
+            address_validator=AdeptioAddressValidator(self.AdeptioParams()),
+        )
