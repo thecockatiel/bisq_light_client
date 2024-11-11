c1da1536eb088bd6bf918088be1e0fa7788d0db4
thecockatiel <thecockatiel@proton.me>
2024-11-11 17:58:55 +0330

    chore: remove pass

--- bisq/asset/coins/__init__.py
+++ bisq/asset/coins/__init__.py
@@ -8,4 +8,3 @@ for module_name in ASSET_COINS:
     module = importlib.import_module(f"bisq.asset.coins.{module_name}")
     setattr(COINS, ASSET_COINS[module_name], module.__dict__[ASSET_COINS[module_name]])
 
-pass
