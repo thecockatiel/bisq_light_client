18e321d7c51959baba4eda9233681a2e4480cb86
thecockatiel <thecockatiel@proton.me>
2024-11-13 02:52:06 +0330

    chore: add CorruptedStorageFileHandler

---
+++ bisq/core/common/file/corrupted_storage_file_handler.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+
+from bisq.logging import get_logger
+
+logger = get_logger(__name__)
+
+
+@dataclass
+class CorruptedStorageFileHandler:
+    _files: list[str] = field(default_factory=list)
+
+    def add_file(self, file_name: str):
+        self._files.append(file_name)
+
+    def get_files(self):
+        if not self._files:
+            return None
+
+        if len(self._files) == 1 and self._files[0] == "ViewPathAsString":
+            logger.debug(
+                "We detected incompatible data base file for Navigation. "
+                + "That is a minor issue happening with refactoring of UI classes "
+                + "and we don't display a warning popup to the user."
+            )
+            return None
+
+        return self._files
