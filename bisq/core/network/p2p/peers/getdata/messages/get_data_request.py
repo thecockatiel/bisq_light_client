ad4ba847905a5119f3f1066f31e5f2007890208e
thecockatiel <thecockatiel@proton.me>
2024-11-14 22:34:23 +0330

    chore: Version.VERSION as version field default in get_data_request

--- bisq/core/network/p2p/peers/getdata/messages/get_data_request.py
+++ bisq/core/network/p2p/peers/getdata/messages/get_data_request.py
@@ -4,6 +4,7 @@ from typing import Set, Optional
 from bisq.core.common.protocol.network.network_envelope import NetworkEnvelope
 from bisq.core.network.p2p.extended_data_size_permission import ExtendedDataSizePermission
 from bisq.core.network.p2p.initial_data_request import InitialDataRequest
+import bisq.core.common.version as Version
 
 @dataclass(kw_only=True)
 class GetDataRequest(NetworkEnvelope, ExtendedDataSizePermission, InitialDataRequest):
@@ -12,4 +13,4 @@ class GetDataRequest(NetworkEnvelope, ExtendedDataSizePermission, InitialDataReq
     excluded_keys: Set[bytes]
     # Added at v1.4.0
     # The version of the requester. Used for response to send potentially missing historical data
-    version: Optional[str] = field(default=None)
+    version: Optional[str] = field(default=Version.VERSION)
