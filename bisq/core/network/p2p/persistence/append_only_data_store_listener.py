69f71bdd94a597be322da4a62667b792e321062a
thecockatiel <thecockatiel@proton.me>
2024-11-14 18:05:41 +0330

    chore: add AppendOnlyDataStoreListener

---
+++ bisq/core/network/p2p/persistence/append_only_data_store_listener.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bisq.core.network.p2p.storage.payload.persistable_network_payload import PersistableNetworkPayload
+
+class AppendOnlyDataStoreListener(ABC):
+    @abstractmethod
+    def on_added(self, payload: "PersistableNetworkPayload"):
+        pass
