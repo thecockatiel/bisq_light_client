88e6cc3ca3a2f4759aa21f41edc9648010ebe877
thecockatiel <thecockatiel@proton.me>
2024-11-14 22:33:37 +0330

    chore: add DateSortedTruncatablePayload

---
+++ bisq/core/network/p2p/storage/payload/data_sorted_truncatable_payload.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Protocol
+
+from bisq.core.network.p2p.storage.payload.persistable_network_payload import PersistableNetworkPayload
+
+class DateSortedTruncatablePayload(PersistableNetworkPayload, Protocol, ABC):
+    """
+    Marker interface for PersistableNetworkPayloads which get truncated at initial data response in case we exceed
+    the max items defined for that type of object. The truncation happens on a sorted list where we use the date for
+    sorting so in case of truncation we prefer to receive the most recent data.
+    """
+    
+    @abstractmethod
+    def get_date(self) -> datetime:
+        pass
+
+    @abstractmethod
+    def max_items(self) -> int:
+        pass
