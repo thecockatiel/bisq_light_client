500c5839c401a96e9ae5622303bd804563930e9c
thecockatiel <thecockatiel@proton.me>
2024-11-16 09:05:45 +0330

    fix: use ThreadSafeSet in master timer

--- bisq/core/common/master_timer.py
+++ bisq/core/common/master_timer.py
@@ -3,25 +3,25 @@ import logging
 import time
 from collections.abc import Callable
 
+from utils.concurrency import ThreadSafeSet
+
 # Runs all listener objects periodically in a short interval.
 class MasterTimer:
     log = logging.getLogger('MasterTimer')
     FRAME_INTERVAL_MS = 100  # frame rate of 60 fps is about 16 ms but we don't need such a short interval, 100 ms should be good enough
 
-    listeners: set[Callable[[], None]] = set()
-    listeners_lock = threading.Lock()
+    listeners: ThreadSafeSet[Callable[[], None]] = ThreadSafeSet()
 
     @staticmethod
     def start_timer():
         def run_timer():
             while True:
                 MasterTimer.log.debug("Executing listeners")
-                with MasterTimer.listeners_lock:
-                    for callable in MasterTimer.listeners:
-                        try:
-                            callable()
-                        except Exception as e:
-                            MasterTimer.log.error(f"Error executing listener: {e}")
+                for callable in MasterTimer.listeners:
+                    try:
+                        callable()
+                    except Exception as e:
+                        MasterTimer.log.error(f"Error executing listener: {e}")
                 time.sleep(MasterTimer.FRAME_INTERVAL_MS / 1000.0)
 
         timer_thread = threading.Thread(target=run_timer, daemon=True)
@@ -29,15 +29,13 @@ class MasterTimer:
 
     @staticmethod
     def add_listener(runnable: Callable[[], None]):
-        with MasterTimer.listeners_lock:
-            MasterTimer.listeners.add(runnable)
-            MasterTimer.log.debug(f"Listener added: {runnable}")
+        MasterTimer.listeners.add(runnable)
+        MasterTimer.log.debug(f"Listener added: {runnable}")
 
     @staticmethod
     def remove_listener(runnable: Callable[[], None]):
-        with MasterTimer.listeners_lock:
-            MasterTimer.listeners.discard(runnable)
-            MasterTimer.log.debug(f"Listener removed: {runnable}")
+        MasterTimer.listeners.discard(runnable)
+        MasterTimer.log.debug(f"Listener removed: {runnable}")
 
 # Initialize and start the timer
 MasterTimer.start_timer()
