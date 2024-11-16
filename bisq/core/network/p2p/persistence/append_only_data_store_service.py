493c9178615b4b85a88a6f98d96697c7b8764d75
thecockatiel <thecockatiel@proton.me>
2024-11-16 11:33:27 +0330

    fix: use AtomicInt in AppendOnlyDataStoreService

--- bisq/core/network/p2p/persistence/append_only_data_store_service.py
+++ bisq/core/network/p2p/persistence/append_only_data_store_service.py
@@ -5,6 +5,7 @@ from bisq.core.network.p2p.persistence.historical_data_store_service import Hist
 from bisq.core.network.p2p.persistence.persistable_network_payload_store import PersistableNetworkPayloadStore
 from bisq.core.network.p2p.storage.payload.persistable_network_payload import PersistableNetworkPayload
 from bisq.core.network.p2p.storage.storage_byte_array import StorageByteArray
+from utils.concurrency import AtomicInt
 
 if TYPE_CHECKING:
     from bisq.core.network.p2p.persistence.map_store_service import MapStoreService
@@ -23,11 +24,11 @@ class AppendOnlyDataStoreService():
         if not self.services:
             complete_handler()
             return
-        remaining = len(self.services)
+        remaining = AtomicInt(len(self.services))
         def _complete_handler():
             nonlocal remaining
-            remaining -= 1
-            if remaining == 0:
+            remaining.decrement_and_get()
+            if remaining.get() == 0:
                 complete_handler()
             
         for service in self.services:
