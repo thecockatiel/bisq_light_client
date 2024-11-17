00e8c7d2812ed28d49dc727f7470d78e38fcb5f4
thecockatiel <thecockatiel@proton.me>
2024-11-17 07:38:06 +0330

    chore: add profiler

---
+++ bisq/common/util/profiler.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+import threading
+import psutil
+
+from bisq.common.setup.log_setup import get_logger
+from bisq.common.user_thread import UserThread
+from utils.formatting import readable_file_size
+
+logger = get_logger(__name__)
+
+
+class Profiler:
+    @staticmethod
+    def print_system_load_periodically(delay: timedelta):
+        UserThread.run_periodically(Profiler.print_system_load, delay)
+
+    @staticmethod
+    def print_system_load():
+        process = psutil.Process()
+        virtual_memory = psutil.virtual_memory()
+        total = virtual_memory.total
+        free = virtual_memory.available
+        used = process.memory_info().rss
+
+        logger.info(
+            f"Total memory: {readable_file_size(total)}; Used memory by process: {readable_file_size(used)}; "
+            f"Free memory: {readable_file_size(free)}; Max memory: {readable_file_size(total)}; "
+            f"No. of threads: {threading.active_count()}"
+        )
+
+    @staticmethod
+    def get_used_memory_in_mb():
+        return Profiler.get_used_memory_in_bytes() / 1024 / 1024
+
+    @staticmethod
+    def get_used_memory_in_bytes():
+        process = psutil.Process()
+        return process.memory_info().rss
+
+    @staticmethod
+    def get_free_memory_in_mb():
+        return psutil.virtual_memory().available / 1024 / 1024
+
+    @staticmethod
+    def get_total_memory_in_mb():
+        return psutil.virtual_memory().total / 1024 / 1024
