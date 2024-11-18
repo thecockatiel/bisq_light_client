ad61d86a8476bf48cb34818706b98dc728de7a63
thecockatiel <thecockatiel@proton.me>
2024-11-18 08:50:53 +0330

    chore: add MarketPrice

---
+++ bisq/core/provider/price/market_price.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from time import time
+
+@dataclass
+class MarketPrice:
+    MARKET_PRICE_MAX_AGE_SEC = 1800  # 30 min
+
+    currency_code: str
+    price: float
+    timestamp_sec: int
+    is_externally_provided_price: bool
+
+    def is_price_available(self) -> bool:
+        return self.price > 0
+
+    def is_recent_price_available(self) -> bool:
+        return self.is_price_available() and self.timestamp_sec > (int(time()) - MarketPrice.MARKET_PRICE_MAX_AGE_SEC)
+
+    def is_recent_external_price_available(self) -> bool:
+        return self.is_externally_provided_price and self.is_recent_price_available()
