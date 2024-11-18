7649b0d6bdeefb12c3bc2107673144c7993055df
thecockatiel <thecockatiel@proton.me>
2024-11-18 08:46:26 +0330

    chore: add HttpClientProvider

---
+++ bisq/core/provider/http_client_provider.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from bisq.common.setup.log_setup import get_logger
+from bisq.core.network.http.http_client import HttpClient
+
+logger = get_logger(__name__)
+
+class HttpClientProvider(ABC):
+    def __init__(self, http_client: HttpClient, base_url: str, ignore_socks5_proxy=False):
+        self.http_client = http_client
+        logger.debug(f"{self.__class__.__name__} with baseUrl {base_url}")
+        http_client.base_url = base_url
+        http_client.ignore_socks5_proxy = ignore_socks5_proxy
+
+    def __str__(self):
+        return f"HttpClientProvider{{\n     httpClient={self.http_client}\n}}"
