b3e9a4a2f28fb16e357ee565832b223b9df6bbe9
thecockatiel <thecockatiel@proton.me>
2024-11-20 00:49:03 +0330

    chore: move test

--- tests/project_utils/data_test.py
+++ tests/project_utils/data_test.py
@@ -1,51 +1,136 @@
 import unittest
-from utils.data import SimpleProperty, SimplePropertyChangeEvent
+
+from utils.data import SimpleProperty, combine_simple_properties
 
 class TestSimpleProperty(unittest.TestCase):
     def setUp(self):
-        self.int_prop = SimpleProperty[int](42)
-        self.str_prop = SimpleProperty[str]("test")
-
-    def test_initialization(self):
-        self.assertEqual(self.int_prop.get(), 42)
-        self.assertEqual(self.str_prop.get(), "test")
-
-    def test_set_and_get(self):
-        self.str_prop.set("updated")
-        self.assertEqual(self.str_prop.get(), "updated")
+        self.property = SimpleProperty[int](42)
+        self.changes = []
+    
+    def test_initial_value(self):
+        self.assertEqual(self.property.get(), 42)
+        self.assertEqual(self.property.value, 42)
+    
+    def test_set_value(self):
+        self.property.set(100)
+        self.assertEqual(self.property.get(), 100)
         
-        self.int_prop.set(100)
-        self.assertEqual(self.int_prop.get(), 100)
+        # Test property decorator syntax
+        self.property.value = 200
+        self.assertEqual(self.property.value, 200)
+    
+    def test_listeners(self):
+        def listener(event):
+            self.changes.append((event.old_value, event.new_value))
+        
+        self.property.add_listener(listener)
+        self.property.set(1)
+        self.property.set(2)
+        
+        self.assertEqual(self.changes, [(42, 1), (1, 2)])
+        
+        self.property.remove_listener(listener)
+        self.property.set(3)
+        
+        self.assertEqual(len(self.changes), 2)
+    
+    def test_no_notification_on_same_value(self):
+        self.property.add_listener(lambda e: self.changes.append((e.old_value, e.new_value)))
+        self.property.set(42)  # Same as initial value
+        
+        self.assertEqual(len(self.changes), 0)
 
-    def test_property_decorator(self):
-        self.assertEqual(self.str_prop.value, "test")
-        self.str_prop.value = "changed"
-        self.assertEqual(self.str_prop.value, "changed")
+    def test_remove_all_listeners(self):
+        def listener1(event):
+            self.changes.append(1)
+        def listener2(event):
+            self.changes.append(2)
+        
+        self.property.add_listener(listener1)
+        self.property.add_listener(listener2)
+        
+        self.property.set(50)
+        self.assertEqual(len(self.changes), 2)
+        
+        self.property.remove_all_listeners()
+        self.property.set(60)
+        
+        # No new changes should be recorded
+        self.assertEqual(len(self.changes), 2)
+        self.assertEqual(len(self.property._listeners), 0)
 
-    def test_listener(self):
-        events = []
-        def listener(event: SimplePropertyChangeEvent[int]):
-            events.append(event)
+class TestCombineSimpleProperties(unittest.TestCase):
+    def setUp(self):
+        self.prop1 = SimpleProperty[int](1)
+        self.prop2 = SimpleProperty[str]("test")
+        
+    def test_combine_properties(self):
+        def transform(values):
+            return f"{values[0]}:{values[1]}"
+        
+        results = []
+        
+        combined = combine_simple_properties(self.prop1, self.prop2, transform=transform)
+        combined.add_listener(lambda e: results.append(e.new_value))
+        
+        self.assertIsNone(combined.value)
+        
+        self.prop1.set(62)
+        self.prop1.set(42)
+        self.assertEqual(results, [])
+        
+        self.prop2.set("hello")
+        
+        self.assertEqual(results, ["42:hello"])
+        
+        self.prop1.set(100)
+        self.assertEqual(results, ["42:hello", "100:hello"])
+    
+    def test_cleanup(self): 
+        prop1 = SimpleProperty(1)
+        prop2 = SimpleProperty(2)
+        
+        combined = combine_simple_properties(prop1, prop2, transform=lambda x: sum(x))
+        
+        
+        self.assertEqual(len(prop1._listeners), 0)
+        self.assertEqual(len(prop2._listeners), 0)
+        self.assertEqual(len(combined._listeners), 0)
+        
+        def on_sum(x: int):
+            pass
+        def on_another_sum(x: int):
+            pass
+        combined.add_listener(on_sum)
+        
+        self.assertEqual(len(prop1._listeners), 1)
+        self.assertEqual(len(prop2._listeners), 1)
+        self.assertEqual(len(combined._listeners), 1)
+        
 
-        self.int_prop.add_listener(listener)
-        self.int_prop.set(99)
+        combined.add_listener(on_sum)
+        self.assertEqual(len(prop1._listeners), 1)
+        self.assertEqual(len(prop2._listeners), 1)
+        self.assertEqual(len(combined._listeners), 1)
+        
 
-        self.assertEqual(len(events), 1)
-        self.assertEqual(events[0].old_value, 42)
-        self.assertEqual(events[0].new_value, 99)
-
-        self.int_prop.remove_listener(listener)
-        self.int_prop.set(100)
-        self.assertEqual(len(events), 1)  # No new events after removal
-
-    def test_no_event_on_same_value(self):
-        events = []
-        def listener(event: SimplePropertyChangeEvent[int]):
-            events.append(event)
-
-        self.int_prop.add_listener(listener)
-        self.int_prop.set(42)  # Same value as initial
-        self.assertEqual(len(events), 0)
+        combined.add_listener(on_another_sum)
+        self.assertEqual(len(prop1._listeners), 1)
+        self.assertEqual(len(prop2._listeners), 1)
+        self.assertEqual(len(combined._listeners), 2)
+        
+        # Remove listeners
+        combined.remove_listener(on_sum)
+        self.assertEqual(len(combined._listeners), 1)
+        self.assertEqual(len(prop1._listeners), 1)
+        self.assertEqual(len(prop2._listeners), 1)
+        
+        # Remove listeners
+        combined.remove_listener(on_another_sum)
+        self.assertEqual(len(combined._listeners), 0)
+        self.assertEqual(len(prop1._listeners), 0)
+        self.assertEqual(len(prop2._listeners), 0)
+        
 
 if __name__ == '__main__':
     unittest.main()
