61168d029a86b3cb0a64e8b273b5435284ced4d2
thecockatiel <thecockatiel@proton.me>
2024-11-20 00:04:26 +0330

    chore: add uid to UidMessage for type

--- bisq/core/network/p2p/uid_message.py
+++ bisq/core/network/p2p/uid_message.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 class UidMessage(ABC):
+    uid: str
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not hasattr(self, 'uid'):
