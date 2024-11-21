e3ed6a67df5992ae572738f32502b8c2c0fd3832
thecockatiel <thecockatiel@proton.me>
2024-11-21 06:54:21 +0330

    chore: add Cookie and CookieKey

---
+++ bisq/core/user/cookie.py
@@ -0,0 +1,35 @@
+from typing import Optional, Dict 
+from bisq.common.protocol.proto_util import ProtoUtil
+from bisq.core.user.cookie_key import CookieKey
+
+class Cookie(dict[CookieKey, str]):
+    """
+    Serves as flexible container for persisting UI states, layout,...
+    Should not be over-used for domain specific data where type safety and data integrity is important.
+    """
+    
+    def put_as_float(self, key: CookieKey, value: float) -> None:
+        self[key] = str(value)
+        
+    def get_as_optional_float(self, key: CookieKey) -> Optional[float]:
+        try:
+            return float(self[key]) if key in self else None
+        except (ValueError, KeyError):
+            return None
+            
+    def put_as_boolean(self, key: CookieKey, value: bool) -> None:
+        self[key] = "1" if value else "0"
+        
+    def get_as_optional_boolean(self, key: CookieKey) -> Optional[bool]:
+        return self.get(key) == "1" if key in self else None
+        
+    def to_proto_message(self) -> Dict[str, str]:
+        return {key.name: value for key, value in self.items() if key is not None}
+    
+    @staticmethod
+    def from_proto(proto_map: Optional[Dict[str, str]] = None) -> 'Cookie':
+        cookie = Cookie()
+        if proto_map is not None:
+            for key, value in proto_map.items():
+                cookie[CookieKey.from_proto(key)] = value
+        return cookie
---
+++ bisq/core/user/cookie_key.py
@@ -0,0 +1,20 @@
+
+from enum import IntEnum
+
+from bisq.common.protocol.proto_util import ProtoUtil
+
+# Used for persistence of Cookie. Entries must not be changes or removed. Only adding entries is permitted.
+class CookieKey(IntEnum):
+    STAGE_X = 0
+    STAGE_Y = 1
+    STAGE_W = 2
+    STAGE_H = 3
+    TRADE_STAT_CHART_USE_USD = 4
+    CLEAN_TOR_DIR_AT_RESTART = 5
+    
+    @staticmethod
+    def from_proto(proto_value: str):
+        return ProtoUtil.enum_from_proto(CookieKey, proto_value)
+
+    def to_proto_message(self):
+        return self.name
