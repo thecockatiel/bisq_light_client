5c332d71418e25a2f8c1e6ea147cbdc16bcbcd78
thecockatiel <thecockatiel@proton.me>
2024-11-23 11:17:06 +0330

    chore: add TradeLimits

---
+++ bisq/core/dao/state/dao_state_service.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING, Union
+from bisq.core.dao.governance.param.param import Param
+from bisq.core.util.coin.bsq_formatter import BsqFormatter
+from utils.concurrency import ThreadSafeList
+
+if TYPE_CHECKING:
+    from bisq.core.dao.state.dao_state_listener import DaoStateListener
+
+# TODO: a dummy class for dao state handling for now
+
+class DaoStateService():
+    
+    def __init__(self, bsq_formatter: BsqFormatter) -> None:
+        self.dao_state_listeners: ThreadSafeList["DaoStateListener"] = ThreadSafeList()
+        self.bsq_formatter = bsq_formatter
+        
+    def add_dao_state_listener(self, listener: "DaoStateListener") -> None:
+        self.dao_state_listeners.append(listener)
+    
+    def remove_dao_state_listener(self, listener: "DaoStateListener") -> None:
+        self.dao_state_listeners.remove(listener)
+        
+    def get_chain_height(self) -> int:
+        return 0
+    
+    def get_param_value(self, param: Param, block_height: int):
+        return param.default_value
+    
+    def get_param_value_as_coin(self, param: Param, block_height_or_param_value: Union[int, str]):
+        if isinstance(block_height_or_param_value, int):
+            block_height_or_param_value = self.get_param_value(param, block_height_or_param_value)
+        return self.bsq_formatter.parse_param_value_to_coin(param, block_height_or_param_value)
