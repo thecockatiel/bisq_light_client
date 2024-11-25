14758824cb91e78f395732e67e90fa9df6ae70ec
thecockatiel <thecockatiel@proton.me>
2024-11-25 05:51:37 +0330

    chore: add eq to DecryptedDataTuple

--- bisq/core/network/crypto/decrypted_data_tuple.py
+++ bisq/core/network/crypto/decrypted_data_tuple.py
@@ -2,6 +2,8 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from bisq.common.crypto.encryption import Encryption
+
 if TYPE_CHECKING:
     from cryptography.hazmat.primitives.asymmetric import dsa
     from bisq.common.protocol.network.network_envelope import NetworkEnvelope
@@ -10,3 +12,9 @@ if TYPE_CHECKING:
 class DecryptedDataTuple:
     network_envelope: 'NetworkEnvelope'
     sig_public_key: 'dsa.DSAPublicKey'
+    
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, DecryptedDataTuple):
+            return False
+        return (self.network_envelope == value.network_envelope and
+                Encryption.is_pubkeys_equal(self.sig_public_key, value.sig_public_key))
