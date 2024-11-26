d76aad49331e4beea0502676a9f160a774cfe22c
thecockatiel <thecockatiel@proton.me>
2024-11-26 18:42:52 +0330

    chore: add java_string_hashcode util

--- utils/java_compat.py
+++ utils/java_compat.py
@@ -1,9 +1,14 @@
-
 def java_arrays_byte_hashcode(bytes_array: bytes):
     result = 1
     for b in bytes_array:
         # Convert signed byte to unsigned & match Java's byte->int conversion
         b = b & 0xFF  
         result = ((31 * result) + b) & 0xFFFFFFFF
+    return result - ((result & 0x80000000) << 1)
+
+def java_string_hashcode(s: str) -> int:
+    result = 0
+    for c in s:
+        result = ((31 * result) + ord(c)) & 0xFFFFFFFF
     return result - ((result & 0x80000000) << 1)
 
