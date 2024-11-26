69619a399e3e7a7d84649925b6978299b9e14e03
thecockatiel <thecockatiel@proton.me>
2024-11-26 15:26:05 +0330

    chore: make DecryptedDirectMessageListener callable

--- bisq/core/network/p2p/decrypted_direct_message_listener.py
+++ bisq/core/network/p2p/decrypted_direct_message_listener.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from collections.abc import Callable
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -8,10 +9,19 @@ if TYPE_CHECKING:
     from bisq.core.network.p2p.node_address import NodeAddress
 
 
-class DecryptedDirectMessageListener(ABC):
+class DecryptedDirectMessageListener(
+    Callable[["DecryptedMessageWithPubKey", "NodeAddress"], None], ABC
+):
     def on_direct_message(
         self,
         decrypted_message_with_pub_key: "DecryptedMessageWithPubKey",
         peer_node_address: "NodeAddress",
     ):
         pass
+    
+    def __call__(
+        self,
+        decrypted_message_with_pub_key: "DecryptedMessageWithPubKey",
+        peer_node_address: "NodeAddress",
+    ):
+        self.on_direct_message(decrypted_message_with_pub_key, peer_node_address)
