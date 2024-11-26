9fb89eca90e2b543caaccc2e8573f2755595c2d0
thecockatiel <thecockatiel@proton.me>
2024-11-26 19:48:52 +0330

    chore: change getters to property

--- bisq/core/provider/price/market_price.py
+++ bisq/core/provider/price/market_price.py
@@ -10,11 +10,14 @@ class MarketPrice:
     timestamp_sec: int
     is_externally_provided_price: bool
 
+    @property
     def is_price_available(self) -> bool:
         return self.price > 0
 
+    @property
     def is_recent_price_available(self) -> bool:
-        return self.is_price_available() and self.timestamp_sec > (int(time()) - MarketPrice.MARKET_PRICE_MAX_AGE_SEC)
+        return self.is_price_available and self.timestamp_sec > (int(time()) - MarketPrice.MARKET_PRICE_MAX_AGE_SEC)
 
+    @property
     def is_recent_external_price_available(self) -> bool:
-        return self.is_externally_provided_price and self.is_recent_price_available()
+        return self.is_externally_provided_price and self.is_recent_price_available
