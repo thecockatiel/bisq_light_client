1c97d6f998eefa88f8346cd9b7f0b490d97e153a
thecockatiel <thecockatiel@proton.me>
2024-11-26 17:14:36 +0330

    chore: add DisputeAgentSelection

---
+++ bisq/core/offer/availability/dispute_agent_selection.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING, TypeVar, List
+import random
+
+if TYPE_CHECKING:
+    from bisq.core.support.dispute.agent.dispute_agent_manager import (
+        DisputeAgentManager,
+    )
+    from bisq.core.support.dispute.agent.dispute_agent import DisputeAgent
+
+T = TypeVar("T", bound="DisputeAgent")
+
+
+class DisputeAgentSelection:
+    LOOK_BACK_RANGE = 100
+
+    @staticmethod
+    def get_random_mediator(dispute_agent_manager: "DisputeAgentManager[T]") -> T:
+        return DisputeAgentSelection.get_random_dispute_agent(dispute_agent_manager)
+
+    @staticmethod
+    def get_random_refund_agent(dispute_agent_manager: "DisputeAgentManager[T]") -> T:
+        return DisputeAgentSelection.get_random_dispute_agent(dispute_agent_manager)
+
+    @staticmethod
+    def get_random_dispute_agent(dispute_agent_manager: "DisputeAgentManager[T]") -> T:
+        dispute_agents = list(dispute_agent_manager.observable_map.values())
+        random.shuffle(dispute_agents)
+
+        if not dispute_agents:
+            raise ValueError("dispute_agents must not be empty")
+
+        return dispute_agents[0]
