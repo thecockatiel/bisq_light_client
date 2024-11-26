86fd35b0f46b16956eecbc75243a8f5f71e99b1c
thecockatiel <thecockatiel@proton.me>
2024-11-26 14:53:51 +0330

    chore: add task and task runner

---
+++ bisq/common/taskrunner/task.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING, TypeVar, Generic, Union
+from abc import ABC, abstractmethod
+from bisq.common.setup.log_setup import get_logger
+from bisq.common.taskrunner.intercept_task_exception import InterceptTaskException
+
+if TYPE_CHECKING:
+    from bisq.common.taskrunner.task_runner import TaskRunner
+
+logger = get_logger(__name__)
+
+T = TypeVar('T')
+
+class Task(ABC, Generic[T]):
+    task_to_intercept = None
+
+    def __init__(self, task_handler: 'TaskRunner', model: T):
+        self.task_handler = task_handler
+        self.model = model
+        self.error_message = f"An error occurred at task: {self.__class__.__name__}"
+        self.completed = False
+
+    @abstractmethod
+    def run(self) -> None:
+        pass
+
+    def run_intercept_hook(self) -> None:
+        if self.__class__ == Task.task_to_intercept:
+            raise InterceptTaskException(f"Task intercepted for testing purpose. Task = {self.__class__.__name__}")
+
+    def append_to_error_message(self, message: Union[str, Exception]) -> None:
+        self.error_message += f"\n{repr(message)}"
+
+    def complete(self) -> None:
+        self.completed = True
+        self.task_handler.handle_complete()
+
+    def failed(self, message: str = None, exc: Exception = None) -> None:
+        if message:
+            self.append_to_error_message(message)
+        if exc:
+            logger.error(self.error_message, exc_info=exc)
+        else:
+            logger.error(self.error_message)
+            
+        self.task_handler.handle_error_message(self.error_message)
---
+++ bisq/common/taskrunner/task_runner.py
@@ -0,0 +1,58 @@
+from bisq.common.setup.log_setup import get_logger
+from bisq.common.taskrunner.task import Task
+from bisq.common.taskrunner.task_model import TaskModel
+from queue import Queue
+from typing import TYPE_CHECKING, Type, TypeVar, Generic
+
+if TYPE_CHECKING:
+    from bisq.common.handlers.error_message_handler import ErrorMessageHandler
+    from bisq.common.handlers.result_handler import ResultHandler
+
+logger = get_logger(__name__)
+
+T = TypeVar('T', bound=TaskModel)
+
+class TaskRunner(Generic[T]):
+    def __init__(self, shared_model: T, 
+                 result_handler: "ResultHandler",
+                 error_message_handler: "ErrorMessageHandler"):
+        self.tasks: Queue[Type[Task[T]]] = Queue()
+        self.shared_model = shared_model
+        self.result_handler = result_handler
+        self.error_message_handler = error_message_handler
+        self.failed = False
+        self.is_canceled = False
+        self.current_task: Type[Task[T]] = None
+
+    def add_tasks(self, *items: Type[Task[T]]) -> None:
+        for item in items:
+            self.tasks.put(item)
+
+    def run(self) -> None:
+        self._next()
+
+    def _next(self) -> None:
+        if not self.failed and not self.is_canceled:
+            if not self.tasks.empty():
+                try:
+                    self.current_task = self.tasks.get()
+                    logger.info(f"Run task: {self.current_task.__name__}")
+                    self.current_task(self, self.shared_model).run()
+                except Exception as e:
+                    logger.exception("Error at taskRunner")
+                    self.handle_error_message(f"Error at taskRunner: {str(e)}")
+            else:
+                self.result_handler()
+
+    def cancel(self) -> None:
+        self.is_canceled = True
+
+    def handle_complete(self) -> None:
+        self._next()
+
+    def handle_error_message(self, error_message: str) -> None:
+        logger.error(f"Task failed: {self.current_task.__name__} / errorMessage: {error_message}")
+        self.failed = True
+        self.error_message_handler(error_message)
+
+
