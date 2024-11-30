198f49c49d531f701265b399a15a35e8942b40ba
thecockatiel <thecockatiel@proton.me>
2024-11-30 18:15:48 +0330

    chore: make AppendOnlyDataStoreListener callable

--- bisq/core/network/p2p/persistence/append_only_data_store_listener.py
+++ bisq/core/network/p2p/persistence/append_only_data_store_listener.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
+from collections.abc import Callable
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from bisq.core.network.p2p.storage.payload.persistable_network_payload import PersistableNetworkPayload
 
-class AppendOnlyDataStoreListener(ABC):
+class AppendOnlyDataStoreListener(Callable[["PersistableNetworkPayload"], None], ABC):
     @abstractmethod
     def on_added(self, payload: "PersistableNetworkPayload"):
         pass
+    
+    def __call__(self, payload: "PersistableNetworkPayload"):
+        self.on_added(payload)
