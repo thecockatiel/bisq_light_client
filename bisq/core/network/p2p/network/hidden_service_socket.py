97eaf193d8d7a5016f9938eb7fdd6f65074487cb
thecockatiel <thecockatiel@proton.me>
2024-12-04 13:10:44 +0330

    feat: add Tor network handling and a test

---
+++ bisq/core/network/p2p/network/hidden_service_socket.py
@@ -0,0 +1,74 @@
+
+import socket
+from typing import TYPE_CHECKING, Optional
+from twisted.internet import reactor
+from txtorcon import FilesystemOnionService, IOnionService, TorOnionAddress
+
+from utils.aio import as_future
+from bisq.common.setup.log_setup import get_logger
+
+if TYPE_CHECKING:
+    from bisq.core.network.p2p.network.tor_network_node import TorNetworkNode
+    
+    
+
+logger = get_logger(__name__)
+
+class OnionIListeningPort:
+    @property
+    def onion_service() -> "IOnionService":
+        pass 
+    
+    def getHost() -> "TorOnionAddress":
+        pass
+
+class HiddenServiceSocket:
+
+    def __init__(self, local_port: int, hidden_service_dir: str, hidden_service_port: int, tor_network_node: "TorNetworkNode"):
+        self._local_port = local_port
+        self._hidden_service_dir = hidden_service_dir
+        self._hidden_service_port = hidden_service_port
+        self._tor_network_node = tor_network_node
+        self._server_socket: Optional[socket.socket] = None
+        # set after initialize
+        self._onion_service: Optional["FilesystemOnionService"] = None
+        
+    async def initialize(self) -> "OnionIListeningPort":
+        # ensure hs parent dir perm be 700
+        self._tor_network_node.tor_mode.get_hidden_service_directory().parent.chmod(0o700) # same as self.tor_dir, but more explicit.
+            
+        
+        self._server_socket = sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('127.0.0.1', self._local_port))
+        sock.listen(5)
+        
+        progress_updates=lambda percent, tag, summary: logger.trace(f"Tor Hidden Service: {percent}%: {tag} - {summary}")
+        # NOTE: this is probably a brittle way to create a hidden service, but it's the simplest way to do it for now.
+        self._onion_service: "FilesystemOnionService"  = await as_future(FilesystemOnionService.create(
+            reactor,
+            self._tor_network_node.tor._config,
+            self._hidden_service_dir,
+            ['%d 127.0.0.1:%d' % (self._hidden_service_port, self._local_port)],
+            version=3,
+            progress=progress_updates,
+        ))
+        
+    @property
+    def server_socket(self):
+        return self._server_socket
+    
+    @property
+    def service_name(self):
+        return self._onion_service.hostname
+    
+    @property
+    def hidden_service_port(self):
+        return self._hidden_service_port
+        
+    def __str__(self) -> str:
+        return f"HiddenServiceSocket(local_port={self._local_port}, hidden_service_dir={self._hidden_service_dir}, hidden_service_port={self._hidden_service_port}, onion_uri={self.service_name})"
+    
+    async def close(self):
+        if self._server_socket:
+            self._server_socket.close()
+            self._server_socket = None
---
+++ bisq/core/network/p2p/network_node_provider.py
@@ -0,0 +1,65 @@
+from typing import TYPE_CHECKING, Optional
+from pathlib import Path
+
+from bisq.common.config.config import CONFIG
+from bisq.core.network.p2p.network.localhost_network_node import LocalhostNetworkNode
+from bisq.core.network.p2p.network.network_node import NetworkNode
+from bisq.core.network.p2p.network.new_tor import NewTor
+from bisq.core.network.p2p.network.running_tor import RunningTor
+from bisq.core.network.p2p.network.tor_network_node import TorNetworkNode
+from bisq.core.network.p2p.network.tor_mode import TorMode
+
+if TYPE_CHECKING:
+    from bisq.common.protocol.network.network_proto_resolver import NetworkProtoResolver
+    from bisq.core.network.p2p.network.bridge_address_provider import BridgeAddressProvider
+    from bisq.core.network.p2p.network.ban_filter import BanFilter
+
+class NetworkNodeProvider:
+    def __init__(self,
+                 network_proto_resolver: "NetworkProtoResolver",
+                 bridge_address_provider: "BridgeAddressProvider",
+                 ban_filter: Optional["BanFilter"],
+                 max_connections: int,
+                 use_localhost_for_p2p: bool,
+                 port: int,
+                 tor_dir: Path,
+                 torrc_file: Optional[Path],
+                 torrc_options: str,
+                 control_host: str,
+                 control_port: int,
+                 password: str,
+                 stream_isolation: bool,
+                 use_bridges_file: bool):
+        
+        if use_localhost_for_p2p:
+            self.network_node = LocalhostNetworkNode(port, network_proto_resolver, ban_filter, max_connections)
+        else:
+            tor_mode = self._get_tor_mode(
+                bridge_address_provider,
+                tor_dir,
+                torrc_file,
+                torrc_options,
+                control_host,
+                control_port,
+                password,
+                use_bridges_file,
+            )
+            self.network_node = TorNetworkNode(port, network_proto_resolver, stream_isolation, 
+                                             tor_mode, ban_filter, max_connections, control_host)
+
+    def _get_tor_mode(self,
+                      bridge_address_provider: "BridgeAddressProvider",
+                      tor_dir: Path,
+                      torrc_file: Optional[Path],
+                      torrc_options: str,
+                      control_host: str,
+                      control_port: int,
+                      password: str,
+                      use_bridges_file: bool) -> TorMode:
+        if control_port != CONFIG.UNSPECIFIED_PORT:
+            return RunningTor(tor_dir, control_host, control_port, password)
+            
+        return NewTor(tor_dir, torrc_file, torrc_options, bridge_address_provider, use_bridges_file)
+
+    def get(self) -> NetworkNode:
+        return self.network_node
