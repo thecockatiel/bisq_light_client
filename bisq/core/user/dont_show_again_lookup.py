f8812e0b47653bfb18d07a6a6ea0a33ffb2b6ce0
thecockatiel <thecockatiel@proton.me>
2024-12-05 10:18:09 +0330

    chore: add DontShowAgainLookup

---
+++ bisq/core/user/dont_show_again_lookup.py
@@ -0,0 +1,21 @@
+
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from bisq.core.user.preferences import Preferences
+
+class DontShowAgainLookup:
+    preferences = None
+
+    @staticmethod
+    def set_preferences(preferences: "Preferences"):
+        DontShowAgainLookup.preferences = preferences
+
+    @staticmethod
+    def show_again(key: str) -> bool:
+        return DontShowAgainLookup.preferences.show_again(key)
+
+    @staticmethod
+    def dont_show_again(key: str, dont_show_again: bool):
+        DontShowAgainLookup.preferences.dont_show_again(key, dont_show_again)
