9142e432bf172ef91042ebe24ff74af189884776
thecockatiel <thecockatiel@proton.me>
2024-12-05 10:27:50 +0330

    chore: add maybe_apply_dao_full_mode to CoreNetworkCapabilities as noop

--- bisq/core/setup/core_network_capabilities.py
+++ bisq/core/setup/core_network_capabilities.py
@@ -1,5 +1,6 @@
 from bisq.common.capabilities import Capabilities
 from bisq.common.capability import Capability
+from bisq.common.config.config import Config
 from bisq.common.setup.log_setup import get_logger
 
 logger = get_logger(__name__)
@@ -20,3 +21,17 @@ class CoreNetworkCapabilities:
             ]
         )
         logger.info(Capabilities.app.pretty_print())
+
+    @staticmethod
+    def maybe_apply_dao_full_mode(config: Config):
+        # NOTE: Replaced with noop as we don't support DAO in python implementation
+        pass
+        # If we set dao full mode at the preferences view we add the capability there. We read the preferences a
+        # bit later than we call that method so we have to add DAO_FULL_NODE Capability at preferences as well to
+        # be sure it is set in both cases.
+        # if config.full_dao_node:
+        #     Capabilities.app.add_all([Capability.DAO_FULL_NODE])
+        # else:
+        #     # A lite node has the capability to receive bsq blocks. We do not want to send BSQ blocks to full nodes
+        #     # as they ignore them anyway.
+        #     Capabilities.app.add_all([Capability.RECEIVE_BSQ_BLOCK])
