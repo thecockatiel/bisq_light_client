8c57477aa37749b68b8f1c69b792199bfd11493b
thecockatiel <thecockatiel@proton.me>
2024-12-06 11:42:59 +0330

    chore: add MediationResultState and RefundResultState

---
+++ bisq/core/support/refund/refund_result_state.py
@@ -0,0 +1,17 @@
+
+from enum import IntEnum
+from bisq.common.protocol.proto_util import ProtoUtil
+import proto.pb_pb2 as protobuf
+
+# JAVA TODO
+class RefundResultState(IntEnum):
+    UNDEFINED_REFUND_RESULT = 0
+    
+    @staticmethod
+    def from_proto(proto: protobuf.RefundResultState) -> 'RefundResultState':
+        return ProtoUtil.enum_from_proto(RefundResultState, protobuf.RefundResultState, proto)
+    
+    @staticmethod
+    def to_proto_message(state: "RefundResultState"):
+        return ProtoUtil.proto_enum_from_enum(protobuf.RefundResultState, state)
+
