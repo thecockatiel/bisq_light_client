3b19e0f10a47d23d499bd065d60d141c9b6556ec
thecockatiel <thecockatiel@proton.me>
2024-12-06 15:40:22 +0330

    chore: add incomplete classes

---
+++ bisq/core/btc/wallet/trade_wallet_service.py
@@ -0,0 +1,8 @@
+from bitcoinj.core.transaction import Transaction
+
+
+# TODO
+class TradeWalletService:
+
+    def get_wallet_tx(tx_id: bytes) -> "Transaction":
+        raise RuntimeError("TradeWalletService.get_wallet_tx Not implemented yet")
