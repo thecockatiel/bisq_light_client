d07fa345dfef5815ddd5b99adb3a9ce05ba38c1b
thecockatiel <thecockatiel@proton.me>
2024-12-06 11:18:46 +0330

    fix: add missing types to TradeModel

--- bisq/core/trade/model/trade_model.py
+++ bisq/core/trade/model/trade_model.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, TypeVar
 from bisq.common.taskrunner.task_model import TaskModel
 from bisq.core.trade.model.tradable import Tradable
 from utils.data import SimpleProperty
@@ -11,7 +11,15 @@ if TYPE_CHECKING:
     from bisq.core.trade.protocol.provider import Provider
     from bisq.core.offer.offer import Offer
     from bisq.core.network.p2p.node_address import NodeAddress
+    from bisq.core.trade.protocol.protocol_model import ProtocolModel
+    from bisq.core.trade.protocol.trade_peer import TradePeer
+    from bisq.core.monetary.price import Price
+    from bisq.core.monetary.volume import Volume
+    from bisq.core.trade.model.trade_phase import TradePhase
+    from bisq.core.trade.model.trade_state import TradeState
+    from bitcoinj.base.coin import Coin
 
+_T = TypeVar("T", bound="TradePeer")
 
 class TradeModel(Tradable, TaskModel):
     def __init__(self, uid: str, offer: "Offer", 
@@ -44,15 +52,15 @@ class TradeModel(Tradable, TaskModel):
         pass
 
     @abstractmethod
-    def get_trade_protocol_model(self):
+    def get_trade_protocol_model(self) -> "ProtocolModel[_T]":
         pass
 
     @abstractmethod
-    def get_trade_state(self):
+    def get_trade_state(self) -> "TradeState":
         pass
 
     @abstractmethod
-    def get_trade_phase(self):
+    def get_trade_phase(self) -> "TradePhase":
         pass
 
     @abstractmethod
@@ -60,27 +68,27 @@ class TradeModel(Tradable, TaskModel):
         pass
 
     @abstractmethod
-    def get_amount(self):
+    def get_amount(self) -> "Coin":
         pass
 
     @abstractmethod
-    def get_volume(self):
+    def get_volume(self) -> "Volume":
         pass
 
     @abstractmethod
-    def get_price(self):
+    def get_price(self) -> "Price":
         pass
 
     @abstractmethod
-    def get_tx_fee(self):
+    def get_tx_fee(self) -> "Coin":
         pass
 
     @abstractmethod
-    def get_taker_fee(self):
+    def get_taker_fee(self) -> "Coin":
         pass
 
     @abstractmethod
-    def get_maker_fee(self):
+    def get_maker_fee(self) -> "Coin":
         pass
 
     def get_offer(self):
