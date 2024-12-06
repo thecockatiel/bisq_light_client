9d384a80ed6b23bc9905a7fb9a1b3f4d137f7885
thecockatiel <thecockatiel@proton.me>
2024-12-06 15:43:55 +0330

    chore: add Dummy TradeStatisticsManager

---
+++ bisq/core/trade/statistics/trade_statistics_manager.py
@@ -0,0 +1,14 @@
+# TODO not essential so probably not gonna implement for now
+
+class TradeStatisticsManager():
+    def shut_down(self):
+        pass
+    
+    def on_all_services_initialized(self):
+        pass
+    
+    def maybe_dump_statistics(self):
+        pass
+    
+    def maybe_republish_trade_statistics(self):
+        pass
