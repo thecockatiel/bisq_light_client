f111a79b542f42b6721cfdb2f1cc430a9b6ce061
thecockatiel <thecockatiel@proton.me>
2024-12-07 10:10:02 +0330

    chore: change HttpClient to use protocol

--- bisq/core/network/http/http_client.py
+++ bisq/core/network/http/http_client.py
@@ -1,43 +1,26 @@
-from abc import ABC, abstractmethod
+from typing import Protocol, runtime_checkable
 
 
-class HttpClient(ABC):
+@runtime_checkable
+class HttpClient(Protocol):
     uid: str
     base_url: str
     has_pending_request: bool
     ignore_socks5_proxy: bool
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        required_attributes = [
-            "uid",
-            "base_url",
-            "has_pending_request",
-            "ignore_socks5_proxy",
-        ]
-        for attr in required_attributes:
-            if not hasattr(self, attr):
-                raise RuntimeError(f"You need to have '{attr}' in {self.__name__}")
 
-    @abstractmethod
     def get(
         self,
         url: str,
         params: dict[str, str] = {},
         headers: dict[str, str] = {},
-    ) -> str:
-        pass
+    ) -> str: ...
 
-    @abstractmethod
     def post(
         self,
         url: str,
         data: dict[str, str] = None,
         params: dict[str, str] = {},
         headers: dict[str, str] = {},
-    ) -> str:
-        pass
+    ) -> str: ...
 
-    @abstractmethod
-    def shut_down(self) -> None:
-        pass
+    def shut_down(self) -> None: ...
