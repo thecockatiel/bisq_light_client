20d332272ae6322e508423b3fc0b6857f59a1cf7
thecockatiel <thecockatiel@proton.me>
2024-12-07 06:11:42 +0330

    chore: add SellerTrade

---
+++ bisq/core/trade/model/bisq_v1/seller_trade.py
@@ -0,0 +1,31 @@
+from abc import ABC 
+from bisq.core.trade.model.bisq_v1.trade import Trade
+from bisq.core.trade.model.trade_dispute_state import TradeDisputeState
+
+
+class SellerTrade(Trade, ABC):
+    
+    def get_payout_amount(self):
+        assert self._offer is not None
+        return self._offer.seller_security_deposit
+    
+    def confirm_permitted(self):
+        match self.dispute_state_property.value:
+            case TradeDisputeState.NO_DISPUTE:
+                return True
+            
+            case TradeDisputeState.DISPUTE_REQUESTED | \
+                 TradeDisputeState.DISPUTE_STARTED_BY_PEER | \
+                 TradeDisputeState.DISPUTE_CLOSED | \
+                 TradeDisputeState.MEDIATION_REQUESTED | \
+                 TradeDisputeState.MEDIATION_STARTED_BY_PEER:
+                return False
+                
+            case TradeDisputeState.MEDIATION_CLOSED:
+                return not self.mediation_result_applied_penalty_to_seller
+                
+            case TradeDisputeState.REFUND_REQUESTED | \
+                 TradeDisputeState.REFUND_REQUEST_STARTED_BY_PEER | \
+                 TradeDisputeState.REFUND_REQUEST_CLOSED | \
+                 _:
+                return False
