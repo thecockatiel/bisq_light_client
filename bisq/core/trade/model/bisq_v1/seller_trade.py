dd48ab8c86cefadcda51737b09772778b12c00f0
thecockatiel <thecockatiel@proton.me>
2024-12-07 06:15:18 +0330

    chore: add buyer trade

--- bisq/core/trade/model/bisq_v1/seller_trade.py
+++ bisq/core/trade/model/bisq_v1/seller_trade.py
@@ -6,7 +6,7 @@ from bisq.core.trade.model.trade_dispute_state import TradeDisputeState
 class SellerTrade(Trade, ABC):
     
     def get_payout_amount(self):
-        assert self._offer is not None
+        assert self._offer is not None, "Invalid state: offer is None at SellerTrade.get_payout_amount()"
         return self._offer.seller_security_deposit
     
     def confirm_permitted(self):
