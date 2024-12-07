a87a8065b10eb0e37c40d130d2a13c3c227684c8
thecockatiel <thecockatiel@proton.me>
2024-12-07 12:34:00 +0330

    chore: add BsqSwapBuyerAsMakerTrade

---
+++ bisq/core/trade/model/bsq_swap/bsq_swap_buyer_trade.py
@@ -0,0 +1,6 @@
+
+from bisq.core.trade.model.bsq_swap.bsq_swap_trade import BsqSwapTrade
+
+
+class BsqSwapBuyerTrade(BsqSwapTrade):
+    pass
---
+++ bisq/core/trade/model/bsq_swap/bsq_swap_seller_trade.py
@@ -0,0 +1,6 @@
+
+from bisq.core.trade.model.bsq_swap.bsq_swap_trade import BsqSwapTrade
+
+
+class BsqSwapSellerTrade(BsqSwapTrade):
+    pass
