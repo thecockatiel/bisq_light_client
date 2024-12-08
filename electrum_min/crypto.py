935b655a37a206d1b19afff3f470ac7936ad1671
thecockatiel <thecockatiel@proton.me>
2024-12-08 17:07:05 +0330

    chore: add AddressEntry

--- electrum_min/crypto.py
+++ electrum_min/crypto.py
@@ -1,4 +1,5 @@
 import hashlib
+import hmac
 from typing import Union
 
 from electrum_min.util import to_bytes
@@ -11,3 +12,6 @@ def sha256d(x: Union[bytes, str]) -> bytes:
     x = to_bytes(x, 'utf8')
     out = bytes(sha256(sha256(x)))
     return out
+
+def hmac_oneshot(key: bytes, msg: bytes, digest) -> bytes:
+    return hmac.digest(key, msg, digest)
