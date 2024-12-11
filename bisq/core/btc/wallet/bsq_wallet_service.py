5edc2302e5f9ac11721e44906c08c6e2f881173d
thecockatiel <thecockatiel@proton.me>
2024-12-11 10:34:35 +0330

    chore: add BsqSwapTradeManager

--- bisq/core/btc/wallet/bsq_wallet_service.py
+++ bisq/core/btc/wallet/bsq_wallet_service.py
@@ -1,7 +1,10 @@
+from collections.abc import Callable
 from bisq.core.btc.wallet.wallet_service import WalletService
 from bisq.core.dao.state.dao_state_listener import DaoStateListener
 
 
 # TODO
 class BsqWalletService(WalletService, DaoStateListener):
-    pass
+    
+    def add_wallet_transactions_change_listener(self, listener: Callable[[], None]):
+        pass
--- bisq/core/btc/wallet/wallet_service.py
+++ bisq/core/btc/wallet/wallet_service.py
@@ -7,6 +7,7 @@ from bisq.common.crypto.hash import get_sha256_hash
 if TYPE_CHECKING:
     from bitcoinj.core.transaction import Transaction
     from bitcoinj.core.network_parameters import NetworkParameters
+    from bitcoinj.core.transaction_confidence import TransactionConfidence
 
 # TODO
 class WalletService(ABC):
@@ -24,3 +25,6 @@ class WalletService(ABC):
     
     def get_tx_from_serialized_tx(self, tx: bytes) -> Optional["Transaction"]:
         return Transaction(self.params, tx)
+    
+    def get_confidence_for_tx_id(self, tx_id: Optional[str])-> Optional['TransactionConfidence']:     
+        raise RuntimeError("WalletService.get_confidence_for_tx_id Not implemented yet")
