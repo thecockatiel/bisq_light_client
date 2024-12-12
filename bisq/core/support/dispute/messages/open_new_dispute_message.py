beb009a409612e64e2ff599c2bf6d9ff3c7f65c8
thecockatiel <thecockatiel@proton.me>
2024-12-12 08:18:21 +0330

    chore: improve and move OpenNewDisputeMessage

--- bisq/core/support/dispute/messages/open_new_dispute_message.py
+++ bisq/core/support/dispute/messages/open_new_dispute_message.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
-
-from bisq.core.protocol.core_proto_resolver import CoreProtoResolver
-from bisq.core.network.p2p.node_address import NodeAddress
-from bisq.core.support.dispute.dispute import Dispute
+from typing import TYPE_CHECKING
 from bisq.core.support.dispute.messages.dispute_message import DisputeMessage
 from bisq.core.support.support_type import SupportType
-
+from bisq.core.network.p2p.node_address import NodeAddress
 import proto.pb_pb2 as protobuf
+
+if TYPE_CHECKING:
+    from bisq.core.support.dispute.dispute import Dispute
+    from bisq.core.protocol.core_proto_resolver import CoreProtoResolver
 
 
 @dataclass(kw_only=True)
@@ -14,7 +15,7 @@ class OpenNewDisputeMessage(DisputeMessage):
     dispute: "Dispute"
     sender_node_address: "NodeAddress"
 
-    def to_proto_network_envelope(self) -> "protobuf.NetworkEnvelope":
+    def to_proto_network_envelope(self):
         envelope = self.get_network_envelope_builder()
         envelope.open_new_dispute_message.CopyFrom(
             protobuf.OpenNewDisputeMessage(
@@ -27,23 +28,22 @@ class OpenNewDisputeMessage(DisputeMessage):
         return envelope
 
     @staticmethod
-    def from_proto(
-        proto: protobuf.OpenNewDisputeMessage,
-        core_proto_resolver: "CoreProtoResolver",
-        message_version: int,
-    ) -> "OpenNewDisputeMessage":
+    def from_proto(proto: protobuf.OpenNewDisputeMessage, core_proto_resolver: "CoreProtoResolver", message_version: int):
+        from bisq.core.support.dispute.dispute import Dispute
+        
         return OpenNewDisputeMessage(
-            message_version=message_version,
-            uid=proto.uid,
-            support_type=SupportType.from_proto(proto.type),
             dispute=Dispute.from_proto(proto.dispute, core_proto_resolver),
             sender_node_address=NodeAddress.from_proto(proto.sender_node_address),
+            uid=proto.uid,
+            support_type=SupportType.from_proto(proto.type),
+            message_version=message_version,
         )
 
-    def get_trade_id(self) -> str:
-        return self.dispute.get_trade_id()
+    @property
+    def trade_id(self):
+        return self.dispute.trade_id
 
-    def __str__(self) -> str:
+    def __str__(self):
         return (
             f"OpenNewDisputeMessage("
             f"\n     dispute={self.dispute}"
