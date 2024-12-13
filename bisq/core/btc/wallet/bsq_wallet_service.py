4a56f89f43394034b515ed8396c9b97beb422437
thecockatiel <thecockatiel@proton.me>
2024-12-14 00:19:26 +0330

    chore: add get_available_balance to bsq wallet service

--- bisq/core/btc/wallet/bsq_wallet_service.py
+++ bisq/core/btc/wallet/bsq_wallet_service.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable
 from bisq.core.btc.wallet.wallet_service import WalletService
 from bisq.core.dao.state.dao_state_listener import DaoStateListener
+from bitcoinj.base.coin import Coin
 
 
 # TODO
@@ -8,3 +9,6 @@ class BsqWalletService(WalletService, DaoStateListener):
     
     def add_wallet_transactions_change_listener(self, listener: Callable[[], None]):
         pass
+
+    def get_available_balance(self) -> Coin:
+        return Coin.ZERO()
