6aaf775ac264c42f86c346776928bc51bb631d13
thecockatiel <thecockatiel@proton.me>
2024-12-14 20:18:05 +0330

    chore: update Res

--- bisq/core/locale/res.py
+++ bisq/core/locale/res.py
@@ -1,6 +1,26 @@
 # TODO: Implemented partially since it was used widely, to complete later as needed.
 
+from bisq.common.config.config import CONFIG
+
 class Res:
+    base_currency_code: str = None
+    base_currency_name: str = None
+    base_currency_name_lower_case: str = None
+    
+    @staticmethod
+    def setup():
+        base_currency_network = CONFIG.base_currency_network
+        Res.set_base_currency_code(base_currency_network.currency_code)
+        Res.set_base_currency_name(base_currency_network.currency_name)
+        
+    @staticmethod
+    def set_base_currency_code(base_currency_code: str):
+        Res.base_currency_code = base_currency_code
+    
+    @staticmethod
+    def set_base_currency_name(base_currency_name: str):
+        Res.base_currency_name = base_currency_name
+        Res.base_currency_name_lower_case = base_currency_name.lower()
     
     @staticmethod
     def get(key: str, *args):
@@ -9,3 +29,5 @@ class Res:
     @staticmethod
     def get_with_col(key: str, *args):
         return Res.get(key) + ":"
+    
+Res.setup()
