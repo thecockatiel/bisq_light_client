7c613cfa8d8caba4164c71c44e931b7146579cdf
thecockatiel <thecockatiel@proton.me>
2024-12-14 19:56:25 +0330

    chore: add AmazonGiftCardAccount

---
+++ bisq/core/payment/amazon_gift_card_account.py
@@ -0,0 +1,69 @@
+from typing import Optional, cast
+from bisq.core.locale.country import Country
+from bisq.core.locale.country_util import find_country_by_code
+from bisq.core.locale.fiat_currency import FiatCurrency
+from bisq.core.locale.trade_currency import TradeCurrency
+from bisq.core.payment.payload.amazon_gift_card_account_payload import (
+    AmazonGiftCardAccountPayload,
+)
+from bisq.core.payment.payload.payment_account_payload import PaymentAccountPayload
+from bisq.core.payment.payload.payment_method import PaymentMethod
+from bisq.core.payment.payload.sepa_account_payload import SepaAccountPayload
+from bisq.core.payment.payment_account import PaymentAccount
+
+
+class AmazonGiftCardAccount(PaymentAccount):
+    SUPPORTED_CURRENCIES: list["TradeCurrency"] = [
+        FiatCurrency("AUD"),
+        FiatCurrency("CAD"),
+        FiatCurrency("EUR"),
+        FiatCurrency("GBP"),
+        FiatCurrency("INR"),
+        FiatCurrency("JPY"),
+        FiatCurrency("SAR"),
+        FiatCurrency("SEK"),
+        FiatCurrency("SGD"),
+        FiatCurrency("TRY"),
+        FiatCurrency("USD"),
+    ]
+
+    def __init__(self):
+        super().__init__(PaymentMethod.AMAZON_GIFT_CARD)
+        self._country: Optional[Country] = None
+
+    def create_payload(self) -> PaymentAccountPayload:
+        return AmazonGiftCardAccountPayload(self.payment_method.id, self.id)
+
+    def get_supported_currencies(self) -> list[TradeCurrency]:
+        return AmazonGiftCardAccount.SUPPORTED_CURRENCIES
+
+    @property
+    def email_or_mobile_nr(self):
+        return self._get_amazon_gift_card_account_payload().email_or_mobile_nr
+
+    @email_or_mobile_nr.setter
+    def email_or_mobile_nr(self, email_or_mobile_nr: str):
+        self._get_amazon_gift_card_account_payload().email_or_mobile_nr = (
+            email_or_mobile_nr
+        )
+
+    @property
+    def country_not_set(self):
+        return self._get_amazon_gift_card_account_payload().country_not_set
+
+    @property
+    def country(self):
+        if self._country is None:
+            country_code = self._get_amazon_gift_card_account_payload().country_code
+            code = find_country_by_code(country_code)
+            if code:
+                self._country = code
+        return self._country
+
+    @country.setter
+    def country(self, country: Country):
+        self._country = country
+        self._get_amazon_gift_card_account_payload().country_code = country.code
+
+    def _get_amazon_gift_card_account_payload(self):
+        return cast(AmazonGiftCardAccountPayload, self.payment_account_payload)
---
+++ bisq/core/payment/payload/amazon_gift_card_account_payload.py
@@ -0,0 +1,63 @@
+from typing import Optional
+from bisq.core.locale.res import Res
+from bisq.core.payment.payload.payment_account_payload import PaymentAccountPayload
+import proto.pb_pb2 as protobuf
+
+class AmazonGiftCardAccountPayload(PaymentAccountPayload):
+
+    def __init__(
+        self,
+        payment_method_id: str,
+        id: str,
+        email_or_mobile_nr: Optional[str] = None,
+        country_code: Optional[str] = None,
+        max_trade_period: int = -1,
+        exclude_from_json_data_map: Optional[dict[str, str]] = None,
+    ):
+        super().__init__(payment_method_id, id, max_trade_period, exclude_from_json_data_map)
+        self.email_or_mobile_nr = email_or_mobile_nr
+        # For backward compatibility we need to exclude the new field for the contract json.
+        # We can remove that after a while when risk that users with pre 1.5.5 version is very low.
+        self.country_code = country_code  # JsonExclude
+
+    def get_json_dict(self):
+        self_dict = self.__dict__.copy()
+        self_dict.pop("country_code")
+        return self_dict
+
+    def to_proto_message(self):
+        amazon_payload = protobuf.AmazonGiftCardAccountPayload(
+            country_code=self.country_code,
+            email_or_mobile_nr=self.email_or_mobile_nr,
+        )
+        
+        builder = self.get_payment_account_payload_builder()
+        builder.amazon_gift_card_account_payload.CopyFrom(amazon_payload)
+        return builder
+    
+    @staticmethod
+    def from_proto(proto: protobuf.PaymentAccountPayload):
+        amazon_payload = proto.amazon_gift_card_account_payload
+        return AmazonGiftCardAccountPayload(
+            payment_method_id=proto.payment_method_id,
+            id=proto.id,
+            email_or_mobile_nr=amazon_payload.email_or_mobile_nr,
+            country_code=amazon_payload.country_code,
+            max_trade_period=proto.max_trade_period,
+            exclude_from_json_data_map=dict(proto.exclude_from_json_data),            
+        )
+
+    def get_payment_details(self) -> str:
+        return f"{Res.get(self.payment_method_id)} - {self.get_payment_details_for_trade_popup().replace('\n', ', ')}"
+
+    def get_payment_details_for_trade_popup(self) -> str:
+        return f"{Res.get_with_col('payment.email.mobile')} {self.email_or_mobile_nr}"
+
+    def get_age_witness_input_data(self) -> bytes:
+        data = f"AmazonGiftCard{self.email_or_mobile_nr}"
+        return super().get_age_witness_input_data_using_bytes(data.encode('utf-8'))
+
+    @property
+    def country_not_set(self) -> bool:
+        return not self.country_code
+
