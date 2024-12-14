b819193b104dc518aaacacd8eb505e89962c83c5
thecockatiel <thecockatiel@proton.me>
2024-12-14 10:52:01 +0330

    chore: add get_estimated_fee_tx_vsize to BtcWalletService

--- bisq/core/btc/wallet/btc_wallet_service.py
+++ bisq/core/btc/wallet/btc_wallet_service.py
@@ -1,6 +1,7 @@
 from bisq.core.btc.model.address_entry import AddressEntry
 from bisq.core.btc.wallet.wallet_service import WalletService
 from bisq.core.dao.state.dao_state_listener import DaoStateListener
+from bitcoinj.base.coin import Coin
 from bitcoinj.core.transaction import Transaction
 
 
@@ -15,3 +16,6 @@ class BtcWalletService(WalletService, DaoStateListener):
     
     def get_address_entry_list_as_immutable_list(self) -> list["AddressEntry"]:
         raise RuntimeError("BtcWalletService.get_address_entry_list_as_immutable_list Not implemented yet")
+    
+    def get_estimated_fee_tx_vsize(self, output_values: list[Coin], tx_fee: Coin) -> int:
+        raise RuntimeError("BtcWalletService.get_estimated_fee_tx_vsize Not implemented yet")
