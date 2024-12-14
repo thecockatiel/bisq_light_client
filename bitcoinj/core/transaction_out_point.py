2e38714355ba9f6cc45b26dc1851f819c6db0e09
thecockatiel <thecockatiel@proton.me>
2024-12-14 10:25:53 +0330

    chore: add OfferUtil

---
+++ bitcoinj/core/transaction_out_point.py
@@ -0,0 +1,16 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from bitcoinj.core.transaction import Transaction
+    from bitcoinj.core.transaction_output import TransactionOutput
+
+
+# TODO
+class TransactionOutPoint:
+    MESSAGE_LENGTH = 36
+
+    def __init__(self):
+        self.index = 0
+        self.from_tx: "Transaction" = None
+        self.connected_output: "TransactionOutput" = None
--- bitcoinj/core/transaction_output.py
+++ bitcoinj/core/transaction_output.py
@@ -1,5 +1,16 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from bitcoinj.core.transaction_input import TransactionInput
+
 
 # TODO
-
 class TransactionOutput:
-    pass
+
+    def __init__(self):
+        self.spent_by: "TransactionInput" = None
+
+    @property
+    def index(self) -> int:
+        raise NotImplementedError("index not implemented in TransactionOutput")
