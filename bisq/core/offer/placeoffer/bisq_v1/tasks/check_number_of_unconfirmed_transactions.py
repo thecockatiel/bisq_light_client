858c0365390326e825c3566de0df195b13ebf610
thecockatiel <thecockatiel@proton.me>
2024-12-15 19:16:50 +0330

    chore: add CheckNumberOfUnconfirmedTransactions

---
+++ bisq/core/offer/placeoffer/bisq_v1/tasks/check_number_of_unconfirmed_transactions.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING, List, Optional
+from bisq.core.btc.model.address_entry_context import AddressEntryContext
+from bisq.core.btc.wallet.wallet_service import WalletService
+from bisq.core.locale.res import Res
+from bitcoinj.base.coin import Coin
+from bitcoinj.core.address import Address
+from bisq.common.taskrunner.task import Task
+
+if TYPE_CHECKING:
+    from bisq.core.offer.placeoffer.bisq_v1.place_offer_model import PlaceOfferModel
+    from bisq.common.taskrunner.task_runner import TaskRunner
+
+
+class CheckNumberOfUnconfirmedTransactions(Task["PlaceOfferModel"]):
+    def __init__(
+        self, task_handler: "TaskRunner[PlaceOfferModel]", model: "PlaceOfferModel"
+    ):
+        super().__init__(task_handler, model)
+
+    def run(self):
+        if (
+            self.model.wallet_service.is_unconfirmed_transactions_limit_hit()
+            or self.model.bsq_wallet_service.is_unconfirmed_transactions_limit_hit()
+        ):
+            self.failed(Res.get("shared.unconfirmedTransactionsLimitReached"))
+        self.complete()  # TODO: java sanity check
