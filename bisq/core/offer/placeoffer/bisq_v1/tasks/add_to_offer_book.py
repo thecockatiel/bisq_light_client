b84908409aeee3a46e17a816305fcee858681d26
thecockatiel <thecockatiel@proton.me>
2024-12-15 19:39:51 +0330

    chore: add AddToOfferBook

---
+++ bisq/core/offer/placeoffer/bisq_v1/tasks/add_to_offer_book.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING
+from bisq.common.taskrunner.task import Task
+
+if TYPE_CHECKING:
+    from bisq.core.offer.placeoffer.bisq_v1.place_offer_model import PlaceOfferModel
+    from bisq.common.taskrunner.task_runner import TaskRunner
+
+
+class AddToOfferBook(Task["PlaceOfferModel"]):
+    def __init__(
+        self, task_handler: "TaskRunner[PlaceOfferModel]", model: "PlaceOfferModel"
+    ):
+        super().__init__(task_handler, model)
+
+    def run(self):
+        try:
+            self.run_intercept_hook()
+            self.model.offer_book_service.add_offer(
+                self.model.offer,
+                self._on_success,
+                self._on_error,
+            )
+        except Exception as t:
+            self.model.offer.error_message = (
+                f"An error occurred.\nError message:\n{str(t)}"
+            )
+            self.failed(t)
+
+    def _on_success(self):
+        self.model.offer_added_to_offer_book = True
+        self.complete()
+
+    def _on_error(self, error_message: str):
+        self.model.offer.error_message = (
+            "Could not add offer to offerbook.\n"
+            "Please check your network connection and try again."
+        )
+        self.failed(error_message)
+
