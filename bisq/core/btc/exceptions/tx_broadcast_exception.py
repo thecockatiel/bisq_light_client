5769e078b28bd1c9c1a4628c4fadd1b3eb3de0c0
thecockatiel <thecockatiel@proton.me>
2024-12-15 19:52:24 +0330

    chore: add TxBroadcastException

---
+++ bisq/core/btc/exceptions/tx_broadcast_exception.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class TxBroadcastException(Exception):
+    """
+    Used in case the broadcasting of a tx did not succeed in the expected time.
+    The broadcast can still succeed at a later moment though.
+    """
+
+    def __init__(
+        self, message: str, cause: Optional[Exception] = None, tx_id: Optional[str] = None
+    ):
+        super().__init__(message)
+        if cause:
+            self.__cause__ = cause
+
+        self.tx_id = tx_id
+
+    def __str__(self):
+        return (
+            f"TxBroadcastException{{\n    tx_id='{self.tx_id}'\n}} {super().__str__()}"
+        )
