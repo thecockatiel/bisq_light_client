cc398462e481f3028a7ea85e3d67938c056d3680
thecockatiel <thecockatiel@proton.me>
2024-12-17 05:41:14 +0330

    chore: small import fix

--- bisq/core/trade/protocol/protocol_model.py
+++ bisq/core/trade/protocol/protocol_model.py
@@ -4,9 +4,9 @@ from typing import TYPE_CHECKING, Generic, Protocol, TypeVar, runtime_checkable
 from bisq.common.protocol.persistable.persistable_payload import PersistablePayload
 from bisq.common.taskrunner.task_model import TaskModel
 from bisq.core.trade.protocol.trade_peer import TradePeer
-from bisq.core.trade.trade_manager import TradeManager 
 
 if TYPE_CHECKING:
+    from bisq.core.trade.trade_manager import TradeManager 
     from bisq.core.network.p2p.p2p_service import P2PService
     from bisq.core.offer.offer import Offer
     from bisq.core.trade.protocol.provider import Provider
