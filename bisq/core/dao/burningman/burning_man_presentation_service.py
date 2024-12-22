d213deb16518b7f6e8cef5d7a9466d0972ed0b58
thecockatiel <thecockatiel@proton.me>
2024-12-22 07:10:02 +0330

    chore: add minimal dao stuff as placeholder

---
+++ bisq/core/dao/burningman/burning_man_presentation_service.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+from bisq.common.config.config import CONFIG
+from bisq.core.dao.state.dao_state_listener import DaoStateListener
+
+if TYPE_CHECKING:
+    from bisq.core.btc.wallet.bsq_wallet_service import BsqWalletService
+    from bisq.core.dao.burningman.burning_man_service import BurningManService
+    from bisq.core.dao.state.dao_state_service import DaoStateService
+
+# TODO
+class BurningManPresentationService(DaoStateListener):
+    """Provides APIs for burningman data representation in the UI."""
+    
+    pass
---
+++ bisq/core/dao/governance/proposal/my_proposal_list_service.py
@@ -0,0 +1,7 @@
+from bisq.common.protocol.persistable.persistable_data_host import PersistedDataHost
+from bisq.core.dao.state.dao_state_listener import DaoStateListener
+
+
+# TODO
+class MyProposalListService(PersistedDataHost, DaoStateListener):
+    pass
