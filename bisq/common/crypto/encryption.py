9ab3722c500709fd01c7ab785db9250e55ef0934
thecockatiel <thecockatiel@proton.me>
2024-12-25 15:17:36 +0330

    fix: ProtectedStorageEntry key comparisons

--- bisq/common/crypto/encryption.py
+++ bisq/common/crypto/encryption.py
@@ -173,7 +173,9 @@ class Encryption:
             raise RuntimeError("Couldn't generate key") from e
 
     @staticmethod
-    def get_public_key_bytes(public_key: Union[PUBLIC_KEY_TYPES, ECPubkey, ECPrivkey]) -> bytes:
+    def get_public_key_bytes(public_key: Union[PUBLIC_KEY_TYPES, ECPubkey, ECPrivkey, bytes]) -> bytes:
+        if isinstance(public_key, bytes):
+            return public_key
         if isinstance(public_key, ECPubkey) or isinstance(public_key, ECPrivkey):
             return public_key.get_public_key_bytes()
         return public_key.public_bytes(
