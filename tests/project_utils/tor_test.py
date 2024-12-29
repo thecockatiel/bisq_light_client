ec0cc4cae1abdc3d467f80e6570c9be7e18ff385
thecockatiel <thecockatiel@proton.me>
2024-12-29 14:34:40 +0330

    chore: add parse_tor_hiddenservice_port

--- tests/project_utils/tor_test.py
+++ tests/project_utils/tor_test.py
@@ -5,20 +5,19 @@ from bisq.common.setup.log_setup import (
     set_custom_log_level,
 )
 
-from utils.tor import is_tor_socks_port_async
+from utils.tor import is_tor_socks_port_async, parse_tor_hidden_service_port
 from utils.twisted import run_with_reactor, teardown_reactor
 
 configure_logging(None)
 set_custom_log_level("TRACE")
 
 import unittest
-import asyncio
 
 from bisq.core.network.p2p.network.tor_network_node import TorNetworkNode
 
 logger = get_logger(__name__)
 
-disabled = False
+disabled = True
 
 
 @unittest.skipIf(disabled, "Disabled test because it requires an already running Tor instances")
@@ -35,6 +34,13 @@ class TestTorDetection(unittest.TestCase):
     @run_with_reactor
     async def test_async_http_client_impl(self):
         self.assertTrue(await is_tor_socks_port_async("127.0.0.1", 9050))
+        
+class TestTorHiddenServiceUtils(unittest.TestCase):
+
+    def test_parse_tor_hiddenservice_port(self):
+        self.assertEqual(parse_tor_hidden_service_port("80"), (80, 80))
+        self.assertEqual(parse_tor_hidden_service_port("80 9999"), (80, 9999))
+        self.assertEqual(parse_tor_hidden_service_port("80 127.0.0.1:9999"), (80, 9999))
 
 
 if __name__ == "__main__":
