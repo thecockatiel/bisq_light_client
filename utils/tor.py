b1fc1452d58e11d02c5b5b10ece38927a152a753
thecockatiel <thecockatiel@proton.me>
2024-12-29 13:14:07 +0330

    chore: make async version of is_tor_socks_port_async

--- utils/tor.py
+++ utils/tor.py
@@ -1,3 +1,4 @@
+import asyncio
 import socket
 from typing import Optional, Tuple
 
@@ -16,6 +17,25 @@ def is_tor_socks_port(host: str, port: int) -> bool:
         pass
     return False
 
+async def is_tor_socks_port_async(host: str, port: int) -> bool:
+    writer = None
+    try:
+        reader, writer = await asyncio.open_connection(host, port)
+        writer.write(b'\x05\x01\x00\x05\xf0\x00\x03\x070.0.0.0\x00\x00')
+        await writer.drain()
+
+        response = await reader.read(1024)
+        
+        return response == b'\x05\x00\x05\x00\x00\x01\x00\x00\x00\x00\x00\x00'
+        
+    except (ConnectionError, asyncio.TimeoutError):
+        return False
+    finally:
+        if writer:
+            writer.close()
+            await writer.wait_closed()
+        
+    
 def detect_tor_socks_proxy() -> Optional[Tuple[str, int]]:
     # Probable ports for Tor to listen at
     candidates = [
