7ae432dd1e621d548211b9f50fd776fa842fbecb
thecockatiel <thecockatiel@proton.me>
2024-12-30 23:40:02 +0330

    chore: bring in more of electrum (while updating things a bit)

--- bitcoinj/script/script_builder.py
+++ bitcoinj/script/script_builder.py
@@ -12,12 +12,12 @@ class ScriptBuilder:
     @staticmethod
     def create_output_script(to: Address) -> Script:
         if isinstance(to, SegwitAddress):
-            return Script(construct_script([to.witness_version, to.witness_program]))
+            return Script(bytes.fromhex(construct_script([to.witness_version, to.witness_program])))
         elif isinstance(to, LegacyAddress):
             hash_160 = to.bytes
             if to.p2sh:
-                return Script(construct_script([opcodes.OP_HASH160, hash_160, opcodes.OP_EQUAL]))
+                return Script(bytes.fromhex(construct_script([opcodes.OP_HASH160, hash_160, opcodes.OP_EQUAL])))
             else:
-                return Script(pubkeyhash_to_p2pkh_script(hash_160))
+                return Script(bytes.fromhex(pubkeyhash_to_p2pkh_script(hash_160)))
         else:
             raise ValueError(f"Unsupported address type: {type(to)}")
