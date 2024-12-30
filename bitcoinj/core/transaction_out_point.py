d2d18d36de90a4ef5160f2ec04e0917243dace04
thecockatiel <thecockatiel@proton.me>
2024-12-30 17:13:43 +0330

    chore: implement a little bit more of transaction out point

--- bitcoinj/core/transaction_out_point.py
+++ bitcoinj/core/transaction_out_point.py
@@ -1,5 +1,6 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
+from bitcoinj.core.sha_256_hash import Sha256Hash
 
 if TYPE_CHECKING:
     from bitcoinj.core.transaction import Transaction
@@ -10,8 +11,36 @@ if TYPE_CHECKING:
 class TransactionOutPoint:
     MESSAGE_LENGTH = 36
 
-    def __init__(self):
+    def __init__(self, *, from_tx: Optional["Transaction"] = None, hash: "Sha256Hash" = None, connected_output: "TransactionOutput" = None, index: int = None):
+        """only either of from_tx or hash or connected_output should be set"""
+        
+        if sum(x is not None for x in [from_tx, hash, connected_output]) > 1:
+            raise ValueError("Only one of from_tx, hash, or connected_output should be set")
+        
         self.index = 0
-        self.from_tx: "Transaction" = None
+        self.from_tx: Optional["Transaction"] = None
+        self.hash: "Sha256Hash" = None
         self.connected_output: "TransactionOutput" = None
-        self.hash: bytes = None
+        self.length = TransactionOutPoint.MESSAGE_LENGTH
+        
+        if connected_output is not None:
+            self.index = connected_output.index
+            self.hash = connected_output.get_parent_transaction_hash()
+            self.connected_output = connected_output
+        elif hash is not None:
+            if index is None:
+                raise ValueError("index must be set when hash is set")
+            self.index = index
+            self.hash = hash
+        else:
+            if index is None:
+                raise ValueError("index must be set when from_tx is set")
+            # from_tx can be none
+            if from_tx is not None:
+                self.hash = from_tx.get_tx_id()
+                self.from_tx = from_tx
+            else:
+                self.hash = Sha256Hash.ZERO_HASH
+
+    def __str__(self):
+        return f"{self.hash}:{self.index}"
--- bitcoinj/core/transaction_output.py
+++ bitcoinj/core/transaction_output.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from bitcoinj.base.coin import Coin
+from bitcoinj.core.sha_256_hash import Sha256Hash
 
 if TYPE_CHECKING:
     from bitcoinj.script.script import Script
@@ -12,6 +13,7 @@ class TransactionOutput:
 
     def __init__(self):
         self.spent_by: "TransactionInput" = None
+        self.parent = None
 
     @property
     def index(self) -> int:
@@ -23,4 +25,10 @@ class TransactionOutput:
     def get_script_pub_key(self) -> Script:
         raise NotImplementedError(
             "get_script_pub_key not implemented in TransactionOutput"
+        )
+
+    def get_parent_transaction_hash(self) -> Optional["Sha256Hash"]:
+        """Returns the transaction hash that owns this output."""
+        raise NotImplementedError(
+            "get_parent_transaction_hash not implemented in TransactionOutput"
         )
