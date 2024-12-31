41d6b8a038cba24ecf20d2432463fda72fb2b20c
thecockatiel <thecockatiel@proton.me>
2024-12-31 11:30:21 +0330

    chore: bring in all of electrum.utils

---
+++ electrum_min/elogging.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Optional
+
+from bisq.common.setup.log_setup import get_logger
+
+class ShortcutInjectingFilter(logging.Filter):
+
+    def __init__(self, *, shortcut: Optional[str]):
+        super().__init__()
+        self.__shortcut = shortcut
+
+    def filter(self, record):
+        record.custom_shortcut = self.__shortcut
+        return True
+
+class Logger:
+
+    # Single character short "name" for this class.
+    # Can be used for filtering log lines. Does not need to be unique.
+    LOGGING_SHORTCUT = None  # type: Optional[str]
+
+    def __init__(self):
+        self.logger = self.__get_logger_for_obj()
+
+    def __get_logger_for_obj(self) -> logging.Logger:
+        cls = self.__class__
+        if cls.__module__:
+            name = f"{cls.__module__}.{cls.__name__}"
+        else:
+            name = cls.__name__
+        try:
+            diag_name = self.diagnostic_name()
+        except Exception as e:
+            raise Exception("diagnostic name not yet available?") from e
+        if diag_name:
+            name += f".[{diag_name}]"
+        logger = get_logger(name)
+        if self.LOGGING_SHORTCUT:
+            logger.addFilter(ShortcutInjectingFilter(shortcut=self.LOGGING_SHORTCUT))
+        return logger
+
+    def diagnostic_name(self):
+        return ''
