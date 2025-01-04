de5f15615f3e3cc8aac48f8f793041f06bf40de2
thecockatiel <thecockatiel@proton.me>
2025-01-04 13:19:24 +0330

    fix: res.get behavior

--- bisq/core/locale/res.py
+++ bisq/core/locale/res.py
@@ -1,6 +1,10 @@
 # TODO: Implemented partially since it was used widely, to complete later as needed.
+from bisq.common.app.dev_env import DevEnv
+from bisq.common.setup.log_setup import get_logger
 from resources import get_resources_path
 from utils.java_compat import parse_resource_bundle
+
+logger = get_logger(__name__)
 
 class Res:
     base_currency_code: str = None
@@ -32,7 +36,16 @@ class Res:
     
     @staticmethod
     def get(key: str, *args):
-        return Res.resources.get(key, key).format(*args)
+        # only format if args are provided
+        try:
+            message = Res.resources[key].replace("BTC", Res.base_currency_code).replace("Bitcoin", Res.base_currency_name).replace("bitcoin", Res.base_currency_name_lower_case)
+        except KeyError:
+            message = key
+            logger.warning(f"Missing resource for key: {key}")
+            
+        if args:
+            return message.format(*args)
+        return message
     
     @staticmethod
     def get_with_col(key: str, *args):
