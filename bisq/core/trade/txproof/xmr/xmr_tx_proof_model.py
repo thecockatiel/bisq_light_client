0fe276f2705f51997cd2446327eea8da755614ee
thecockatiel <thecockatiel@proton.me>
2025-01-05 00:56:48 +0330

    fix: make XmrTxProofModel easier to test

--- bisq/core/trade/txproof/xmr/xmr_tx_proof_model.py
+++ bisq/core/trade/txproof/xmr/xmr_tx_proof_model.py
@@ -1,11 +1,14 @@
+from datetime import datetime
 from typing import TYPE_CHECKING, cast
 from bisq.common.app.dev_env import DevEnv
+from bisq.core.monetary.volume import Volume
 from bisq.core.payment.payload.assets_account_payload import AssetsAccountPayload
 from bisq.core.trade.txproof.asset_tx_proof_model import AssetTxProofModel
 
 if TYPE_CHECKING:
     from bisq.core.user.auto_confirm_settings import AutoConfirmSettings
     from bisq.core.trade.model.bisq_v1.trade import Trade
+
 
 class XmrTxProofModel(AssetTxProofModel):
     # These are values from a valid tx which are set automatically if DevEnv.isDevMode is enabled
@@ -14,24 +17,65 @@ class XmrTxProofModel(AssetTxProofModel):
     DEV_TX_HASH = "5e665addf6d7c6300670e8a89564ed12b5c1a21c336408e2835668f9a6a0d802"
     DEV_AMOUNT = 8902597360000
 
-    def __init__(self, trade: 'Trade', service_address: str, auto_confirm_settings: 'AutoConfirmSettings'):
+    def __init__(
+        self,
+        service_address: str,
+        auto_confirm_settings: "AutoConfirmSettings",
+        volume: Volume,
+        recipient_address: str,
+        tx_hash: str,
+        tx_key: str,
+        trade_date: datetime,
+        trade_id: str,
+    ):
         self.service_address = service_address
         self.auto_confirm_settings = auto_confirm_settings
 
+        self.amount = (
+            XmrTxProofModel.DEV_AMOUNT
+            if DevEnv.is_dev_mode()
+            else (volume.value * 10000 if volume else 0)
+        )  # XMR satoshis have 12 decimal places vs. bitcoin's 8
+        self.recipient_address = (
+            XmrTxProofModel.DEV_ADDRESS
+            if DevEnv.is_dev_mode()
+            else recipient_address
+        )
+        self.tx_hash = tx_hash
+        self.tx_key = tx_key
+        self.trade_date = trade_date
+        self.trade_id = trade_id
+
+    @staticmethod
+    def from_trade(
+        trade: "Trade",
+        service_address: str,
+        auto_confirm_settings: "AutoConfirmSettings",
+    ) -> "XmrTxProofModel":
         volume = trade.get_volume()
-        self.amount = XmrTxProofModel.DEV_AMOUNT if DevEnv.is_dev_mode() else (volume.value * 10000 if volume else 0) # XMR satoshis have 12 decimal places vs. bitcoin's 8
         assert trade.contract is not None, "trade.contract must not be None"
         sellers_payment_account_payload = trade.contract.seller_payment_account_payload
-        assert sellers_payment_account_payload is not None, "trade.contract.sellers_payment_account_payload must not be None"
+        assert (
+            sellers_payment_account_payload is not None
+        ), "trade.contract.sellers_payment_account_payload must not be None"
         # For dev testing we need to add the matching address to the dev tx key and dev view key
-        self.recipient_address = XmrTxProofModel.DEV_ADDRESS if DevEnv.is_dev_mode() else cast(AssetsAccountPayload, sellers_payment_account_payload).address
-        self.tx_hash = trade.counter_currency_tx_id
-        self.tx_key = trade.counter_currency_extra_data
-        self.trade_date = trade.get_date()
-        self.trade_id = trade.get_id()
-        
+        recipient_address = cast(AssetsAccountPayload, sellers_payment_account_payload).address
+        tx_hash = trade.counter_currency_tx_id
+        tx_key = trade.counter_currency_extra_data
+        trade_date = trade.get_date()
+        trade_id = trade.get_id()
+        return XmrTxProofModel(
+            service_address,
+            auto_confirm_settings,
+            volume,
+            recipient_address,
+            tx_hash,
+            tx_key,
+            trade_date,
+            trade_id,
+        )
+
     # NumRequiredConfirmations is read just in time. If user changes autoConfirmSettings during requests it will
     # be reflected at next result parsing.
     def get_num_required_confirmations(self):
         return self.auto_confirm_settings.required_confirmations
-
