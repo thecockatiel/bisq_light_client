99e0df96a93740274ea0f7d80833c0e845ce3eb2
thecockatiel <thecockatiel@proton.me>
2025-01-06 09:09:49 +0330

    chore: create script pattern and use it

--- bitcoinj/script/script.py
+++ bitcoinj/script/script.py
@@ -1,15 +1,13 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any, Optional
 
+from bitcoinj.base.coin import Coin
 from bitcoinj.core.legacy_address import LegacyAddress
 from bitcoinj.core.segwit_address import SegwitAddress
+from bitcoinj.script.script_pattern import ScriptPattern
 from bitcoinj.script.script_type import ScriptType
+from bitcoinj.script.script_verify_flag import ScriptVerifyFlag
 from electrum_min.bitcoin import opcodes
 from electrum_min.transaction import (
-    SCRIPTPUBKEY_TEMPLATE_P2PKH,
-    SCRIPTPUBKEY_TEMPLATE_P2SH,
-    SCRIPTPUBKEY_TEMPLATE_P2WPKH,
-    SCRIPTPUBKEY_TEMPLATE_P2WSH,
-    SCRIPTPUBKEY_TEMPLATE_WITNESS_V0,
     MalformedBitcoinScript,
     OPPushDataGeneric,
     match_script_against_template,
@@ -17,54 +15,71 @@ from electrum_min.transaction import (
 )
 
 if TYPE_CHECKING:
+    from bitcoinj.core.transaction import Transaction
     from bitcoinj.core.address import Address
     from bitcoinj.core.network_parameters import NetworkParameters
 
 
 # TODO
 class Script:
+    ALL_VERIFY_FLAGS = set([
+        ScriptVerifyFlag.P2SH,
+        ScriptVerifyFlag.STRICTENC,
+        ScriptVerifyFlag.DERSIG,
+        ScriptVerifyFlag.LOW_S,
+        ScriptVerifyFlag.NULLDUMMY,
+        ScriptVerifyFlag.SIGPUSHONLY,
+        ScriptVerifyFlag.MINIMALDATA,
+        ScriptVerifyFlag.DISCOURAGE_UPGRADABLE_NOPS,
+        ScriptVerifyFlag.CLEANSTACK,
+        ScriptVerifyFlag.CHECKLOCKTIMEVERIFY,
+        ScriptVerifyFlag.CHECKSEQUENCEVERIFY,
+    ])
 
     def __init__(self, program_bytes: Optional[bytes] = None):
         self.program = program_bytes or bytes()
         self.address = None
+        self._decoded: Optional[list[tuple[int, bytes | None, int | Any]]] = None
+        
+    @property
+    def decoded(self):
+        if self._decoded is None:
+            self._decoded = [x for x in script_GetOp(self.program)]
+        return self._decoded
 
     def hex(self) -> str:
         return self.program.hex()
 
     def get_to_address(self, params: "NetworkParameters") -> "Address":
-        decoded = [x for x in script_GetOp(self.program)]
-
         # p2pkh
-        if match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2PKH):
-            return LegacyAddress(params, False, decoded[2][1])
+        if ScriptPattern.is_p2pkh(self):
+            return LegacyAddress(params, False, self.decoded[2][1])
 
         # p2sh
-        if match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2SH):
-            return LegacyAddress(params, True, decoded[1][1])
+        if ScriptPattern.is_p2sh(self):
+            return LegacyAddress(params, True, self.decoded[1][1])
 
         # segwit address (version 0)
-        if match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_WITNESS_V0):
-            return SegwitAddress.from_hash(decoded[1][1], params, 0)
+        if ScriptPattern.is_witness_v0(self):
+            return SegwitAddress.from_hash(self.decoded[1][1], params, 0)
 
         # segwit address (version 1-16)
         future_witness_versions = list(range(opcodes.OP_1, opcodes.OP_16 + 1))
         for witver, opcode in enumerate(future_witness_versions, start=1):
             match = [opcode, OPPushDataGeneric(lambda x: 2 <= x <= 40)]
-            if match_script_against_template(decoded, match):
-                return SegwitAddress.from_hash(decoded[1][1], params, witver)
+            if match_script_against_template(self.decoded, match):
+                return SegwitAddress.from_hash(self.decoded[1][1], params, witver)
 
         return MalformedBitcoinScript("Unknown script type")
 
     def get_script_type(self) -> Optional["ScriptType"]:
-        decoded = [x for x in script_GetOp(self.program)]
-
-        if match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2PKH):
+        if ScriptPattern.is_p2pkh(self):
             return ScriptType.P2PKH
-        elif match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2SH):
+        elif ScriptPattern.is_p2sh(self):
             return ScriptType.P2SH
-        elif match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2WPKH):
+        elif ScriptPattern.is_p2wpkh(self):
             return ScriptType.P2WPKH
-        elif match_script_against_template(decoded, SCRIPTPUBKEY_TEMPLATE_P2WSH):
+        elif ScriptPattern.is_p2wsh(self):
             return ScriptType.P2WSH
         else:
             return None
