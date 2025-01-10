6430665a9fe51ce173ec1aa98588441fe927f63f
thecockatiel <thecockatiel@proton.me>
2025-01-10 20:45:06 +0330

    chore: add SellerSendPayoutTxPublishedMessage

---
+++ bisq/core/trade/protocol/bisq_v1/tasks/seller/seller_send_payout_tx_published_message.py
@@ -0,0 +1,72 @@
+from typing import TYPE_CHECKING, Optional
+from bisq.common.setup.log_setup import get_logger
+from bisq.core.trade.model.trade_state import TradeState
+from bisq.core.trade.protocol.bisq_v1.messages.payout_tx_published_message import PayoutTxPublishedMessage
+from bisq.core.trade.protocol.bisq_v1.tasks.send_mailbox_message_task import (
+    SendMailboxMessageTask,
+)
+
+if TYPE_CHECKING:
+    from bisq.core.trade.protocol.bisq_v1.messages.trade_mailbox_message import TradeMailboxMessage
+    from bisq.common.taskrunner.task_runner import TaskRunner
+    from bisq.core.trade.model.bisq_v1.trade import Trade
+    from bisq.core.account.sign.signed_witness import SignedWitness
+
+logger = get_logger(__name__)
+
+class SellerSendPayoutTxPublishedMessage(SendMailboxMessageTask):
+
+    def __init__(self, task_handler: "TaskRunner[Trade]", model: "Trade"):
+        super().__init__(task_handler, model)
+        self.signed_witness: Optional[SignedWitness] = None
+
+    def get_trade_mailbox_message(self, id: str) -> "TradeMailboxMessage":
+        assert self.trade.payout_tx is not None, "trade.payout_tx must not be None"
+        payout_tx = self.trade.payout_tx
+        
+        account_age_witness_service = self.process_model.account_age_witness_service
+        if account_age_witness_service.is_sign_witness_trade(self.trade):
+            # Broadcast is done in accountAgeWitness domain.
+            witness = account_age_witness_service.trader_sign_and_publish_peers_account_age_witness(self.trade)
+            if witness:
+                self.signed_witness = witness
+                
+        return PayoutTxPublishedMessage(
+            trade_id=id,
+            payout_tx=payout_tx.bitcoin_serialize(),
+            sender_node_address=self.process_model.my_node_address,
+            signed_witness=self.signed_witness
+        )
+    
+    def set_state_sent(self) -> None:
+        self.trade.state_property.set(TradeState.SELLER_SENT_PAYOUT_TX_PUBLISHED_MSG)
+        logger.info(f"Sent PayoutTxPublishedMessage: trade_id={self.trade.get_id()} at peer {self.trade.trading_peer_node_address} SignedWitness {self.signed_witness}")
+        self.process_model.trade_manager.request_persistence()
+
+    def set_state_arrived(self) -> None:
+        self.trade.state_property.set(TradeState.SELLER_SAW_ARRIVED_PAYOUT_TX_PUBLISHED_MSG)
+        logger.info(f"PayoutTxPublishedMessage arrived: trade_id={self.trade.get_id()} at peer {self.trade.trading_peer_node_address} SignedWitness {self.signed_witness}")
+        self.process_model.trade_manager.request_persistence()
+
+    def set_state_stored_in_mailbox(self) -> None:
+        self.trade.state_property.set(TradeState.SELLER_STORED_IN_MAILBOX_PAYOUT_TX_PUBLISHED_MSG)
+        logger.info(f"PayoutTxPublishedMessage storedInMailbox: trade_id={self.trade.get_id()} at peer {self.trade.trading_peer_node_address} SignedWitness {self.signed_witness}")
+        self.process_model.trade_manager.request_persistence()
+
+    def set_state_fault(self) -> None:
+        self.trade.state_property.set(TradeState.SELLER_SEND_FAILED_PAYOUT_TX_PUBLISHED_MSG)
+        logger.error(f"PayoutTxPublishedMessage failed: trade_id={self.trade.get_id()} at peer {self.trade.trading_peer_node_address} SignedWitness {self.signed_witness}")
+        self.process_model.trade_manager.request_persistence()
+
+    def run(self) -> None:
+        try:
+            self.run_intercept_hook()
+
+            if self.trade.payout_tx is None:
+                logger.error("PayoutTx is null")
+                self.failed("PayoutTx is null")
+                return
+
+            super().run()
+        except Exception as e:
+            self.failed(e)
