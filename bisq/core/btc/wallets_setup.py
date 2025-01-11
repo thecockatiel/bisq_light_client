119d6844c3549629beea7bba3c071a572fc540ee
thecockatiel <thecockatiel@proton.me>
2025-01-11 09:06:29 +0330

    chore: add incomplete WalletAppSetup

--- bisq/core/btc/wallets_setup.py
+++ bisq/core/btc/wallets_setup.py
@@ -1,14 +1,19 @@
 from bisq.common.config.config import Config
+from bisq.common.setup.log_setup import get_logger
 from utils.data import SimpleProperty
 
+logger = get_logger(__name__)
 
 # TODO
 class WalletsSetup:
+    PRE_SEGWIT_BTC_WALLET_BACKUP = "pre_segwit_bisq_BTC.wallet.backup" 
+    PRE_SEGWIT_BSQ_WALLET_BACKUP = "pre_segwit_bisq_BSQ.wallet.backup"
     
     def __init__(self, config: "Config"):
         self.params = config.base_currency_network.parameters
         self.num_peers_property = SimpleProperty(0)
         self.download_percentage_property = SimpleProperty(100.0/100.0)
+        self.chain_height_property = SimpleProperty(0)
     
     @property
     def is_download_complete(self):
