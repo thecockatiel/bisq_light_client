5a2aceeeb2b2b73ef31311cd0627ee104aeea212
thecockatiel <thecockatiel@proton.me>
2025-01-11 08:28:18 +0330

    chore: add incomplete DaoSetup

---
+++ bisq/core/dao/dao_setup.py
@@ -0,0 +1,23 @@
+from collections.abc import Callable
+from bisq.core.dao.dao_setup_service import DaoSetupService
+
+
+# TODO
+class DaoSetup:
+    """
+    High level entry point for Dao domain.
+    We initialize all main service classes here to be sure they are started.
+    """
+
+    def __init__(self):
+        self.dao_setup_services: list["DaoSetupService"] = []
+
+    def on_all_services_initialized(
+        self,
+        error_message_handler: Callable[[str], None],
+        warn_message_handler: Callable[[str], None],
+    ):
+        pass
+
+    def shutdown(self):
+        pass
