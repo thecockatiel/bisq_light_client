48c25f92743513bd127013fa0b4d2a0d575f69b7
thecockatiel <thecockatiel@proton.me>
2025-01-17 10:02:19 +0330

    chore: finish CoreSetup

--- bisq/core/setup/core_network_capabilities.py
+++ bisq/core/setup/core_network_capabilities.py
@@ -1,29 +1,36 @@
+from typing import TYPE_CHECKING
 from bisq.common.capabilities import Capabilities
 from bisq.common.capability import Capability
-from bisq.common.config.config import Config
 from bisq.common.setup.log_setup import get_logger
 
+if TYPE_CHECKING:
+    from bisq.common.config.config import Config
+
 logger = get_logger(__name__)
+
 
 class CoreNetworkCapabilities:
 
     @staticmethod
-    def set_supported_capabilities():
+    def set_supported_capabilities(config: "Config"):
         Capabilities.app.add_all(
             [
                 Capability.ACK_MSG,
                 Capability.BUNDLE_OF_ENVELOPES,
                 Capability.MEDIATION,
-                Capability.DAO_STATE, # mandatory capability but we ignore it later
+                Capability.DAO_STATE,  # mandatory capability but we ignore it later
                 Capability.SIGNED_ACCOUNT_AGE_WITNESS,
                 Capability.REFUND_AGENT,
                 Capability.NO_ADDRESS_PRE_FIX,
             ]
         )
+
+        CoreNetworkCapabilities.maybe_apply_dao_full_mode(config)
+
         logger.info(Capabilities.app.pretty_print())
 
     @staticmethod
-    def maybe_apply_dao_full_mode(config: Config):
+    def maybe_apply_dao_full_mode(config: "Config"):
         # NOTE: Replaced with noop as we don't support DAO in python implementation
         pass
         # If we set dao full mode at the preferences view we add the capability there. We read the preferences a
