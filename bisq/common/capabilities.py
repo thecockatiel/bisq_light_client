ff0589d129ad8e6606f8b6a480e5480c280b9830
thecockatiel <thecockatiel@proton.me>
2025-01-18 12:33:08 +0330

    chore: format file

--- bisq/common/capabilities.py
+++ bisq/common/capabilities.py
@@ -5,6 +5,7 @@ from bisq.common.setup.log_setup import get_logger
 
 logger = get_logger(__name__)
 
+
 class Capabilities:
     """
     hold a set of capabilities and offers appropriate comparison methods.
@@ -20,7 +21,9 @@ class Capabilities:
     MANDATORY_CAPABILITY = Capability.DAO_STATE
 
     def __init__(self, capabilities: Optional[list[Capability]] = None):
-        self.capabilities: frozenset[Capability] = frozenset(capabilities) if capabilities else frozenset()
+        self.capabilities: frozenset[Capability] = (
+            frozenset(capabilities) if capabilities else frozenset()
+        )
 
     def set(self, capabilities: Optional[frozenset[Capability]] = None):
         self.capabilities = capabilities
@@ -28,7 +31,7 @@ class Capabilities:
     def add_all(self, capabilities: Optional[list[Capability]] = None):
         if capabilities:
             self.capabilities = self.capabilities.union(capabilities)
-            
+
     def is_empty(self) -> bool:
         return not self.capabilities
 
@@ -37,39 +40,41 @@ class Capabilities:
 
     def __contains__(self, capability):
         return capability in self.capabilities
-    
+
     def __iter__(self):
         return iter(self.capabilities)
-    
+
     def __eq__(self, other):
         if isinstance(other, Capabilities):
             return self.capabilities == other.capabilities
         else:
             return False
-    
+
     def __len__(self):
         return len(self.capabilities)
-    
+
     def __hash__(self):
         return None
-    
+
     def __str__(self) -> str:
         return str(self.to_int_list(self))
 
     @staticmethod
-    def to_int_list(capabilities_obj: 'Capabilities') -> list[int]:
+    def to_int_list(capabilities_obj: "Capabilities") -> list[int]:
         return sorted([cap.value for cap in capabilities_obj.capabilities])
 
     @staticmethod
-    def from_int_list(capabilities_list: list[int]) -> 'Capabilities':
+    def from_int_list(capabilities_list: list[int]) -> "Capabilities":
         valid_capabilities = {
             cap for cap in Capability if 0 <= cap.value < len(Capability)
         }
-        caps = {Capability(cap) for cap in capabilities_list if cap in valid_capabilities}
+        caps = {
+            Capability(cap) for cap in capabilities_list if cap in valid_capabilities
+        }
         return Capabilities(list(caps))
 
     @staticmethod
-    def from_string_list(list_str: str) -> 'Capabilities':
+    def from_string_list(list_str: str) -> "Capabilities":
         if not list_str:
             return Capabilities()
         entries = list_str.replace(" ", "").split(",")
@@ -85,16 +90,24 @@ class Capabilities:
         return ", ".join(map(str, self.to_int_list(self)))
 
     @staticmethod
-    def has_mandatory_capability(capabilities: 'Capabilities', mandatory_capability: Optional['Capability'] = None) -> bool:
+    def has_mandatory_capability(
+        capabilities: "Capabilities",
+        mandatory_capability: Optional["Capability"] = None,
+    ) -> bool:
         if mandatory_capability is None:
             mandatory_capability = Capabilities.MANDATORY_CAPABILITY
-        return any(c == mandatory_capability for c in capabilities.capabilities)        
+        return any(c == mandatory_capability for c in capabilities.capabilities)
 
     def pretty_print(self) -> str:
-        return ", ".join([f"{cap.name} [{cap.value}]" for cap in sorted(self.capabilities, key=lambda x: x.value)])
+        return ", ".join(
+            [
+                f"{cap.name} [{cap.value}]"
+                for cap in sorted(self.capabilities, key=lambda x: x.value)
+            ]
+        )
 
     # We return true if our capabilities have less capabilities than the parameter value
-    def has_less(self, other: 'Capabilities') -> bool:
+    def has_less(self, other: "Capabilities") -> bool:
         return self.find_highest_capability() < other.find_highest_capability()
 
     # We use the sum of all capabilities. Alternatively we could use the highest entry.
@@ -102,5 +115,6 @@ class Capabilities:
     # backward compatibility issues, so we should treat capabilities as an append-only data structure.
     def find_highest_capability(self) -> int:
         return sum(cap.value for cap in self.capabilities)
+
 
 Capabilities.app = Capabilities()
