fac08eabfb7c0b5ce164f4ada601176cef3a5fde
thecockatiel <thecockatiel@proton.me>
2025-01-24 18:01:42 +0330

    chore: add exact_multiply

--- bisq/common/util/math_utils.py
+++ bisq/common/util/math_utils.py
@@ -4,20 +4,23 @@ from decimal import Decimal, ROUND_HALF_UP, InvalidOperation
 
 logger = get_logger(__name__)
 
-_infinities = (float('inf'), float('-inf'))
+_infinities = (float("inf"), float("-inf"))
+
 
 # NOTE: on edge cases values where different between java and python, but in general it's ok.
 class MathUtils:
     @staticmethod
-    def round_double(value: float, precision: int, rounding_mode: int = ROUND_HALF_UP) -> float:
+    def round_double(
+        value: float, precision: int, rounding_mode: int = ROUND_HALF_UP
+    ) -> float:
         if precision < 0:
             raise ValueError("Precision cannot be negative")
         if not isinstance(value, (int, float)) or value in _infinities or isnan(value):
             raise ValueError(f"Expected a finite float, but found {value}")
-            
+
         try:
             dec = Decimal(str(value))
-            rounded = dec.quantize(Decimal('0.1') ** precision, rounding=rounding_mode)
+            rounded = dec.quantize(Decimal("0.1") ** precision, rounding=rounding_mode)
             return float(rounded)
         except (InvalidOperation, ValueError) as e:
             logger.error(str(e))
@@ -27,10 +30,10 @@ class MathUtils:
     def round_double_to_long(value: float, rounding_mode: int = ROUND_HALF_UP) -> int:
         if not isinstance(value, (int, float)) or value in _infinities or isnan(value):
             raise ValueError(f"Expected a finite float, but found {value}")
-            
+
         try:
             dec = Decimal(str(value))
-            rounded = dec.quantize(Decimal('1'), rounding=rounding_mode)
+            rounded = dec.quantize(Decimal("1"), rounding=rounding_mode)
             return int(rounded)
         except (InvalidOperation, ValueError) as e:
             logger.error(str(e))
@@ -46,3 +49,7 @@ class MathUtils:
         factor = pow(10, exponent)
         return value / factor
 
+    @staticmethod
+    def exact_multiply(value1: float, value2: float) -> float:
+        result = Decimal(str(value1)) * Decimal(str(value2))
+        return float(result)
