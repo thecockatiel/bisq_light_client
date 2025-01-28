03caa6f64b7e187ed57984f294991905e555a2b8
thecockatiel <thecockatiel@proton.me>
2025-01-29 03:11:32 +0330

    fix: make TradeResultHandler cleaner

--- bisq/core/trade/bisq_v1/trade_result_handler.py
+++ bisq/core/trade/bisq_v1/trade_result_handler.py
@@ -5,11 +5,8 @@ from typing import Generic, TypeVar
 _T = TypeVar("_T")
 
 
-class TradeResultHandler(Callable[[_T], None], Generic[_T]):
+class TradeResultHandler(Generic[_T]):
 
     @abstractmethod
-    def handle_result(self, trade: _T) -> None:
-        pass
-
     def __call__(self, trade: _T):
-        return self.handle_result(trade)
+        pass
