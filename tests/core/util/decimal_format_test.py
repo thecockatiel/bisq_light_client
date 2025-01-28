390060575050215fbc5e6302eb7056b989132882
thecockatiel <thecockatiel@proton.me>
2025-01-28 20:31:18 +0330

    chore: add grouping support to decimal format and add test

--- tests/core/util/decimal_format_test.py
+++ tests/core/util/decimal_format_test.py
@@ -42,5 +42,22 @@ class TestDecimalFormat(unittest.TestCase):
         self.assertEqual("123.4", df.format(123.4))
         self.assertEqual("123.45", df.format(123.454))
 
+    def test_grouping(self):
+        df = DecimalFormat("#.##")
+        df.grouping_used = True
+        self.assertEqual("1,234.56", df.format(1234.56))
+        self.assertEqual("1,234,567.89", df.format(1234567.89))
+        self.assertEqual("-1,234.56", df.format(-1234.56))
+        self.assertEqual("999", df.format(999))
+        self.assertEqual("9,999", df.format(9999))
+
+    def test_custom_grouping_size(self):
+        df = DecimalFormat("#.##")
+        df.grouping_used = True
+        df.grouping_size = 4
+        self.assertEqual("12,3456.78", df.format(123456.78))
+        self.assertEqual("1,2345,6789.01", df.format(123456789.01))
+        self.assertEqual("-1,2345,6789.01", df.format(-123456789.01))
+
 if __name__ == '__main__':
     unittest.main()
