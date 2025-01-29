9aaec1fadef3f3db56d4c3de4f09b18870a3307d
thecockatiel <thecockatiel@proton.me>
2025-01-29 10:48:58 +0330

    chore: add BuyerProcessPayoutTxPublishedMessage

---
+++ bisq/core/trade/protocol/bisq_v1/tasks/buyer/buyer_process_payout_tx_published_message.py
@@ -0,0 +1,65 @@
+from bisq.common.setup.log_setup import get_logger
+from bisq.core.btc.wallet.btc_wallet_service import BtcWalletService
+from bisq.core.btc.wallet.wallet_service import WalletService
+from bisq.core.trade.model.trade_state import TradeState
+from bisq.core.trade.protocol.bisq_v1.messages.payout_tx_published_message import (
+    PayoutTxPublishedMessage,
+)
+from bisq.core.trade.protocol.bisq_v1.tasks.trade_task import TradeTask
+from bisq.core.util.validator import Validator
+
+logger = get_logger(__name__)
+
+
+class BuyerProcessPayoutTxPublishedMessage(TradeTask):
+
+    def run(self):
+        try:
+            self.run_intercept_hook()
+            message = self.process_model.trade_message
+            if not isinstance(message, PayoutTxPublishedMessage):
+                raise ValueError(
+                    f"Expected PayoutTxPublishedMessage but got {message.__class__.__name__ if message else None}"
+                )
+            Validator.check_trade_id(self.process_model.offer_id, message)
+            assert message is not None, "Message is required"
+            assert message.payout_tx is not None, "Payout tx is required"
+
+            # update to the latest peer address of our peer if the message is correct
+            self.trade.trading_peer_node_address = (
+                self.process_model.temp_trading_peer_node_address
+            )
+
+            if self.trade.payout_tx is None:
+                committed_payout_tx = WalletService.maybe_add_network_tx_to_wallet(
+                    message.payout_tx,
+                    self.process_model.btc_wallet_service.wallet,
+                )
+                self.trade.payout_tx = committed_payout_tx
+                BtcWalletService.print_tx(
+                    "payoutTx received from peer", committed_payout_tx
+                )
+
+                self.trade.state_property.set(
+                    TradeState.BUYER_RECEIVED_PAYOUT_TX_PUBLISHED_MSG
+                )
+                self.process_model.btc_wallet_service.reset_coin_locked_in_multi_sig_address_entry(
+                    self.trade.get_id()
+                )
+            else:
+                logger.info(
+                    f"We got the payout tx already set from BuyerSetupPayoutTxListener and do nothing here. trade ID={self.trade.get_id()}"
+                )
+
+            if message.signed_witness is not None:
+                # We received the signedWitness from the seller and publish the data to the network.
+                # The signer has published it as well but we prefer to re-do it on our side as well to achieve higher
+                # resilience.
+                self.process_model.account_age_witness_service.publish_own_signed_witness(
+                    message.signed_witness
+                )
+
+            self.process_model.trade_manager.request_persistence()
+            self.complete()
+        except Exception as e:
+            self.failed(exc=e)
