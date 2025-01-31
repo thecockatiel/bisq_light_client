45dcaf160b39ebaab92b8a1265d4e8d660857fb1
thecockatiel <thecockatiel@proton.me>
2025-01-31 14:24:31 +0330

    chore: add AlreadyExistsException and docs

--- bisq/core/api/exception/failed_precondition_exception.py
+++ bisq/core/api/exception/failed_precondition_exception.py
@@ -2,4 +2,9 @@ from bisq.common.bisq_exception import BisqException
 
 
 class FailedPreconditionException(BisqException):
+    """
+    To be thrown in cases when client is attempting to change some state requiring a
+    pre-conditional state to exist, e.g., when attempting to lock or unlock a wallet that
+    is not encrypted.
+    """
     pass
