1ea59cbafe8c542b91e9b751d18267cfa4950b5a
thecockatiel <thecockatiel@proton.me>
2025-01-31 15:52:40 +0330

    chore: use check_argument more places

--- bisq/common/protocol/network/network_envelope.py
+++ bisq/common/protocol/network/network_envelope.py
@@ -2,6 +2,7 @@ from abc import ABC
 from dataclasses import dataclass, field 
 from google.protobuf.message import Message
 from bisq.common.envelope import Envelope
+from bisq.common.util.preconditions import check_argument
 import pb_pb2 as protobuf
 from bisq.common.version import Version
 
@@ -23,8 +24,7 @@ class NetworkEnvelope(Envelope, ABC):
     def get_message_version(self) -> int:
         # -1 is used for the case that we use an envelope message as payload (mailbox)
         # so we check only against 0 which is the default value if not set
-        if self.message_version == 0:
-            raise ValueError("messageVersion is not set (0).")
+        check_argument(self.message_version != 0, "messageVersion is not set (0).")
         return self.message_version
 
     def __str__(self):
