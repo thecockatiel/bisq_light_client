549c952f6cb3604bd7312625c3f5b4c3772a279d
thecockatiel <thecockatiel@proton.me>
2025-01-31 17:43:12 +0330

    chore: make check_argument second arg optional

--- utils/preconditions.py
+++ utils/preconditions.py
@@ -2,6 +2,6 @@ from typing import Any, Union
 from bisq.core.exceptions.illegal_argument_exception import IllegalArgumentException
 
 
-def check_argument(expression: Union[bool, Any], errorMessage: Union[str, Exception, Any]):
+def check_argument(expression: Union[bool, Any], errorMessage: Union[str, Exception, Any] = ""):
     if not expression:
         raise IllegalArgumentException(str(errorMessage))
