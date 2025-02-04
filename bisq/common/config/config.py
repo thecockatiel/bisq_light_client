ca12229efe5105f5e53f45b4c22e423bb5e1a920
thecockatiel <thecockatiel@proton.me>
2025-02-04 18:51:46 +0330

    chore: move argparse extensions to general utils

--- bisq/common/config/config.py
+++ bisq/common/config/config.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from collections.abc import Callable
-from dataclasses import dataclass, field
 import os
 from pathlib import Path
 import argparse
@@ -10,6 +9,7 @@ from typing import Any, Optional
 from bisq.common.config.base_currency_network import BaseCurrencyNetwork
 from bisq.common.config.config_exception import ConfigException
 from bisq.common.config.config_file_reader import ConfigFileReader
+from utils.argparse_ext import CustomArgumentParser
 from utils.dir import user_data_dir
 from utils.tor import parse_tor_hidden_service_port
 
@@ -69,118 +69,6 @@ def get_if_is_file_and_exists_and_is_readable_or_none(path: Path) -> Optional[Pa
     return None
 
 
-class ConditionalArgumentAction(argparse.Action):
-    def __init__(
-        self,
-        unavailable_if: list[str] = None,
-        available_if: list[str] = None,
-        needs: list[str] = None,
-        *args,
-        **kwargs,
-    ):
-        # checks are run later after parse
-        self.unavailable_if = unavailable_if
-        self.available_if = available_if
-        self.needs = needs
-        super().__init__(*args, **kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, values)
-
-
-class DisabledArgumentAction(argparse.Action):
-    def __init__(self, disable_message: str = None, *args, **kwargs):
-        self.disable_message = disable_message
-        super().__init__(*args, **kwargs)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        msg = self.disable_message or f"Option '{option_string}' is currently disabled"
-        parser.error(msg)
-
-
-class CustomArgumentParser(argparse.ArgumentParser):
-    def add_disabled_argument(self, *args, disable_message: str = None, **kwargs):
-        kwargs["action"] = DisabledArgumentAction
-        kwargs["disable_message"] = disable_message
-        return self.add_argument(*args, **kwargs)
-
-    def add_conditional_argument(
-        self,
-        *args,
-        unavailable_if: list[str] = None,
-        available_if: list[str] = None,
-        needs: list[str] = None,
-        **kwargs,
-    ):
-        kwargs["action"] = ConditionalArgumentAction
-        kwargs["unavailable_if"] = unavailable_if
-        kwargs["available_if"] = available_if
-        kwargs["needs"] = needs
-        return self.add_argument(*args, **kwargs)
-
-    def parse_known_args(self, *args, **kw_args):
-        namespace, args = super().parse_known_args(*args, **kw_args)
-        for action in self._actions:
-            if isinstance(action, ConditionalArgumentAction):
-                if (
-                    action.unavailable_if
-                    and getattr(namespace, action.dest) is not None
-                ):
-                    for arg in action.unavailable_if:
-                        if getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' is not allowed when '{arg}' is present."
-                            )
-                if action.available_if and getattr(namespace, action.dest) is not None:
-                    for arg in action.available_if:
-                        if not getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' is not allowed when '{arg}' is not present."
-                            )
-                if action.needs and getattr(namespace, action.dest) is not None:
-                    for arg in action.needs:
-                        if not getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' requires '{arg}' to be present."
-                                + (
-                                    f" all needed options: {action.needs}"
-                                    if len(action.needs) > 1
-                                    else ""
-                                )
-                            )
-        return namespace, args
-
-    def parse_known_intermixed_args(self, *args, **kw_args):
-        namespace, extras = super().parse_known_intermixed_args(*args, **kw_args)
-        for action in self._actions:
-            if isinstance(action, ConditionalArgumentAction):
-                if (
-                    action.unavailable_if
-                    and getattr(namespace, action.dest) is not None
-                ):
-                    for arg in action.unavailable_if:
-                        if getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' is not allowed when '{arg}' is present."
-                            )
-                if action.available_if and getattr(namespace, action.dest) is not None:
-                    for arg in action.available_if:
-                        if not getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' is not allowed when '{arg}' is not present."
-                            )
-                if action.needs and getattr(namespace, action.dest) is not None:
-                    for arg in action.needs:
-                        if not getattr(namespace, arg):
-                            self.error(
-                                f"Option '{action.dest}' requires '{arg}' to be present."
-                                + (
-                                    f" all needed options: {action.needs}"
-                                    if len(action.needs) > 1
-                                    else ""
-                                )
-                            )
-        return namespace, extras
 
 
 class Config:
