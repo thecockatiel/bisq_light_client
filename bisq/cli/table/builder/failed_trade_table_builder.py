452a4b245d4cba439ed2a7b8b06608c52e850d12
thecockatiel <thecockatiel@proton.me>
2025-02-06 14:03:40 +0330

    chore: add FailedTradeTableBuilder

---
+++ bisq/cli/table/builder/failed_trade_table_builder.py
@@ -0,0 +1,39 @@
+from bisq.cli.table.builder.abstract_trade_list_builder import AbstractTradeListBuilder
+from bisq.cli.table.builder.table_type import TableType
+from bisq.cli.table.table import Table
+import grpc_pb2
+
+
+class FailedTradeTableBuilder(AbstractTradeListBuilder):
+    """Builds a Table from a list of grpc_pb2.TradeInfo objects."""
+
+    def __init__(self, protos: "list[grpc_pb2.TradeInfo]"):
+        super().__init__(TableType.FAILED_TRADES_TBL, protos)
+
+    def build(self) -> Table:
+        self._populate_columns()
+        return Table(
+            self.col_trade_id,
+            self.col_create_date.as_string_column(),
+            self.col_market,
+            self.col_price.justify(),
+            self.col_amount.as_string_column(),
+            self.col_mixed_amount.justify(),
+            self.col_currency,
+            self.col_offer_type,
+            self.col_role,
+            self.col_closing_status,
+        )
+
+    def _populate_columns(self):
+        for trade in self.protos:
+            self.col_trade_id.add_row(trade.trade_id)
+            self.col_create_date.add_row(trade.date)
+            self.col_market.add_row(self.to_market(trade))
+            self.col_price.add_row(trade.trade_price)
+            self.col_amount.add_row(trade.trade_amount_as_long)
+            self.col_mixed_amount.add_row(trade.trade_volume)
+            self.col_currency.add_row(self.to_payment_currency_code(trade))
+            self.col_offer_type.add_row(self.to_offer_type(trade))
+            self.col_role.add_row(trade.role)
+            self.col_closing_status.add_row("Failed")
