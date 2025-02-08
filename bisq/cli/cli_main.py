b11e05858710699e87c12dff9b8ce648bf0af627
thecockatiel <thecockatiel@proton.me>
2025-02-09 02:43:07 +0330

    chore: print to stderr if unsuccessful

--- bisq/cli/cli_main.py
+++ bisq/cli/cli_main.py
@@ -535,7 +535,7 @@ class CliMain:
                 if reply.success:
                     print("proto message sent successfully")
                 else:
-                    print(f"error sending proto message: {reply.error_message}")
+                    print(f"error sending proto message: {reply.error_message}", file=sys.stderr)
             else:
                 raise IllegalArgumentException(f"'{method_name}' is not a supported method")
 
