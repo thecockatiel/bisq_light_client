b83f768b0335867b3f606a2fc27c5bdd8b6a96c8
thecockatiel <thecockatiel@proton.me>
2025-02-12 09:18:41 +0330

    chore: add BurningManAccountingStore

---
+++ bisq/core/dao/burningman/accounting/exceptions/block_height_not_connecting_exception.py
@@ -0,0 +1,2 @@
+class BlockHeightNotConnectingException(Exception):
+    pass
---
+++ bisq/core/dao/burningman/burning_man_accounting_service.py
@@ -0,0 +1,22 @@
+from bisq.common.config.config import Config
+from utils.python_helpers import classproperty
+
+
+# TODO
+class BurningManAccountingService:
+    """
+    Provides APIs for the accounting related aspects of burningmen.
+    Combines the received funds from BTC trade fees and DPT payouts and the burned BSQ.
+    """
+
+    # now 763195 -> 107159 blocks takes about 14h
+    # First tx at BM address 656036 Sun Nov 08 19:02:18 EST 2020
+    # 2 months ago 754555 (date of comment = 2022-11-22)
+    @classproperty
+    def EARLIEST_BLOCK_HEIGHT(cls):
+        return 111 if Config.BASE_CURRENCY_NETWORK_VALUE.is_regtest() else 656035
+
+    EARLIEST_DATE_YEAR = 2020
+    EARLIEST_DATE_MONTH = 10
+    HIST_BSQ_PRICE_LAST_DATE_YEAR = 2022
+    HIST_BSQ_PRICE_LAST_DATE_MONTH = 10
