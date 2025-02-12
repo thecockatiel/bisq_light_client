e4fa964760455ff13a8cbcc736db61c6a74d79d9
thecockatiel <thecockatiel@proton.me>
2025-02-13 00:34:57 +0330

    chore: add TxInput

---
+++ bisq/core/dao/state/model/blockchain/tx_input.py
@@ -0,0 +1,62 @@
+from typing import Optional
+from bisq.common.protocol.persistable.persistable_payload import PersistablePayload
+from bisq.core.dao.state.model.blockchain.tx_output_key import TxOutputKey
+from bisq.core.dao.state.model.immutable_dao_state_model import ImmutableDaoStateModel
+import pb_pb2 as protobuf
+
+
+class TxInput(PersistablePayload, ImmutableDaoStateModel):
+    """
+    An input is really just a reference to the spending output. It gets identified by the
+    txId and the index of that output. We use TxOutputKey to encapsulate that.
+    """
+
+    def __init__(
+        self,
+        connected_tx_output_tx_id: str,
+        connected_tx_output_index: int,
+        pub_key: Optional[str] = None,
+    ):
+        self.connected_tx_output_tx_id = connected_tx_output_tx_id
+        self.connected_tx_output_index = connected_tx_output_index
+        self.pub_key = pub_key
+        """as hex string. Optional."""
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // PROTO BUFFER
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    def to_proto_message(self):
+        builder = protobuf.TxInput(
+            connected_tx_output_tx_id=self.connected_tx_output_tx_id,
+            connected_tx_output_index=self.connected_tx_output_index,
+        )
+        if self.pub_key:
+            builder.pub_key = self.pub_key
+        return builder
+
+    @staticmethod
+    def from_proto(proto: protobuf.TxInput):
+        return TxInput(
+            connected_tx_output_tx_id=proto.connected_tx_output_tx_id,
+            connected_tx_output_index=proto.connected_tx_output_index,
+            pub_key=proto.pub_key if proto.pub_key else None,
+        )
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // API
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    def get_connected_tx_output_key(self):
+        return TxOutputKey(
+            self.connected_tx_output_tx_id, self.connected_tx_output_index
+        )
+
+    def __str__(self):
+        return (
+            "TxInput{\n"
+            f"                    connectedTxOutputTxId='{self.connected_tx_output_tx_id}',\n"
+            f"                    connectedTxOutputIndex={self.connected_tx_output_index},\n"
+            f"                    pubKey={self.pub_key}\n"
+            "               }"
+        )
