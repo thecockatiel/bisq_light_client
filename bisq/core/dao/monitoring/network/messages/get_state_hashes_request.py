57237342cfd3e58fcc2736dd2b15ffc6dc872053
thecockatiel <thecockatiel@proton.me>
2025-02-16 15:37:49 +0330

    chore: add GetStateHashesRequest

---
+++ bisq/core/dao/monitoring/network/messages/get_state_hashes_request.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from bisq.common.capabilities import Capabilities
+from bisq.common.capability import Capability
+from bisq.common.protocol.network.network_envelope import NetworkEnvelope
+from bisq.core.network.p2p.direct_message import DirectMessage
+from bisq.core.network.p2p.initial_data_request import InitialDataRequest
+from bisq.core.network.p2p.storage.payload.capability_requiring_payload import (
+    CapabilityRequiringPayload,
+)
+from utils.data import raise_required
+
+
+@dataclass
+class GetStateHashesRequest(
+    NetworkEnvelope, DirectMessage, CapabilityRequiringPayload, InitialDataRequest
+):
+    height: int = field(default_factory=raise_required)
+    nonce: int = field(default_factory=raise_required)
+
+    def get_required_capabilities(self):
+        return Capabilities([Capability.DAO_STATE])
+
+    def __str__(self):
+        return (
+            f"GetStateHashesRequest{{\n"
+            f"    height={self.height},\n"
+            f"    nonce={self.nonce}\n"
+            f"}} {super().__str__()}"
+        )
