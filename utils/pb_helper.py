3da78c94333b3b790a833d71c96be24d870a976e
thecockatiel <thecockatiel@proton.me>
2025-02-21 00:13:26 +0330

    fix: reload module after applying env

--- utils/pb_helper.py
+++ utils/pb_helper.py
@@ -1,16 +1,16 @@
 import os
+from importlib import reload
+import pb_pb2 as protobuf
 
 
 def is_python_pb_impl_required():
-    from pb_pb2 import GetInventoryResponse
-
-    original = GetInventoryResponse(
+    original = protobuf.GetInventoryResponse(
         inventory={"1": "1", "2": "2", "3": "3"}
     ).SerializeToString()
     for i in range(10):
         # we need to test it 10 times quickly, because it is non deterministic
-        inv = dict(GetInventoryResponse.FromString(original).inventory)
-        test = GetInventoryResponse(inventory=inv).SerializeToString()
+        inv = dict(protobuf.GetInventoryResponse.FromString(original).inventory)
+        test = protobuf.GetInventoryResponse(inventory=inv).SerializeToString()
         if test != original:
             return True
     return False
@@ -30,6 +30,7 @@ def check_and_use_pure_python_pb_implementation(print_warning=True):
         os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = (
             "python"  # Prevents using cpp implementation of protobuf, and thus prevents the random dictionary order issue
         )
+        reload(protobuf)
 
         if is_python_pb_impl_required():
             raise AssertionError(
