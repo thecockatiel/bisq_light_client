3807e6c769658c2a76c74c7b1bc9097494bac1ae
thecockatiel <thecockatiel@proton.me>
2025-02-20 23:52:18 +0330

    fix: make is_python_pb_impl_required more robust

--- utils/pb_helper.py
+++ utils/pb_helper.py
@@ -4,11 +4,13 @@ import os
 def is_python_pb_impl_required():
     from pb_pb2 import GetInventoryResponse
 
-    original = GetInventoryResponse(inventory={"1": "1", "2": "2"}).SerializeToString()
-    inv = dict(GetInventoryResponse.FromString(original).inventory)
-    test = GetInventoryResponse(inventory=inv).SerializeToString()
-    if test != original:
-        return True
+    original = GetInventoryResponse(inventory={"1": "1", "2": "2", '3': '3'}).SerializeToString()
+    for i in range(10):
+        # we need to test it 10 times quickly, because it is non deterministic
+        inv = dict(GetInventoryResponse.FromString(original).inventory)
+        test = GetInventoryResponse(inventory=inv).SerializeToString()
+        if test != original:
+                return True
     return False
 
 
@@ -23,3 +25,5 @@ def check_and_use_pure_python_pb_implementation():
         os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = (
             "python"  # Prevents using cpp implementation of protobuf, and thus prevents the random dictionary order issue
         )
+
+check_and_use_pure_python_pb_implementation()
