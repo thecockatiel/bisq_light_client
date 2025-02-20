12211c901ad309cb82141d8c3166d82e07effbe0
thecockatiel <thecockatiel@proton.me>
2025-02-20 18:12:17 +0330

    chore: implement a bit to skip

--- bisq/core/trade/statistics/trade_statistics_manager.py
+++ bisq/core/trade/statistics/trade_statistics_manager.py
@@ -1,14 +1,24 @@
 # TODO not essential so probably not gonna implement for now
 
-class TradeStatisticsManager():
+from typing import TYPE_CHECKING
+from utils.data import ObservableSet
+
+if TYPE_CHECKING:
+    from bisq.core.trade.statistics.trade_statistics_3 import TradeStatistics3
+
+class TradeStatisticsManager:
+
+    def __init__(self):
+        self.observable_trade_statistics_set = ObservableSet["TradeStatistics3"]()
+
     def shut_down(self):
         pass
-    
+
     def on_all_services_initialized(self):
         pass
-    
+
     def maybe_dump_statistics(self):
         pass
-    
+
     def maybe_republish_trade_statistics(self):
         pass
