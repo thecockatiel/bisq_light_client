a88a19030212ee0c059829bb30796eea468c0320
thecockatiel <thecockatiel@proton.me>
2025-02-20 23:41:57 +0330

    fix: only use pure python pb implementation if necessary

--- run_cli.py
+++ run_cli.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # -*- mode: python -*-
-import os
 import sys
 
-os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python' # Prevents using cpp implementation of protobuf, and thus prevents the random dictionary order issue
+from utils.pb_helper import check_and_use_pure_python_pb_implementation
+
+check_and_use_pure_python_pb_implementation()
 
 MIN_PYTHON_VERSION = "3.9.0"
 _min_python_version_tuple = tuple(map(int, (MIN_PYTHON_VERSION.split("."))))
 
 
 if sys.version_info[:3] < _min_python_version_tuple:
-    sys.exit(f"Error: Bisq light client requires Python version >= {MIN_PYTHON_VERSION}...")
+    sys.exit(
+        f"Error: Bisq light client requires Python version >= {MIN_PYTHON_VERSION}..."
+    )
 
 from bisq.cli.cli_main import CliMain
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     CliMain.main(sys.argv[1:])
