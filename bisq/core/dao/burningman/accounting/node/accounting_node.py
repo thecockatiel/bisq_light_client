62130ca50c001957fbf1bfc7e504ce8b3ace382b
thecockatiel <thecockatiel@proton.me>
2025-02-21 14:00:11 +0330

    chore: add AccountingNode

---
+++ bisq/core/dao/burningman/accounting/node/accounting_node.py
@@ -0,0 +1,145 @@
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+from datetime import timedelta
+from typing import TYPE_CHECKING, Optional
+from bisq.common.setup.log_setup import get_logger
+from bisq.common.user_thread import UserThread
+from bisq.core.dao.dao_setup_service import DaoSetupService
+from bisq.core.dao.state.dao_state_listener import DaoStateListener
+from bisq.core.network.p2p.bootstrap_listener import BootstrapListener
+
+if TYPE_CHECKING:
+    from bisq.core.dao.burningman.accounting.node.full.accounting_block_parser import (
+        AccountingBlockParser,
+    )
+    from bisq.core.dao.burningman.burning_man_accounting_service import (
+        BurningManAccountingService,
+    )
+    from bisq.core.dao.state.dao_state_service import DaoStateService
+    from bisq.core.user.preferences import Preferences
+    from bisq.core.network.p2p.p2p_service import P2PService
+
+logger = get_logger(__name__)
+
+
+class AccountingNode(DaoSetupService, DaoStateListener, ABC):
+    PERMITTED_PUB_KEYS = {
+        "034527b1c2b644283c19c180efbcc9ba51258fbe5c5ce0c95b522f1c9b07896e48",
+        "02a06121632518eef4400419a3aaf944534e8cf357138dd82bba3ad78ce5902f27",
+        "029ff0da89aa03507dfe0529eb74a53bc65fbee7663ceba04f014b0ee4520973b5",
+        "0205c992604969bc70914fc89a6daa43cd5157ac886224a8c0901dd1dc6dd1df45",
+        "023e699281b3ee41f35991f064a5c12cb1b61286dda22c8220b3f707aa21235efb",
+    }
+
+    def __init__(
+        self,
+        p2p_service: "P2PService",
+        dao_state_service: "DaoStateService",
+        burning_man_accounting_service: "BurningManAccountingService",
+        accounting_block_parser: "AccountingBlockParser",
+        preferences: "Preferences",
+    ):
+        self._p2p_service = p2p_service
+        self._dao_state_service = dao_state_service
+        self._burning_man_accounting_service = burning_man_accounting_service
+        self._accounting_block_parser = accounting_block_parser
+        self._preferences = preferences
+
+        self.error_message_handler: Optional[Callable[[str], None]] = None
+        self.warn_message_handler: Optional[Callable[[str], None]] = None
+        self._bootstrap_listener: Optional[BootstrapListener] = None
+        self._try_reorg_counter = 0
+        self._p2p_network_ready = False
+        self._initial_block_requests_complete = False
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // DaoStateListener
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    def on_parse_block_chain_complete(self):
+        self.on_initial_dao_block_parsing_complete()
+
+        # We get called on_parse_block_chain_complete at each new block arriving but we want to react only after initial
+        # parsing is done, so we remove after getting called ourself as listener.
+        self._dao_state_service.remove_dao_state_listener(self)
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // DaoSetupService
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    def add_listeners(self):
+        if not self._preferences.is_process_burning_man_accounting_data():
+            return
+
+        if self._dao_state_service.parse_block_chain_complete:
+            logger.info(
+                "daoStateService.isParseBlockChainComplete is already true, "
+                "we call on_initial_dao_block_parsing_complete directly"
+            )
+            self.on_initial_dao_block_parsing_complete()
+        else:
+            self._dao_state_service.add_dao_state_listener(self)
+
+        class Listener(BootstrapListener):
+            def on_no_seed_node_available(self_):
+                self.on_p2p_network_ready()
+
+            def on_data_received(self_):
+                self.on_p2p_network_ready()
+
+        self._bootstrap_listener = Listener()
+
+    def start(self):
+        # We do not start yet but wait until DAO block parsing is complete to not interfere with
+        # that higher priority activity.
+        pass
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // API
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @abstractmethod
+    def shut_down(self):
+        pass
+
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+    # // Protected
+    # ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @abstractmethod
+    def on_initial_dao_block_parsing_complete(self):
+        pass
+
+    def on_initialized(self):
+        if self._p2p_service.is_bootstrapped():
+            logger.info(
+                "p2PService.isBootstrapped is already true, we call on_p2p_network_ready directly."
+            )
+            self.on_p2p_network_ready()
+        else:
+            self._p2p_service.add_p2p_service_listener(self._bootstrap_listener)
+
+    def on_p2p_network_ready(self):
+        self._p2p_network_ready = True
+        self._p2p_service.remove_p2p_service_listener(self._bootstrap_listener)
+
+    @abstractmethod
+    def start_request_blocks(self):
+        pass
+
+    def on_initial_block_requests_complete(self):
+        self._initial_block_requests_complete = True
+        self._burning_man_accounting_service.on_initial_block_requests_complete()
+
+    def apply_reorg(self):
+        logger.warning("apply_reorg called")
+        self._try_reorg_counter += 1
+        if self._try_reorg_counter < 5:
+            self._burning_man_accounting_service.purge_last_ten_blocks()
+            # Increase delay at each retry
+            delay = self._try_reorg_counter * self._try_reorg_counter
+            UserThread.run_after(self.start_request_blocks, timedelta(seconds=delay))
+        else:
+            logger.warning(
+                f"We tried {self._try_reorg_counter} times to request blocks again after a reorg signal but it is still failing.",
+            )
