786bf1726d38fce474ca7fc406fccdd1236d26d1
thecockatiel <thecockatiel@proton.me>
2025-02-22 07:34:06 +0330

    chore: do instance check in casts

--- bisq/core/dao/monitoring/network/request_proposal_state_hashes_handler.py
+++ bisq/core/dao/monitoring/network/request_proposal_state_hashes_handler.py
@@ -8,6 +8,7 @@ from bisq.core.dao.monitoring.network.messages.get_proposal_state_hashes_request
 from bisq.core.dao.monitoring.network.messages.get_proposal_state_hashes_response import (
     GetProposalStateHashesResponse,
 )
+from bisq.core.exceptions.illegal_state_exception import IllegalStateException
 
 
 class RequestProposalStateHashesHandler(
@@ -24,6 +25,10 @@ class RequestProposalStateHashesHandler(
     def cast_to_get_state_hashes_response(
         self, network_envelope: NetworkEnvelope
     ) -> "GetProposalStateHashesResponse":
+        if not isinstance(network_envelope, GetProposalStateHashesResponse):
+            raise IllegalStateException(
+                f"Expected an instance of GetProposalStateHashesResponse but got {network_envelope.__class__.__name__}"
+            )
         return network_envelope
 
     def is_get_state_hashes_response(self, network_envelope: NetworkEnvelope) -> bool:
