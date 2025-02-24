eaf46fc3219b2358fd6f29eaf2a7017b0ab12b2b
thecockatiel <thecockatiel@proton.me>
2025-02-24 07:32:57 +0330

    chore: update CoreNetworkCapabilities

--- bisq/core/setup/core_network_capabilities.py
+++ bisq/core/setup/core_network_capabilities.py
@@ -15,13 +15,21 @@ class CoreNetworkCapabilities:
     def set_supported_capabilities(config: "Config"):
         Capabilities.app.add_all(
             [
+                # Capability.TRADE_STATISTICS,
+                # Capability.TRADE_STATISTICS_2,
+                Capability.ACCOUNT_AGE_WITNESS,
                 Capability.ACK_MSG,
+                Capability.PROPOSAL,
+                Capability.BLIND_VOTE,
+                Capability.DAO_STATE, # mandatory capability
                 Capability.BUNDLE_OF_ENVELOPES,
                 Capability.MEDIATION,
-                Capability.DAO_STATE,  # mandatory capability but we ignore it later
                 Capability.SIGNED_ACCOUNT_AGE_WITNESS,
                 Capability.REFUND_AGENT,
+                # Capability.TRADE_STATISTICS_HASH_UPDATE,
                 Capability.NO_ADDRESS_PRE_FIX,
+                # Capability.TRADE_STATISTICS_3,
+                Capability.BSQ_SWAP_OFFER
             ]
         )
 
@@ -31,14 +39,14 @@ class CoreNetworkCapabilities:
 
     @staticmethod
     def maybe_apply_dao_full_mode(config: "Config"):
-        # TODO
-        pass
         # If we set dao full mode at the preferences view we add the capability there. We read the preferences a
         # bit later than we call that method so we have to add DAO_FULL_NODE Capability at preferences as well to
         # be sure it is set in both cases.
-        # if config.full_dao_node:
-        #     Capabilities.app.add_all([Capability.DAO_FULL_NODE])
-        # else:
-        #     # A lite node has the capability to receive bsq blocks. We do not want to send BSQ blocks to full nodes
-        #     # as they ignore them anyway.
-        #     Capabilities.app.add_all([Capability.RECEIVE_BSQ_BLOCK])
+        if config.full_dao_node:
+            # Capabilities.app.add_all([Capability.DAO_FULL_NODE])
+            # TODO: uncomment above if full node is implemented
+            Capabilities.app.add_all([Capability.RECEIVE_BSQ_BLOCK])
+        else:
+            # A lite node has the capability to receive bsq blocks. We do not want to send BSQ blocks to full nodes
+            # as they ignore them anyway.
+            Capabilities.app.add_all([Capability.RECEIVE_BSQ_BLOCK])
