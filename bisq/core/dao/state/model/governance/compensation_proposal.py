36d114501449fb4a4cdbdb30f97148892667242c
thecockatiel <thecockatiel@proton.me>
2025-02-24 06:02:03 +0330

    chore: add missing methods

--- bisq/core/dao/state/model/governance/compensation_proposal.py
+++ bisq/core/dao/state/model/governance/compensation_proposal.py
@@ -80,6 +80,12 @@ class CompensationProposal(Proposal, IssuanceProposal, ImmutableDaoStateModel):
     # // Getters
     # ///////////////////////////////////////////////////////////////////////////////////////////
 
+    def get_bsq_address(self):
+        return self.bsq_address
+
+    def get_tx_id(self):
+        return self.tx_id
+
     def get_requested_bsq(self) -> Coin:
         return Coin.value_of(self.requested_bsq)
 
