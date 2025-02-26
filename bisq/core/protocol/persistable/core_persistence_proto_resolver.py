dab9d1b94430fe30a521fa6e94e1516c93e7a263
thecockatiel <thecockatiel@proton.me>
2025-02-26 11:44:06 +0330

    chore: add TradeStatistics stores

--- bisq/core/protocol/persistable/core_persistence_proto_resolver.py
+++ bisq/core/protocol/persistable/core_persistence_proto_resolver.py
@@ -31,6 +31,8 @@ from bisq.core.support.dispute.arbitration.arbitration_dispute_list import Arbit
 from bisq.core.support.dispute.mediation.mediation_dispute_list import MediationDisputeList
 from bisq.core.support.refund.refund_dispute_list import RefundDisputeList
 from bisq.core.trade.model.tradable_list import TradableList
+from bisq.core.trade.statistics.trade_statistics_2_store import TradeStatistics2Store
+from bisq.core.trade.statistics.trade_statistics_3_store import TradeStatistics3Store
 from bisq.core.user.preferences_payload import PreferencesPayload
 from bisq.core.user.user_payload import UserPayload
 import pb_pb2 as protobuf
@@ -44,6 +46,7 @@ if TYPE_CHECKING:
 
 logger = get_logger(__name__)
 
+# fmt: off
 proto_map: dict[str, Callable[[protobuf.PersistableEnvelope, "CorePersistenceProtoResolver"], "PersistableEnvelope"]] = {
     "sequence_number_map": lambda p, resolver: SequenceNumberMap.from_proto(p.sequence_number_map),
     "peer_list": lambda p, resolver: PeerList.from_proto(p.peer_list),
@@ -57,7 +60,7 @@ proto_map: dict[str, Callable[[protobuf.PersistableEnvelope, "CorePersistencePro
     "navigation_path": lambda p, resolver: NavigationPath.from_proto(p.navigation_path),
     "payment_account_list": lambda p, resolver: PaymentAccountList.from_proto(p.payment_account_list, resolver),
     "account_age_witness_store": lambda p, resolver: AccountAgeWitnessStore.from_proto(p.account_age_witness_store),
-    # "trade_statistics2_store": lambda p, resolver: TradeStatistics2Store.from_proto(p),
+    "trade_statistics2_store": lambda p, resolver: TradeStatistics2Store.from_proto(p.trade_statistics2_store),
     "blind_vote_store": lambda p, resolver: BlindVoteStore.from_proto(p.blind_vote_store),
     "proposal_store": lambda p, resolver: ProposalStore.from_proto(p.proposal_store),
     "temp_proposal_store": lambda p, resolver: TempProposalStore.from_proto(p.temp_proposal_store, resolver._network_proto_resolver),
@@ -70,13 +73,14 @@ proto_map: dict[str, Callable[[protobuf.PersistableEnvelope, "CorePersistencePro
     "my_proof_of_burn_list": lambda p, resolver: MyProofOfBurnList.from_proto(p.my_proof_of_burn_list),
     "unconfirmed_bsq_change_output_list": lambda p, resolver: UnconfirmedBsqChangeOutputList.from_proto(p.unconfirmed_bsq_change_output_list),
     "signed_witness_store": lambda p, resolver: SignedWitnessStore.from_proto(p.signed_witness_store),
-    # "trade_statistics3_store": lambda p, resolver: TradeStatistics3Store.from_proto(p),
+    "trade_statistics3_store": lambda p, resolver: TradeStatistics3Store.from_proto(p.trade_statistics3_store),
     "mailbox_message_list": lambda p, resolver: MailboxMessageList.from_proto(p.mailbox_message_list, resolver._network_proto_resolver),
     "ignored_mailbox_map": lambda p, resolver: IgnoredMailboxMap.from_proto(p.ignored_mailbox_map),
     "removed_payloads_map": lambda p, resolver: RemovedPayloadsMap.from_proto(p.removed_payloads_map),
     "bsq_block_store": lambda p, resolver: BsqBlockStore.from_proto(p.bsq_block_store),
     "burning_man_accounting_store": lambda p, resolver: BurningManAccountingStore.from_proto(p.burning_man_accounting_store),
 }
+# fmt: on
 
 class CorePersistenceProtoResolver(CoreProtoResolver, PersistenceProtoResolver):
     def __init__(self, clock: "Clock", btc_wallet_service_provider: DependencyProvider["BtcWalletService"], network_proto_resolver: "NetworkProtoResolver"):
