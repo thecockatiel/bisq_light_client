f604714571b1acbb5da975c56d9a686b9cbf1948
thecockatiel <thecockatiel@proton.me>
2025-02-27 11:15:31 +0330

    chore: format PreliminaryGetDataRequest

--- bisq/core/network/p2p/peers/getdata/messages/preliminary_get_data_request.py
+++ bisq/core/network/p2p/peers/getdata/messages/preliminary_get_data_request.py
@@ -4,36 +4,52 @@ from bisq.common.capabilities import Capabilities
 from bisq.common.protocol.proto_util import ProtoUtil
 from bisq.core.network.p2p.anonymous_message import AnonymousMessage
 from bisq.core.network.p2p.peers.getdata.messages.get_data_request import GetDataRequest
-from bisq.core.network.p2p.supported_capabilities_message import SupportedCapabilitiesMessage
+from bisq.core.network.p2p.supported_capabilities_message import (
+    SupportedCapabilitiesMessage,
+)
 from bisq.common.setup.log_setup import get_logger
 
 import pb_pb2 as protobuf
 
 logger = get_logger(__name__)
 
+
 @dataclass
-class PreliminaryGetDataRequest(GetDataRequest, AnonymousMessage, SupportedCapabilitiesMessage):
-    supported_capabilities: Capabilities = field(default_factory=lambda: Capabilities.app)
+class PreliminaryGetDataRequest(
+    GetDataRequest, AnonymousMessage, SupportedCapabilitiesMessage
+):
+    supported_capabilities: Capabilities = field(
+        default_factory=lambda: Capabilities.app
+    )
 
     def to_proto_network_envelope(self):
         request = protobuf.PreliminaryGetDataRequest(
             nonce=self.nonce,
             excluded_keys=self.excluded_keys,
-            supported_capabilities=Capabilities.to_int_list(self.supported_capabilities),
+            supported_capabilities=Capabilities.to_int_list(
+                self.supported_capabilities
+            ),
+            version=self.version,
         )
-        if self.version:
-            request.version = self.version
         envelope = self.get_network_envelope_builder()
         envelope.preliminary_get_data_request.CopyFrom(request)
-        logger.info(f"Sending a PreliminaryGetDataRequest with {request.ByteSize() / 1000} kB and {len(self.excluded_keys)} excluded key entries. Requester's version={self.version}")
+        logger.info(
+            f"Sending a PreliminaryGetDataRequest with {request.ByteSize() / 1000} kB and {len(self.excluded_keys)} excluded key entries. Requester's version={self.version}"
+        )
         return envelope
 
     @staticmethod
     def from_proto(proto: protobuf.PreliminaryGetDataRequest, message_version: int):
-        excluded_keys = ProtoUtil.byte_set_from_proto_byte_string_list(proto.excluded_keys)
-        requesters_version  = ProtoUtil.string_or_none_from_proto(proto.version)
-        supported_capabilities = Capabilities.from_int_list(proto.supported_capabilities)
-        logger.info(f"Received a PreliminaryGetDataRequest with {proto.ByteSize() / 1000} kB and {len(excluded_keys)} excluded key entries. Requester's version={requesters_version}")
+        excluded_keys = ProtoUtil.byte_set_from_proto_byte_string_list(
+            proto.excluded_keys
+        )
+        requesters_version = ProtoUtil.string_or_none_from_proto(proto.version)
+        supported_capabilities = Capabilities.from_int_list(
+            proto.supported_capabilities
+        )
+        logger.info(
+            f"Received a PreliminaryGetDataRequest with {proto.ByteSize() / 1000} kB and {len(excluded_keys)} excluded key entries. Requester's version={requesters_version}"
+        )
         return PreliminaryGetDataRequest(
             message_version=message_version,
             nonce=proto.nonce,
