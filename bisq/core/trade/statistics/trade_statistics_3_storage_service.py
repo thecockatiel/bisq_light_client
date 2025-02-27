35f9d1ca7bc15665a26f7b5626d29a8a44f3af63
thecockatiel <thecockatiel@proton.me>
2025-02-27 11:39:20 +0330

    chore: add TradeStatistics3StorageService

---
+++ bisq/core/trade/statistics/trade_statistics_3_storage_service.py
@@ -0,0 +1,31 @@
+from bisq.common.persistence.persistence_manager_source import PersistenceManagerSource
+from bisq.core.network.p2p.persistence.map_store_service import MapStoreService
+from bisq.core.network.p2p.storage.payload.persistable_network_payload import (
+    PersistableNetworkPayload,
+)
+from bisq.core.trade.statistics.trade_statistics_3 import TradeStatistics3
+from bisq.core.trade.statistics.trade_statistics_3_store import TradeStatistics3Store
+
+
+
+class TradeStatistics3StorageService(
+    MapStoreService["TradeStatistics3Store", "PersistableNetworkPayload"]
+):
+    FILE_NAME = "TradeStatistics3Store"
+
+    def get_file_name(self):
+        return TradeStatistics3StorageService.FILE_NAME
+
+    def initialize_persistence_manager(self):
+        self.persistence_manager.initialize(
+            self.store, PersistenceManagerSource.NETWORK
+        )
+
+    def can_handle(self, payload: "PersistableNetworkPayload"):
+        return isinstance(payload, TradeStatistics3)
+
+    def create_store(self):
+        return TradeStatistics3Store()
+
+    def persist_now(self):
+        self.persistence_manager.persist_now(lambda: None)
