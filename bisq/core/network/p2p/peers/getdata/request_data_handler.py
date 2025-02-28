1b3decf471b62d4666e657d479010a1e3e1ff8e0
thecockatiel <thecockatiel@proton.me>
2025-02-28 10:10:31 +0330

    chore: mark fields private and format

--- bisq/core/network/p2p/peers/getdata/request_data_handler.py
+++ bisq/core/network/p2p/peers/getdata/request_data_handler.py
@@ -45,15 +45,15 @@ class RequestDataHandler(MessageListener):
         peer_manager: "PeerManager",
         listener: "Listener",
     ):
-        self.network_node = network_node
-        self.data_storage = data_storage
-        self.peer_manager = peer_manager
-        self.listener = listener
-        self.timeout_timer: Optional[Timer] = None
-        self.nonce = next_random_int()
-        self.stopped = False
-        self.peers_node_address: Optional["NodeAddress"] = None
-        self.get_data_request_type = ""
+        self._network_node = network_node
+        self._data_storage = data_storage
+        self._peer_manager = peer_manager
+        self._listener = listener
+        self._timeout_timer: Optional[Timer] = None
+        self._nonce = next_random_int()
+        self._stopped = False
+        self._peers_node_address: Optional["NodeAddress"] = None
+        self._get_data_request_type: Optional[str] = None
 
     def cancel(self):
         self._cleanup()
@@ -61,35 +61,35 @@ class RequestDataHandler(MessageListener):
     def request_data(
         self, node_address: "NodeAddress", is_preliminary_data_request: bool
     ):
-        self.peers_node_address = node_address
+        self._peers_node_address = node_address
 
-        if self.stopped:
+        if self._stopped:
             logger.warning("We have stopped already. We ignore that requestData call.")
             return
 
         if is_preliminary_data_request:
-            get_data_request = self.data_storage.build_preliminary_get_data_request(
-                self.nonce
+            get_data_request = self._data_storage.build_preliminary_get_data_request(
+                self._nonce
             )
         else:
-            get_data_request = self.data_storage.build_get_updated_data_request(
-                self.network_node.node_address_property.value, self.nonce
+            get_data_request = self._data_storage.build_get_updated_data_request(
+                self._network_node.node_address_property.value, self._nonce
             )
 
-        if self.timeout_timer is None:
-            self.timeout_timer = UserThread.run_after(
+        if self._timeout_timer is None:
+            self._timeout_timer = UserThread.run_after(
                 lambda: self._handle_request_data_timeout(
                     get_data_request, node_address
                 ),
                 timedelta(seconds=RequestDataHandler.TIMEOUT_SEC),
             )
 
-        self.get_data_request_type = get_data_request.__class__.__name__
+        self._get_data_request_type = get_data_request.__class__.__name__
         logger.info(
-            f"\n\n>> We send a {self.get_data_request_type} to peer {node_address}\n"
+            f"\n\n>> We send a {self._get_data_request_type} to peer {node_address}\n"
         )
-        self.network_node.add_message_listener(self)
-        future = self.network_node.send_message(node_address, get_data_request)
+        self._network_node.add_message_listener(self)
+        future = self._network_node.send_message(node_address, get_data_request)
         future.add_done_callback(
             lambda f: self._handle_request_data_done(f, node_address, get_data_request)
         )
@@ -98,7 +98,7 @@ class RequestDataHandler(MessageListener):
         self, get_data_request: "GetDataRequest", node_address: "NodeAddress"
     ):
         # setup before sending to avoid race conditions
-        if not self.stopped:
+        if not self._stopped:
             error_message = f"A timeout occurred at sending getDataRequest: {get_data_request} on nodeAddress: {node_address}"
             logger.debug(f"{error_message} / RequestDataHandler={self}")
             self._handle_fault(
@@ -120,7 +120,7 @@ class RequestDataHandler(MessageListener):
             connection = future.result()
             if connection is None:
                 raise Exception("Future returned None, connection was expected")
-            if not self.stopped:
+            if not self._stopped:
                 logger.trace(f"Send {get_data_request} to {node_address} succeeded.")
             else:
                 logger.trace(
@@ -128,7 +128,7 @@ class RequestDataHandler(MessageListener):
                     + "Might be caused by a previous timeout."
                 )
         except Exception as e:
-            if not self.stopped:
+            if not self._stopped:
                 message = (
                     "Sending getDataRequest to "
                     + str(node_address)
@@ -158,13 +158,13 @@ class RequestDataHandler(MessageListener):
         if isinstance(network_envelope, GetDataResponse):
             if (
                 connection.peers_node_address is not None
-                and connection.peers_node_address == self.peers_node_address
+                and connection.peers_node_address == self._peers_node_address
             ):
-                if not self.stopped:
+                if not self._stopped:
                     ts = get_time_ms()
                     get_data_response = network_envelope
                     self._log_contents(get_data_response)
-                    if get_data_response.request_nonce == self.nonce:
+                    if get_data_response.request_nonce == self._nonce:
                         self._stop_timeout_timer()
                         if not connection.peers_node_address:
                             logger.error(
@@ -172,17 +172,17 @@ class RequestDataHandler(MessageListener):
                             )
                             return
 
-                        self.data_storage.process_get_data_response(
+                        self._data_storage.process_get_data_response(
                             get_data_response, connection.peers_node_address
                         )
                         self._cleanup()
-                        self.listener.on_complete(get_data_response.was_truncated)
+                        self._listener.on_complete(get_data_response.was_truncated)
                     else:
                         logger.warning(
                             f"Nonce not matching. That can happen rarely if we get a response after a canceled "
                             + "handshake (timeout causes connection close but peer might have sent a msg before "
                             + "connection was closed).\n\t"
-                            + f"We drop that message. nonce={self.nonce} / requestNonce={get_data_response.request_nonce}"
+                            + f"We drop that message. nonce={self._nonce} / requestNonce={get_data_response.request_nonce}"
                         )
                     logger.info(
                         f"Processing GetDataResponse took {get_time_ms() - ts} ms"
@@ -214,9 +214,9 @@ class RequestDataHandler(MessageListener):
             class_name = payload.__class__.__name__
             self._add_details(num_payloads, payload, class_name)
         sb = "\n#################################################################\n"
-        sb += f"Data provided by node: {self.peers_node_address.get_full_address()}\n"
+        sb += f"Data provided by node: {self._peers_node_address.get_full_address()}\n"
         items = len(data_set) + len(persistable_set)
-        sb += f"Received {items} instances from a {self.get_data_request_type}\n"
+        sb += f"Received {items} instances from a {self._get_data_request_type}\n"
         for key, value in num_payloads.items():
             val1 = value[0].get()
             val2 = value[1].get()
@@ -232,13 +232,15 @@ class RequestDataHandler(MessageListener):
     ):
         if class_name not in num_payloads:
             num_payloads[class_name] = (AtomicInt(0), AtomicInt(0))
-        
+
         num_payloads[class_name][0].get_and_increment()
         # NOTE: following comment is for Java version of bisq. we need to benchmark this for python
         # toProtoMessage().getSerializedSize() is not very cheap. For about 1500 objects it takes about 20 ms
         # I think its justified to get accurate metrics but if it turns out to be a performance issue we might need
         # to remove it and use some more rough estimation by taking only the size of one data type and multiply it.
-        num_payloads[class_name][1].get_and_add(network_payload.to_proto_message().ByteSize())
+        num_payloads[class_name][1].get_and_add(
+            network_payload.to_proto_message().ByteSize()
+        )
 
     def _handle_fault(
         self,
@@ -248,15 +250,15 @@ class RequestDataHandler(MessageListener):
     ):
         self._cleanup()
         logger.info(error_message)
-        self.peer_manager.handle_connection_fault(node_address=node_address)
-        self.listener.on_fault(error_message, None)
+        self._peer_manager.handle_connection_fault(node_address=node_address)
+        self._listener.on_fault(error_message, None)
 
     def _cleanup(self):
-        self.stopped = True
-        self.network_node.remove_message_listener(self)
+        self._stopped = True
+        self._network_node.remove_message_listener(self)
         self._stop_timeout_timer()
 
     def _stop_timeout_timer(self):
-        if self.timeout_timer:
-            self.timeout_timer.stop()
-            self.timeout_timer = None
+        if self._timeout_timer:
+            self._timeout_timer.stop()
+            self._timeout_timer = None
