e15bbe95152afcf142e7a66772d1baa8304984a0
thecockatiel <thecockatiel@proton.me>
2025-03-05 14:49:38 +0330

    chore: add create_new_bisq_wallet

--- electrum_min/wallet.py
+++ electrum_min/wallet.py
@@ -50,7 +50,7 @@ import asyncio
 from aiorpcx import timeout_after, TaskTimeout, ignore_after, run_in_thread
 
 from .i18n import _
-from .bip32 import BIP32Node, convert_bip32_intpath_to_strpath, convert_bip32_strpath_to_intpath
+from .bip32 import BIP32Node, convert_bip32_intpath_to_strpath, convert_bip32_strpath_to_intpath, normalize_bip32_derivation
 from .crypto import sha256
 from . import util
 from .util import (NotEnoughFunds, UserCancelled, profiler, OldTaskGroup, ignore_exceptions,
@@ -3920,6 +3920,32 @@ def create_new_wallet(*, path, config: SimpleConfig, passphrase=None, password=N
 
     seed = Mnemonic('en').make_seed(seed_type=seed_type)
     k = keystore.from_seed(seed, passphrase=passphrase)
+    db.put('keystore', k.dump())
+    db.put('wallet_type', 'standard')
+    if k.can_have_deterministic_lightning_xprv():
+        db.put('lightning_xprv', k.get_lightning_xprv(None))
+    if gap_limit is not None:
+        db.put('gap_limit', gap_limit)
+    wallet = Wallet(db, config=config)
+    wallet.update_password(old_pw=None, new_pw=password, encrypt_storage=encrypt_file)
+    wallet.synchronize()
+    msg = "Please keep your seed in a safe place; if you lose it, you will not be able to restore your wallet."
+    wallet.save_db()
+    return {'seed': seed, 'wallet': wallet, 'msg': msg}
+
+
+def create_new_bisq_wallet(*, path, config: SimpleConfig, derivation_path: str, passphrase=None, password=None,
+                      encrypt_file=True, seed_type=None, gap_limit=None) -> dict:
+    """Create a new wallet for bisq"""
+    storage = WalletStorage(path)
+    if storage.file_exists():
+        raise UserFacingException("Remove the existing wallet first!")
+    db = WalletDB('', storage=storage, upgrade=True)
+
+    seed = Mnemonic('en').make_seed(seed_type=seed_type, num_bits=256) # 24 words
+    root_seed = keystore.bip39_to_seed(seed, passphrase=passphrase)
+    derivation_path = normalize_bip32_derivation(derivation_path)
+    k = keystore.from_bip43_rootseed(root_seed, derivation=derivation_path, xtype="p2wpkh")
     db.put('keystore', k.dump())
     db.put('wallet_type', 'standard')
     if k.can_have_deterministic_lightning_xprv():
