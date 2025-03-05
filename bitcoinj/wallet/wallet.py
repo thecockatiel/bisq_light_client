5971a8513cf070fb2c407b3a850fe7722e1ab8d9
thecockatiel <thecockatiel@proton.me>
2025-03-05 12:45:57 +0330

    chore: update Wallet

--- bitcoinj/wallet/wallet.py
+++ bitcoinj/wallet/wallet.py
@@ -1,42 +1,89 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from bitcoinj.core.address import Address
+from bitcoinj.core.network_parameters import NetworkParameters
 from bitcoinj.crypto.deterministic_key import DeterministicKey
 from bitcoinj.script.script_type import ScriptType
+from electrum_min.util import EventListener, event_listener
 from utils.concurrency import ThreadSafeSet
 
 if TYPE_CHECKING:
+    from electrum_min.wallet import Abstract_Wallet
     from bitcoinj.wallet.listeners.wallet_change_event_listener import WalletChangeEventListener
 
-# TODO
+# TODO implement as needed
 class Wallet:
     
-    def __init__(self):
-        self.change_listeners = ThreadSafeSet["WalletChangeEventListener"]()
+    def __init__(self, electrum_wallet: "Abstract_Wallet", network: "NetworkParameters"):
+        self._electrum_wallet = electrum_wallet
+        self._network = network
+        self._change_listeners = ThreadSafeSet["WalletChangeEventListener"]()
+        self._registered_for_callbacks = False
+        self.register_electrum_callbacks()
 
-    def find_key_from_pub_key_hash(
-        self,
-        pub_key_hash: bytes,
-        script_type: ScriptType,
-    ) -> "DeterministicKey":
-        raise NotImplementedError("find_key_from_pub_key_hash not implemented yet")
+    # //////////////////////////////////////////////////////////////////////
+    # // Electrum bridge
+    # //////////////////////////////////////////////////////////////////////
+
+    def register_electrum_callbacks(self):
+        if not self._registered_for_callbacks:
+            self._registered_for_callbacks = True
+            EventListener.register_callbacks(self)
+        
+    def unregister_electrum_callbacks(self):
+        if self._registered_for_callbacks:
+            self._registered_for_callbacks = False
+            EventListener.unregister_callbacks(self)
+
+    @event_listener
+    def on_event_verified(self, wallet, txid, info):
+        if self._electrum_wallet == wallet:
+            for listener in self._change_listeners:
+                listener.on_wallet_changed(self)
+
+    @event_listener
+    def on_event_new_transaction(self, wallet, tx):
+        if self._electrum_wallet == wallet:
+            for listener in self._change_listeners:
+                listener.on_wallet_changed(self)
+
+    @event_listener
+    def on_event_removed_transaction(self, wallet, tx):
+        if self._electrum_wallet == wallet:
+            for listener in self._change_listeners:
+                listener.on_wallet_changed(self)
+
+    @event_listener
+    def on_event_wallet_updated(self, wallet):
+        if self._electrum_wallet == wallet:
+            for listener in self._change_listeners:
+                listener.on_wallet_changed(self)
+
+    # //////////////////////////////////////////////////////////////////////
+    # // Bitcoinj Wallet API
+    # //////////////////////////////////////////////////////////////////////
 
     def find_key_from_address(
         self,
         address: "Address",
-    ) -> "DeterministicKey":
+    ) -> Optional["DeterministicKey"]:
         script_type = address.output_script_type
         if script_type == ScriptType.P2PKH or script_type == ScriptType.P2WPKH:
-            return self.find_key_from_pub_key_hash(address.hash, script_type)
+            keys = self._electrum_wallet.get_public_keys_with_deriv_info(str(address))
+            if keys:
+                first_item = next(iter(keys.items()))
+                pubkey = first_item[0]
+                keystore = first_item[1][0]
+                return DeterministicKey(pubkey, keystore)
         return None
 
-    def fresh_receive_address(self, script_type: ScriptType) -> "Address":
-        raise NotImplementedError("fresh_receive_address not implemented yet")
+    def get_receiving_address(self) -> "Address":
+        return Address.from_string(self._electrum_wallet.get_receiving_address(), self._network)
 
     def add_change_event_listener(self, listener: "WalletChangeEventListener"):
-        self.change_listeners.add(listener)
+        self._change_listeners.add(listener)
 
     def remove_change_event_listener(self, listener: "WalletChangeEventListener"):
-        if listener in self.change_listeners:
-            self.change_listeners.discard(listener)
+        if listener in self._change_listeners:
+            self._change_listeners.discard(listener)
             return True
         return False
