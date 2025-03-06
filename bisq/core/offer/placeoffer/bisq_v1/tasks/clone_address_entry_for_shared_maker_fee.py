5a97e3be5171ab037a489fdad42c407bf665b51e
thecockatiel <thecockatiel@proton.me>
2025-03-07 00:05:44 +0330

    chore: update CloneAddressEntryForSharedMakerFee

--- bisq/core/offer/placeoffer/bisq_v1/tasks/clone_address_entry_for_shared_maker_fee.py
+++ bisq/core/offer/placeoffer/bisq_v1/tasks/clone_address_entry_for_shared_maker_fee.py
@@ -9,21 +9,28 @@ if TYPE_CHECKING:
     from bisq.core.offer.placeoffer.bisq_v1.place_offer_model import PlaceOfferModel
     from bisq.common.taskrunner.task_runner import TaskRunner
 
-class CloneAddressEntryForSharedMakerFee(Task['PlaceOfferModel']):
-    def __init__(self, task_handler: 'TaskRunner[PlaceOfferModel]', model: 'PlaceOfferModel'):
+
+class CloneAddressEntryForSharedMakerFee(Task["PlaceOfferModel"]):
+    def __init__(
+        self, task_handler: "TaskRunner[PlaceOfferModel]", model: "PlaceOfferModel"
+    ):
         super().__init__(task_handler, model)
-        
+
     def run(self):
         self.run_intercept_hook()
 
         offer = self.model.offer
         maker_fee_tx_id = offer.offer_fee_payment_tx_id
         wallet_service = self.model.wallet_service
-        
-        for reserved_for_trade_entry in wallet_service.get_address_entries(AddressEntryContext.RESERVED_FOR_TRADE):
+
+        for reserved_for_trade_entry in wallet_service.get_address_entries(
+            AddressEntryContext.RESERVED_FOR_TRADE
+        ):
             found_tx_id = self._find_tx_id(reserved_for_trade_entry.get_address())
             if found_tx_id and found_tx_id == maker_fee_tx_id:
-                wallet_service.get_or_clone_address_entry_with_offer_id(reserved_for_trade_entry, offer.id)
+                wallet_service.get_or_clone_address_entry_with_offer_id(
+                    reserved_for_trade_entry, offer.id
+                )
                 self.complete()
                 return
 
@@ -35,17 +42,19 @@ class CloneAddressEntryForSharedMakerFee(Task['PlaceOfferModel']):
         and return the txId if found.
         """
         wallet_service = self.model.wallet_service
-        transactions = wallet_service.get_all_recent_transactions(False)
-        
-        for transaction in transactions:
+
+        for transaction in wallet_service.get_transactions():
             for output in transaction.outputs:
-                if (wallet_service.is_transaction_output_mine(output) and 
-                        WalletService.is_output_script_convertible_to_address(output)):
-                    address_string = WalletService.get_address_string_from_output(output)
+                if wallet_service.is_transaction_output_mine(output):
+                    address_string = WalletService.get_address_string_from_output(
+                        output
+                    )
                     # make sure the output is still unspent
-                    if (address_string is not None and 
-                            address_string == str(address) and 
-                            output.spent_by is None):
+                    if (
+                        address_string is not None
+                        and address_string == str(address)
+                        and output.spent_by is None
+                    ):
                         return transaction.get_tx_id()
-        
+
         return None
