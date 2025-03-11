3023675cb349165c6ac1314494f104550c6f2aeb
thecockatiel <thecockatiel@proton.me>
2025-03-11 04:37:07 +0330

    chore: update TransactionOutput

--- bitcoinj/core/transaction_output.py
+++ bitcoinj/core/transaction_output.py
@@ -120,3 +120,28 @@ class TransactionOutput:
                 )
             )
             return False
+    
+    @staticmethod
+    def is_dust(output: Union["TransactionOutput", "ElectrumTxOutput"]) -> bool:
+        # Transactions that are OP_RETURN can't be dust regardless of their value.
+        if not isinstance(output, ElectrumTxOutput):
+            output = output._ec_tx_output
+
+        assert isinstance(
+            output, int
+        )  # we don't expend spend max like here
+        
+        if ScriptPattern.is_op_return(Script(output.scriptpubkey)):
+            return False
+        return output.value < TransactionOutput._get_min_non_dust_value(output.serialize_to_network())
+    
+    @staticmethod
+    def _get_min_non_dust_value(serialized_size: int):
+        # A typical output is 33 bytes (pubkey hash + opcodes) and requires an input of 148 bytes to spend so we add
+        # that together to find out the total amount of data used to transfer this amount of value. Note that this
+        # formula is wrong for anything that's not a P2PKH output, unfortunately, we must follow Bitcoin Core's
+        # wrongness in order to ensure we're considered standard. A better formula would either estimate the
+        # size of data needed to satisfy all different script types, or just hard code 33 below.
+        fee_per_kb = 3000 # REFERENCE_DEFAULT_MIN_TX_FEE * 3
+        size = serialized_size + 148
+        return fee_per_kb * size // 1000
