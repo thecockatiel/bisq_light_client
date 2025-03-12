a47ad61c3af0ab7a491e0970b3bab50992fd1ba2
thecockatiel <thecockatiel@proton.me>
2025-03-12 16:48:30 +0330

    chore: update protobuf impl checker

--- utils/pb_helper.py
+++ utils/pb_helper.py
@@ -16,19 +16,18 @@ def is_pb_map_order_preserved():
     return True
 
 def is_python_pb_impl_required():
-    # grpc version < 1.49.0 has a bug that causes map order to be lost during serialization
-    # after 1.49 it still happens, but it seems so rare that we can probaby tolerate it
-    # TODO: we have to check if this is really rare or not later
-    import grpc
-    try:
-        version = grpc.__version__
-        version = version.split('.')
-        major, minor = int(version[0]), int(version[1])
-        if major > 1 or (major == 1 and minor > 48):
-            return False
-    except:
-        pass
+    # there's no known version that doesn't have this issue, so we just return True
     return True
+    # import grpc
+    # try:
+    #     version = grpc.__version__
+    #     version = version.split('.')
+    #     major, minor = int(version[0]), int(version[1])
+    #     if major > 1 or (major == 1 and minor > 48):
+    #         return False
+    # except:
+    #     pass
+    # return True
 
 def use_pure_python_pb_implementation():
     os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
@@ -47,10 +46,10 @@ def check_and_use_pure_python_pb_implementation(print_warning=True):
 
         use_pure_python_pb_implementation()
 
-        # Now we can run the check again to make sure it worked
-        if not is_pb_map_order_preserved():
-            raise AssertionError(
-                "Protobuf map order was not preserved after check. \n"
-                "This is a fatal error. \n"
-                "Exiting now..."
-            )
+    # Now we can run the check again to make sure it worked
+    if not is_pb_map_order_preserved():
+        raise AssertionError(
+            "Protobuf map order was not preserved during the test. \n"
+            "This is a fatal error. \n"
+            "Exiting now..."
+        )
