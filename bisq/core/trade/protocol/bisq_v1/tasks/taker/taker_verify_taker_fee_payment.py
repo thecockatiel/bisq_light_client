2efe41de0123242a9724585e102a854d513cddb1
thecockatiel <thecockatiel@proton.me>
2025-03-18 21:10:39 +0330

    chore: add TakerVerifyMakerFeePayment

---
+++ bisq/core/trade/protocol/bisq_v1/tasks/taker/taker_verify_taker_fee_payment.py
@@ -0,0 +1,14 @@
+from bisq.core.trade.protocol.bisq_v1.tasks.trade_task import TradeTask
+
+
+class TakerVerifyMakerFeePayment(TradeTask):
+
+    def run(self):
+        try:
+            self.run_intercept_hook()
+
+            # JAVA TODO missing impl.
+
+            self.complete()
+        except Exception as e:
+            self.failed(exc=e)
