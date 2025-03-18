6bb4dee9f729b64f5c1fef5efc7da44807f53cfd
thecockatiel <thecockatiel@proton.me>
2025-03-18 07:24:13 +0330

    chore: update preconditions

--- utils/preconditions.py
+++ utils/preconditions.py
@@ -3,11 +3,21 @@ from bisq.core.exceptions.illegal_argument_exception import IllegalArgumentExcep
 from bisq.core.exceptions.illegal_state_exception import IllegalStateException
 
 
-def check_argument(expression: Union[bool, Any], errorMessage: Union[str, Exception, Any] = ""):
+def check_argument(
+    expression: Union[bool, Any], error_message: Union[str, Exception, Any] = ""
+):
     if not expression:
-        raise IllegalArgumentException(str(errorMessage))
+        raise IllegalArgumentException(str(error_message))
 
 
-def check_state(expression: Union[bool, Any], errorMessage: Union[str, Exception, Any] = ""):
+def check_state(
+    expression: Union[bool, Any], error_message: Union[str, Exception, Any] = ""
+):
     if not expression:
-        raise IllegalStateException(str(errorMessage))
+        raise IllegalStateException(str(error_message))
+
+
+def check_not_null(reference: Any, error_message: str = "Reference is null") -> Any:
+    if reference is None:
+        raise AssertionError(error_message)
+    return reference
