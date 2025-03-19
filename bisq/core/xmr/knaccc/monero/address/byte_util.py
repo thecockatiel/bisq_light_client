15b46f57ab58339c89ed287793e92b0b57943258
thecockatiel <thecockatiel@proton.me>
2025-03-19 07:16:58 +0330

    chore: add ByteUtil

---
+++ bisq/core/xmr/knaccc/monero/address/byte_util.py
@@ -0,0 +1,19 @@
+class ByteUtil:
+
+    @staticmethod
+    def byte_to_hex(v: int):
+        # make sure v is a byte int value
+        return hex(v & 0xFF)
+
+    @staticmethod
+    def long_to_little_endian_uint32_byte_array(value: int):
+        array = [(value >> (i * 8)) & 0xFF for i in range(4)]
+        return bytes(array)
+
+    @staticmethod
+    def get_big_integer_from_unsigned_little_endian_byte_array(a1: bytes):
+        a = bytearray(a1)
+        a.reverse()
+        a2 = bytearray(len(a1) + 1)
+        a2[1:] = a
+        return int.from_bytes(a2, byteorder="big", signed=False)
