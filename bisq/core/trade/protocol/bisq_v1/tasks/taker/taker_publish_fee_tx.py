a1d01214343a540deb004e796a813538ce714bc7
thecockatiel <thecockatiel@proton.me>
2025-03-19 05:28:30 +0330

    chore: add TakerPublishFeeTx

---
+++ bisq/core/trade/protocol/bisq_v1/tasks/taker/taker_publish_fee_tx.py
@@ -0,0 +1,94 @@
+from typing import TYPE_CHECKING
+from bisq.common.setup.log_setup import get_logger
+from bisq.core.btc.wallet.tx_broadcaster_callback import TxBroadcasterCallback
+from bisq.core.dao.state.model.blockchain.tx_type import TxType
+from bisq.core.trade.model.trade_state import TradeState
+from bisq.core.trade.protocol.bisq_v1.tasks.trade_task import TradeTask
+
+if TYPE_CHECKING:
+    from bisq.core.btc.exceptions.tx_broadcast_exception import TxBroadcastException
+    from bitcoinj.core.transaction import Transaction
+
+
+logger = get_logger(__name__)
+
+
+class TakerPublishFeeTx(TradeTask):
+
+    def run(self):
+        try:
+            self.run_intercept_hook()
+
+            trade_wallet_service = self.process_model.trade_wallet_service
+            take_offer_fee_tx = self.process_model.take_offer_fee_tx
+
+            if self.trade.is_currency_for_taker_fee_btc:
+                # We committed to be sure the tx gets into the wallet even in the broadcast process it would be
+                # committed as well, but if user would close app before success handler returns the commit would not
+                # be done.
+                trade_wallet_service.commit_tx(take_offer_fee_tx)
+
+                class Listener(TxBroadcasterCallback):
+                    def on_success(self_, transaction: "Transaction"):
+                        self._on_success(transaction)
+
+                    def on_failure(self_, exception: "TxBroadcastException"):
+                        self._on_failure(exception)
+
+                trade_wallet_service.broadcast_tx(
+                    take_offer_fee_tx,
+                    Listener(),
+                )
+            else:
+                bsq_wallet_service = self.process_model.bsq_wallet_service
+                bsq_wallet_service.commit_tx(take_offer_fee_tx, TxType.PAY_TRADE_FEE)
+                # We need to create another instance, otherwise the tx would trigger an invalid state exception
+                # if it gets committed 2 times
+                trade_wallet_service.commit_tx(
+                    trade_wallet_service.get_cloned_transaction(take_offer_fee_tx)
+                )
+
+                # We use a short timeout as there are issues with BSQ txs. See comment in TxBroadcaster
+                class Listener(TxBroadcasterCallback):
+                    def on_success(self_, transaction: "Transaction"):
+                        self._on_success(transaction)
+
+                    def on_failure(self_, exception: "TxBroadcastException"):
+                        self._on_failure(exception)
+
+                bsq_wallet_service.broadcast_tx(
+                    take_offer_fee_tx,
+                    Listener(),
+                    timeout_sec=1,
+                )
+
+        except Exception as e:
+            self.failed(exc=e)
+
+    def _on_failure(self, exception: "TxBroadcastException"):
+        if not self.completed:
+            logger.error(str(exception))
+            self.trade.error_message = (
+                "An error occurred.\n" + "Error message:\n" + str(exception)
+            )
+            self.failed(exc=exception)
+        else:
+            logger.warning(
+                "We got the _on_failure callback called after the timeout has been triggered a complete()."
+            )
+
+    def _on_success(self, transaction: "Transaction"):
+        if not self.completed:
+            if transaction is not None:
+                self.trade.taker_fee_tx_id = transaction.get_tx_id()
+                self.trade.state_property.value = (
+                    TradeState.TAKER_PUBLISHED_TAKER_FEE_TX
+                )
+
+                self.process_model.trade_manager.request_persistence()
+
+                self.complete()
+        else:
+            logger.warning(
+                "We got the _on_success callback called after the timeout has been triggered a complete()."
+            )
