301239f645f16680da379f66e410f83d283278ba
thecockatiel <thecockatiel@proton.me>
2025-03-20 06:48:52 +0330

    chore: update TradeProtocolFactory

--- bisq/core/trade/protocol/trade_protocol_factory.py
+++ bisq/core/trade/protocol/trade_protocol_factory.py
@@ -1,3 +1,4 @@
+from bisq.core.exceptions.illegal_state_exception import IllegalStateException
 from bisq.core.trade.model.bisq_v1.buyer_as_maker_trade import BuyerAsMakerTrade
 from bisq.core.trade.model.bisq_v1.buyer_as_taker_trade import BuyerAsTakerTrade
 from bisq.core.trade.model.bisq_v1.seller_as_maker_trade import SellerAsMakerTrade
@@ -15,6 +16,18 @@ from bisq.core.trade.model.bsq_swap.bsq_swap_seller_as_taker_trade import (
     BsqSwapSellerAsTakerTrade,
 )
 from bisq.core.trade.model.trade_model import TradeModel
+from bisq.core.trade.protocol.bisq_v1.buyer_as_maker_protocol import (
+    BuyerAsMakerProtocol,
+)
+from bisq.core.trade.protocol.bisq_v1.buyer_as_taker_protocol import (
+    BuyerAsTakerProtocol,
+)
+from bisq.core.trade.protocol.bisq_v1.seller_as_maker_protocol import (
+    SellerAsMakerProtocol,
+)
+from bisq.core.trade.protocol.bisq_v1.seller_as_taker_protocol import (
+    SellerAsTakerProtocol,
+)
 from bisq.core.trade.protocol.trade_protocol import TradeProtocol
 
 
@@ -23,4 +36,35 @@ class TradeProtocolFactory:
 
     @staticmethod
     def get_new_trade_protocol(trade_model: "TradeModel") -> TradeProtocol:
-        raise RuntimeError("get_new_trade_protocol not implemented")
+        if isinstance(trade_model, BuyerAsMakerTrade):
+            return BuyerAsMakerProtocol(trade_model)
+        elif isinstance(trade_model, BuyerAsTakerTrade):
+            return BuyerAsTakerProtocol(trade_model)
+        elif isinstance(trade_model, SellerAsMakerTrade):
+            return SellerAsMakerProtocol(trade_model)
+        elif isinstance(trade_model, SellerAsTakerTrade):
+            return SellerAsTakerProtocol(trade_model)
+        elif isinstance(trade_model, BsqSwapBuyerAsMakerTrade):
+            raise NotImplementedError(
+                "BsqSwapBuyerAsMakerProtocol is not implemented yet"
+            )
+            # return BsqSwapBuyerAsMakerProtocol(trade_model)
+        elif isinstance(trade_model, BsqSwapBuyerAsTakerTrade):
+            raise NotImplementedError(
+                "BsqSwapBuyerAsTakerProtocol is not implemented yet"
+            )
+            # return BsqSwapBuyerAsTakerProtocol(trade_model)
+        elif isinstance(trade_model, BsqSwapSellerAsMakerTrade):
+            raise NotImplementedError(
+                "BsqSwapSellerAsMakerProtocol is not implemented yet"
+            )
+            # return BsqSwapSellerAsMakerProtocol(trade_model)
+        elif isinstance(trade_model, BsqSwapSellerAsTakerTrade):
+            raise NotImplementedError(
+                "BsqSwapSellerAsTakerProtocol is not implemented yet"
+            )
+            # return BsqSwapSellerAsTakerProtocol(trade_model)
+        else:
+            raise IllegalStateException(
+                f"Trade not of expected type. Trade={trade_model}"
+            )
