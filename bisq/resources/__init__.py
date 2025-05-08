151062cfc2313cbd24b169c19b2745f823a57faf
thecockatiel <thecockatiel@proton.me>
2025-05-08 20:23:56 +0330

    chore: format

--- bisq/resources/__init__.py
+++ bisq/resources/__init__.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from typing import Optional, Union
 
@@ -6,9 +5,10 @@ __resource_dir = Path(__file__).parent
 core_resource_dir = __resource_dir.joinpath("data", "core", "src", "main", "resources")
 p2p_resource_dir = __resource_dir.joinpath("data", "p2p", "src", "main", "resources")
 
+
 def core_resource_readlines(resource_name: Union[str, Path]) -> Optional[list[str]]:
     try:
-        with open(core_resource_dir.joinpath(resource_name), 'r') as f:
+        with open(core_resource_dir.joinpath(resource_name), "r") as f:
             return f.readlines()
     except FileNotFoundError:
-            return None
+        return None
